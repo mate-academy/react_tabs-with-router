60bc1bb122c78dfa57bfdd71c7fafc3954eee48d
NikitaUshenko <nikita-ushenko@mail.ru>
2019-07-31 18:37:31 +0300

    4 task implemented

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -3,29 +3,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const Tabs = ({ children, currentTab, handleClick }) => (
-  <>
-    <div>
-      {children.map(child => (
-        <Link to={`/tabs/${child.props.id}`}>
-          <button
-            type="button"
-            key={child.props.id}
-            onClick={() => handleClick(child.props.id)}
-            className={(currentTab === child.props.id)
-              ? 'button active'
-              : 'button'}
-          >
-            {child.props.title}
-          </button>
-        </Link>
-      ))}
-    </div>
-    <div className="content">
-      {children[currentTab - 1]}
-    </div>
-  </>
-);
+class Tabs extends React.Component {
+  componentDidMount() {
+    this.props.handleClick(+this.props.match.params.tabId);
+  }
+
+  render() {
+    return (
+      <>
+        <div>
+          {this.props.children.map(child => (
+            <Link to={`/tabs/${child.props.id}`} key={child.props.id}>
+              <button
+                type="button"
+                onClick={() => this.props.handleClick(child.props.id)}
+                className={(this.props.currentTab === child.props.id)
+                  ? 'button active'
+                  : 'button'}
+              >
+                {child.props.title}
+              </button>
+            </Link>
+          ))}
+        </div>
+        <div className="content">
+          {this.props.children[this.props.currentTab - 1]}
+        </div>
+      </>
+    );
+  }
+}
 
 Tabs.Tab = ({ children }) => <div>{children}</div>;
 
@@ -33,9 +40,15 @@ Tabs.propTypes = {
   children: PropTypes.arrayOf(PropTypes.object).isRequired,
   currentTab: PropTypes.number.isRequired,
   handleClick: PropTypes.func.isRequired,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      tabId: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
 };
 
 Tabs.Tab.propTypes = {
-  children: PropTypes.arrayOf(PropTypes.object).isRequired,
+  children: PropTypes.string.isRequired,
 };
+
 export default Tabs;
