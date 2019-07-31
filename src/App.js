f96206bda74ea58622ee17fad739bb9336869591
AnnaKuvarina <anya.kuvarina@gmail.com>
2019-07-31 20:49:59 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
 
+import { Route, NavLink } from 'react-router-dom';
+import Home from './Home';
+import Tabs from './Tabs';
+
 class App extends React.Component {
   state = {
     tabs: [
@@ -8,15 +12,42 @@ class App extends React.Component {
       { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
       { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
     ],
+    indexItem: 1,
+  };
+
+  onTabSelected = (id) => {
+    this.setState({ indexItem: id });
   };
 
   render() {
-    const { tabs } = this.state;
+    const { tabs, indexItem } = this.state;
 
     return (
       <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>{tabs.length} tabs</h1>
+        <nav>
+          <ul>
+            <li className="App__nav-link">
+              <NavLink to="/home">Home</NavLink>
+            </li>
+            <li className="App__nav-link">
+              <NavLink to="/tabs">Tabs</NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/home" component={Home} />
+        <Route
+          path="/tabs"
+          render={({ match }) => (
+            <Tabs
+              onTabSelected={this.onTabSelected}
+              indexItem={indexItem}
+              tabs={tabs}
+              match={match}
+            />
+          )}
+        />
+
       </div>
     );
   }
