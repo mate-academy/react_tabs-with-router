4018c97c5e6bcb32b766e9a14c800e218f764978
Kamran <djkamry@gmail.com>
2019-08-05 11:13:40 +0300

    refac

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ const App = () => (
   <BrowserRouter>
     <div className="App">
       <nav className="main-menu">
-        <NavLink to="/" exact>HomePage</NavLink>
+        <NavLink to="/tabs-with-router" exact>HomePage</NavLink>
         <NavLink to="/tabs">TabsPage</NavLink>
       </nav>
-      <Route path="/" exact component={HomePage} />
-      <Route path="/tabs/:tabId?" component={TabsPage} />
+      <Route path="/tabs-with-router/" exact component={HomePage} />
+      <Route path="/tabs-with-router/:tabId?" component={TabsPage} />
     </div>
   </BrowserRouter>
 );
