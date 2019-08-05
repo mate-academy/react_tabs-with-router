a327be23e398c0bb6c532a772b841e316fcb4aa8
Kamran <djkamry@gmail.com>
2019-08-05 11:20:12 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ const App = () => (
   <BrowserRouter>
     <div className="App">
       <nav className="main-menu">
-        <NavLink to="/tabs-with-router" exact>HomePage</NavLink>
+        <NavLink to="/tabs-with-router/" exact>HomePage</NavLink>
         <NavLink to="/tabs-with-router/tabs">TabsPage</NavLink>
       </nav>
       <Route path="/tabs-with-router/" exact component={HomePage} />
-      <Route path="/tabs-with-router/tabs/:tabId?" component={TabsPage} />
+      <Route path="/tabs-with-router/:tabId?" component={TabsPage} />
     </div>
   </BrowserRouter>
 );
