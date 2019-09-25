a4510e9b8ea149e6e6bb2e6ac29d693ccbaca8b4
Rafler <amuronyk@gmail.com>
2019-09-25 20:43:31 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch, NavLink } from 'react-router-dom';
+import Tabs from './components/Tabs/Tabs';
 
 const App = () => (
-  <div className="App">
-    <HomePage />
-    <TabsPage />
-  </div>
+  <>
+    <nav className="ui pointing menu">
+      <NavLink
+        exact
+        to="/"
+        activeClassName="active item"
+        className="item"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/tabs"
+        activeClassName="active item"
+        className="item"
+      >
+        Tabs
+      </NavLink>
+    </nav>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/tabs/:id?" component={TabsPage} />
+    </Switch>
+  </>
 );
 
 const HomePage = () => <h1>Home page</h1>;
@@ -15,8 +36,8 @@ const tabs = [
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
-const TabsPage = () => <Tabs tabs={tabs} currentTabId="tab-1" />;
-
-const Tabs = () => <h2>Tabs</h2>;
+const TabsPage = ({ match }) => (
+  <Tabs tabs={tabs} currentId={match.params.id} />
+);
 
 export default App;
---
+++ src/components/Tabs/Tabs.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Tabs = ({ tabs, currentId }) => {
+  const indexElement
+    = parseInt(currentId !== undefined ? currentId.replace(/tab-/, '') : 0, 10)
+    - 1;
+
+  return (
+    <>
+      <div className="ui attached tabular menu">
+        {tabs.map((tab, i) => (
+          <NavLink
+            to={`/tabs/${tab.id}`}
+            className="item"
+            activeClassName="active item"
+            key={i}
+          >
+            {tab.title}
+          </NavLink>
+        ))}
+      </div>
+      {currentId !== undefined && indexElement <= tabs.length && (
+        <div className="ui bottom attached segment active tab">
+          {tabs[indexElement].content}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Tabs;
