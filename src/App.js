5b9ea1121c420add652106b68767bb966ac21e45
unknown <shklyarigor2015@gmail.com>
2019-10-01 00:58:19 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, NavLink, Route, Switch,
+} from 'react-router-dom';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <HomePage />
-    <TabsPage />
-  </div>
-);
-
-const HomePage = () => <h1>Home page</h1>;
+import HomePage from './components/HomePage/HomePage';
+import TabsPage from './components/TabsPage/TabsPage';
 
 const tabs = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  { id: 'tab-1', title: 'Tabs 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tabs 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tabs 3', content: 'Some text 3' },
 ];
-const TabsPage = () => <Tabs tabs={tabs} currentTabId="tab-1" />;
 
-const Tabs = () => <h2>Tabs</h2>;
+const App = () => (
+  <Router>
+    <nav className="nav">
+      <NavLink exact to="/">Home</NavLink>
+      <NavLink to="/tabs">Tabs</NavLink>
+    </nav>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route
+        path="/tabs/:tabId?"
+        render={({ match: { params: { tabId } } }) => (
+          <TabsPage tabId={tabId} tabs={tabs} />
+        )}
+      />
+    </Switch>
+  </Router>
+);
 
 export default App;
---
+++ src/components/TabsPage/TabsPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '../Tabs/Tabs';
+
+const TabsPage = ({ tabs, tabId }) => (
+  <>
+    <h1>Tabs page</h1>
+    <Tabs tabs={tabs} tabId={tabId} />
+  </>
+);
+
+TabsPage.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.object).isRequired,
+  tabId: PropTypes.string.isRequired,
+};
+
+export default TabsPage;
