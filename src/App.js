d0f9448dbd6494d1e26fe0a9e8c0c62e88bf69f2
Alina Kizilova <alinakzilova@gmail.com>
2019-09-30 23:53:09 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from 'react-router-dom';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <HomePage />
-    <TabsPage />
-  </div>
-);
-
-const HomePage = () => <h1>Home page</h1>;
+import TabsPage from './components/TabsPage/TabsPage';
+import HomePage from './components/HomePage/HomePage';
 
 const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
-const TabsPage = () => <Tabs tabs={tabs} currentTabId="tab-1" />;
 
-const Tabs = () => <h2>Tabs</h2>;
+const App = () => (
+  <div className="app">
+    <Router>
+      <nav className="nav">
+        <NavLink className="nav-link" exact to="/">Home</NavLink>
+        <NavLink className="nav-link" to="/tabs">Tabs</NavLink>
+      </nav>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route
+          path="/tabs/:tabId?"
+          render={({ match: { params: { tabId } } }) => (
+            <TabsPage tabId={tabId} tabs={tabs} />
+          )}
+        />
+      </Switch>
+    </Router>
+  </div>
+);
 
 export default App;
---
+++ src/components/Tabs/Tabs.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import './Tabs.css';
+
+const Tabs = ({ tabs, tabId }) => (
+  <div className="tabs__wrapper">
+    <div className="tabs">
+      {tabs.map(tab => (
+        <div key={tab.id}>
+          <NavLink
+            to={`/tabs/${tab.id}`}
+            className="tab"
+          >
+            {tab.title}
+          </NavLink>
+        </div>
+      ))}
+    </div>
+    <div className="tabs__content">
+      {tabId
+        && tabs[tabs.findIndex(tab => tab.id === tabId)].content}
+    </div>
+  </div>
+);
+
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.object).isRequired,
+  tabId: PropTypes.string.isRequired,
+};
+
+export default Tabs;
