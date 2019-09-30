0548db94b4594cdd09c9b38d3f5fda5b66fc1926
Alex Andrutskii <alex.andrutskii@gmail.com>
2019-10-01 02:32:57 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,39 +1,49 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  NavLink,
+} from 'react-router-dom';
+
+import './App.css';
 
 import HomePage from './components/HomePage/HomePage';
 import TabsPage from './components/TabsPage/TabsPage';
 
-const App = () => {
-  const tabs = [
-    { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-    { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-    { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-  ];
-
-  return (
+const App = () => (
+  <BrowserRouter basename="/tabs">
     <div className="App">
-      <HomePage />
-      <TabsPage tabs={tabs} />
+      <nav>
+        <ul className="main-nav">
+          <li className="main-nav__item">
+            <NavLink
+              className="main-nav__link" 
+              activeClassName="main-nav__link--active" 
+              to="/" 
+              exact
+            >
+              HomePage
+            </NavLink>
+          </li>
+          <li className="main-nav__item">
+            <NavLink 
+              className="main-nav__link" 
+              activeClassName="main-nav__link--active" 
+              to="/tabs" 
+              exact
+            >
+              TabsPage
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/tabs/:tabId?" exact component={TabsPage} />
+      </Switch>
     </div>
-  );
-};
-
-// const App = () => (
-//   <div className="App">
-//     <HomePage />
-//     <TabsPage />
-//   </div>
-// );
-
-// const HomePage = () => <h1>Home page</h1>;
-
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
-// const TabsPage = () => <Tabs tabs={tabs} currentTabId="tab-1" />;
-
-// const Tabs = () => <h2>Tabs</h2>;
+  </BrowserRouter>
+);
 
 export default App;
