7ac70c6658bf8145a3578c484c150c147135a7c5
Illia Bezkorovainyi <illia.bezkorovainyi@heathmont.net>
2019-10-02 01:51:06 +0300

    Tabs with router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, NavLink, Route } from 'react-router-dom';
+import Tabs from './components/Tabs/Tabs';
 
 const App = () => (
-  <div className="App">
-    <HomePage />
-    <TabsPage />
+  <div className="app">
+    <BrowserRouter>
+      <div>
+        <ul className="nav-bar">
+          <li className="nav-bar--item">
+            <NavLink className="nav-bar--item_link" to="/" exact>Home</NavLink>
+          </li>
+          <li className="nav-bar--item">
+            <NavLink className="nav-bar--item_link" to="/tabs">Tabs</NavLink>
+          </li>
+        </ul>
+        <hr />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/tabs" component={TabsPage} />
+      </div>
+    </BrowserRouter>
   </div>
 );
 
-const HomePage = () => <h1>Home page</h1>;
+const HomePage = () => <h1>Home</h1>;
 
 const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
-const TabsPage = () => <Tabs tabs={tabs} currentTabId="tab-1" />;
-
-const Tabs = () => <h2>Tabs</h2>;
+const TabsPage = ({ match }) => <Tabs tabs={tabs} match={match} />;
 
 export default App;
---
+++ src/components/Tabs/Tabs.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavLink, Route, Switch } from 'react-router-dom';
+import './Tabs.css';
+
+const Tabs = ({ tabs, match }) => (
+  <div className="tabs-container">
+    <div className="tabs-header">
+      {tabs.map(tab => (
+        <NavLink
+          className="tab-item"
+          activeClassName="tab-item--active"
+          to={`${match.url}/${tab.id}`}
+        >
+          {tab.title}
+        </NavLink>
+      ))}
+    </div>
+    <Switch>
+      <Route
+        path={`${match.path}/:tabId`}
+        render={({ match }) => (
+          <div className="tab-body">
+            {tabs.find(tab => tab.id === match.params.tabId) === undefined
+              ? ''
+              : tabs.find(tab => tab.id === match.params.tabId).content
+            }
+          </div>
+        )}
+      />
+    </Switch>
+  </div>
+);
+
+export default Tabs;
