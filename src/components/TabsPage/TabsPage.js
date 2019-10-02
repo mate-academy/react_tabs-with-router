5f0faad71d722a197be1e160e79d82b08086d64a
Viktoriia Tsykun <vikatsikun@gmail.com>
2019-10-02 18:47:04 +0300

    Comments fixed

--- src/components/TabsPage/TabsPage.js
+++ src/components/TabsPage/TabsPage.js
@@ -6,11 +6,15 @@ const TabsPage = ({ tabs, currentTabId }) => (
   <>
     <h1>Tabs</h1>
     <Tabs tabs={tabs} />
-    {currentTabId && (
-      <p>
-        {tabs.find(tab => tab.id === currentTabId).content}
-      </p>
-    )}
+    {tabs.find(tab => tab.id === currentTabId)
+      ? (
+        <p>
+          {tabs.find(tab => tab.id === currentTabId).content}
+        </p>
+      ) : (
+        <p>No such tab</p>
+      )
+    }
   </>
 );
 
