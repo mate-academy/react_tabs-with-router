a5c8dabca3eff29eb6eece4c410100850b5dbb21
KseniiaMazan <ksusamazan@gmail.com>
2019-10-02 18:03:47 +0300

    more comments are fixed

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
 import React from 'react';
 import './App.css';
 import {
-  HashRouter, NavLink, Route, Switch,
+  NavLink, Route, Switch,
 } from 'react-router-dom';
 
 import TabsPage from './Components/TabsPage/TabsPage';
 import HomePage from './Components/HomePage/HomePage';
 
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
 const App = () => (
-  <HashRouter>
-    <div className="App">
-      <nav className="navigation">
-        <NavLink className="btn btn-primary nav-item" exact to="/">
+  <div className="App">
+    <nav className="navigation">
+      <NavLink className="btn btn-primary nav-item" exact to="/">
           HomePage
-        </NavLink>
-        <NavLink className="btn btn-primary" to="/tabs">
+      </NavLink>
+      <NavLink className="btn btn-primary" to="/tabs">
           TabsPage
-        </NavLink>
-      </nav>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/tabs/:tabId?" component={TabsPage} />
-      </Switch>
-    </div>
-  </HashRouter>
+      </NavLink>
+    </nav>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route
+        path="/tabs/:tabId?"
+        render={
+          ({ match }) => (
+            <TabsPage
+              match={match}
+              tabs={tabs}
+            />
+          )
+        }
+      />
+    </Switch>
+  </div>
 );
 
 export default App;
--- src/Components/TabsPage/TabsPage.js
+++ src/Components/TabsPage/TabsPage.js
@@ -2,17 +2,15 @@ import React from 'react';
 import Tabs from '../Tabs/Tabs';
 import { TabsPageProps } from '../PropTypes/PropTypes';
 
-const tabs = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-];
+const TabsPage = (props) => {
+  const { match, tabs } = props;
 
-const TabsPage = ({ match }) => (
-  <>
-    <Tabs match={match} tabs={tabs} />
-  </>
-);
+  return (
+    <>
+      <Tabs match={match} tabs={tabs} />
+    </>
+  );
+};
 
 TabsPage.propTypes = TabsPageProps;
 
