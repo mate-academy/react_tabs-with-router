3b400a55d37440b5cc664f0349b24eca20274d48
ArnyMac <max@MacBook-Air-ArnyMac.local>
2019-10-03 22:17:47 +0300

    fix

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,25 +1,11 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
-const Tabs = ({ tabs, match }) => {
-  const tab = tabs.find(item => item.id === match.params.tabsId);
+const TabsContent = ({ tab }) => (
+  <>
+    <h2 style={{ marginLeft: '40px' }}>
+      {tab}
+    </h2>
+  </>
+);
 
-  return (
-    <>
-      <ul className="tabs-list">
-        {tabs.map(tab => (
-          <li key={tab.id}>
-            <NavLink active to={`/tabs/${tab.id}`}>
-              {tab.title}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-      <h2 style={{ marginLeft: '40px' }}>
-        {tab && tab.content}
-      </h2>
-    </>
-  );
-};
-
-export default Tabs;
+export default TabsContent;
--- src/components/TabsPage/TabsPage.jsx
+++ src/components/TabsPage/TabsPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './TabsPage.scss';
-import Tabs from '../Tabs/Tabs';
+import { NavLink } from 'react-router-dom';
+import TabsContent from '../Tabs/Tabs';
 
 const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -8,15 +9,29 @@ const tabs = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const TabsPage = ({ match }) => (
-  <>
-    <h1>
+const TabsPage = ({ match }) => {
+  const currentTab = tabs.find(tab => tab.id === match.params.tabsId)
+    || { content: 'select tab please' };
+
+  return (
+    <>
+      <h1>
       Tabs
-    </h1>
-    <div className="tabs-container">
-      <Tabs match={match} tabs={tabs} />
-    </div>
-  </>
-);
+      </h1>
+      <ul className="tabs-list">
+        {tabs.map(tab => (
+          <li key={tab.id}>
+            <NavLink active to={`/tabs/${tab.id}`}>
+              {tab.title}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+      <div className="tabs-container">
+        <TabsContent match={match} tab={currentTab.content} />
+      </div>
+    </>
+  );
+};
 
 export default TabsPage;
