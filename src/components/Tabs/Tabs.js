1c34b29a88f26c15c530afa70f1d18fec5fd5bf1
Illia Bezkorovainyi <illia.bezkorovainyi@heathmont.net>
2019-10-07 13:07:27 +0300

    refactoring

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -2,41 +2,31 @@ import React from 'react';
 import { NavLink, Route, Switch } from 'react-router-dom';
 import './Tabs.css';
 
-const Tabs = ({ tabs, match }) => (
-  <div className="tabs-container">
-    <div className="tabs-header">
-      {tabs.map(tab => (
-        <NavLink
-          className="tab-item"
-          activeClassName="tab-item--active"
-          to={`/tabs/${tab.id}`}
-          key={tab.id}
-        >
-          {tab.title}
-        </NavLink>
-      ))}
-    </div>
-    <div className="tab-body">
-      {tabs.find(tab => tab.id === match.params.id) === undefined
-        ? ''
-        : tabs.find(tab => tab.id === match.params.id).content
-      }
-    </div>
+const Tabs = ({ tabs, match }) => {
+  const activeTab = tabs.find(tab => tab.id === match.params.id);
 
-    {/* <Switch> */}
-    {/*  <Route */}
-    {/*    path={`${match.path}/:tabId`} */}
-    {/*    render={({ match }) => ( */}
-    {/*      <div className="tab-body"> */}
-    {/*        {tabs.find(tab => tab.id === match.params.tabId) === undefined */}
-    {/*          ? '' */}
-    {/*          : tabs.find(tab => tab.id === match.params.tabId).content */}
-    {/*        } */}
-    {/*      </div> */}
-    {/*    )} */}
-    {/*  /> */}
-    {/* </Switch> */}
-  </div>
-);
+  return (
+    <div className="tabs-container">
+      <div className="tabs-header">
+        {tabs.map(tab => (
+          <NavLink
+            className="tab-item"
+            activeClassName="tab-item--active"
+            to={`/tabs/${tab.id}`}
+            key={tab.id}
+          >
+            {tab.title}
+          </NavLink>
+        ))}
+      </div>
+      <div className="tab-body">
+        {activeTab === undefined
+          ? ''
+          : activeTab.content
+        }
+      </div>
+    </div>
+  );
+};
 
 export default Tabs;
