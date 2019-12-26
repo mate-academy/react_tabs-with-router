82528850e5caf8d13fd7185a2ffd6c1465c52f7f
helmax-y <max_rad@ukr.net>
2019-12-26 13:35:27 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 import './App.css';
 import tabs from './api/tabs';
 import Tabs from './components/Tabs';
 
 const App = () => (
-  <BrowserRouter>
+  <>
     <NavLink to="/" exact>Get Home</NavLink>
     <NavLink to="/tabs">Show Tabs</NavLink>
 
     <Route path="/" exact component={HomePage} />
     <Route path="/tabs/:tabId?" exact component={TabsPage} />
-  </BrowserRouter>
+  </>
 );
 
 const HomePage = () => <h1>Home page</h1>;
