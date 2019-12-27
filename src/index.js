649ed3cf91fe860c154737ca6761a376e47d04f1
vmishchenko <vladislav.a.mishchenko@gmail.com>
2019-12-27 22:26:05 +0200

    Router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter><App /></BrowserRouter>, document.getElementById('root')
+  <HashRouter basename="/react_tabs-with-router">
+    <App />
+  </HashRouter>, document.getElementById('root')
 );
