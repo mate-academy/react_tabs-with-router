12675e16c345fd06e00ba3093d1d202a3d7e2c7f
vvxforce <vvxforce@gmail.com>
2019-12-27 14:20:40 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Switch, Route, NavLink, HashRouter } from 'react-router-dom';
+import HomePage from './HomePage';
+import Tabs from './Tabs';
 import './App.css';
 
 const App = () => (
-  <div className="App">
-    <HomePage />
-    <TabsPage />
-  </div>
+  <HashRouter>
+    <div className="App">
+      <NavLink
+        to="/"
+        exact
+        className="nav-item"
+        activeClassName="nav-item_active"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/tabs"
+        exact
+        className="nav-item"
+        activeClassName="nav-item_active"
+      >
+        Tabs
+      </NavLink>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/tabs" component={TabsPage} />
+      </Switch>
+    </div>
+  </HashRouter>
 );
 
-const HomePage = () => <h1>Home page</h1>;
-
 const tabs = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  {
+    id: 'tab-1', title: 'Tab 1', content: 'Some text 1',
+  },
+  {
+    id: 'tab-2', title: 'Tab 2', content: 'Some text 2',
+  },
+  {
+    id: 'tab-3', title: 'Tab 3', content: 'Some text 3',
+  },
 ];
-const TabsPage = () => <Tabs tabs={tabs} currentTabId="tab-1" />;
 
-const Tabs = () => <h2>Tabs</h2>;
+const TabsPage = ({ match }) => (
+  <Tabs tabs={tabs} match={match} />
+);
+
+TabsPage.propTypes = {
+  match: PropTypes.shape({ path: PropTypes.string.isRequired }).isRequired,
+};
 
 export default App;
