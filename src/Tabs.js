bf8cdfa238b5ba5923b22ebd21256c8d33d8bc7e
IsaacKlark <isaackeklarke@gmail.com>
2019-12-27 12:07:46 +0200

    solution

--- src/Tabs.js
+++ src/Tabs.js
@@ -19,7 +19,7 @@ const tabs = [
 
 const Tabs = ({ match }) => {
   const variant = match.params.tabsId === undefined
-    ? 0
+    ? -1
     : match.params.tabsId.match(/\d/g) - 1;
   let showContentOfTabs = false;
 
