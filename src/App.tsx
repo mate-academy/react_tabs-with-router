4c3ea9b23b27de97b5439e90eb7263b235fc9162
Olexander <kravchenko.sania@gmail.com>
2020-03-19 13:54:41 +0200

    attempt 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,43 +3,30 @@ import React, { FC } from 'react';
 
 import './App.scss';
 import {
-  Route, Switch, NavLink, RouteComponentProps,
+  Route, Switch, NavLink
 } from 'react-router-dom';
-import { Tabs } from './components/Tabs/Tabs';
-
-
-const tabs: Tab[] = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-];
-
-
-const HomePage: FC = () => <h1>Home</h1>;
-
-interface MatchParams {
-  tabId: string;
-}
-
-type Props = RouteComponentProps<MatchParams>;
-
-
-const TabsPage: FC<Props> = ({ match }) => {
-  return (
-    <>
-      <h1>Tabs</h1>
-      <Tabs tabs={tabs} currentTabId={match.params.tabId} />
-    </>
-  );
-};
-
+import { TabsPage } from './components/TabsPage/TabsPage'
+import { HomePage } from './components/HomePage/HomePage'
 
 const App: FC = () => (
 
   <div className="App">
     <nav>
-      <NavLink to="/" exact className="link" activeClassName="link--active">home</NavLink>
-      <NavLink to="/tabs" className="link" activeClassName="link--active">tabs</NavLink>
+      <NavLink
+        to="/"
+        exact
+        className="link"
+        activeClassName="link--active"
+      >
+        home
+      </NavLink>
+      <NavLink
+        to="/tabs"
+        className="link"
+        activeClassName="link--active"
+      >
+        tabs
+      </NavLink>
     </nav>
 
     <Switch>
---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,3 @@
+import React, { FC } from 'react';
+
+export const HomePage: FC = () => <h1>Home</h1>;
