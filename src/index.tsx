6a3cef7d358a325bd6e6cf8da330a659362c3e18
Bohdan Salenik <bohdan.salenik@gmail.com>
2020-03-19 09:57:28 +0200

    My solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 const app = (
-  <BrowserRouter>
+  <HashRouter basename="/react_tabs-with-router">
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
