4e45bbcb997c1c39a6d60feacd9def58a4503542
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-03-31 12:18:29 +0300

    add HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 import { HomePage } from './components/HomePage';
 import { TabsPage } from './components/TabsPage';
 import './App.css';
 
 const App = () => (
-  <BrowserRouter>
-    <div className="App">
-      <h1>Tabs with router</h1>
-      <NavLink to="/" className="app__navlink" exact>Home page</NavLink>
-      <NavLink to="/tabs" className="app__navlink">Tabs page</NavLink>
+  <div className="App">
+    <h1>Tabs with router</h1>
+    <NavLink to="/" className="app__navlink" exact>Home page</NavLink>
+    <NavLink to="/tabs" className="app__navlink">Tabs page</NavLink>
 
-      <Route path="/" exact component={HomePage} />
-      <Route path="/tabs" component={TabsPage} className="app__tabsPage" />
+    <Route path="/" exact component={HomePage} />
+    <Route path="/tabs" component={TabsPage} className="app__tabsPage" />
 
-    </div>
-  </BrowserRouter>
+  </div>
 );
 
 export default App;
