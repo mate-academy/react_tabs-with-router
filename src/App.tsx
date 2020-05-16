866dc6577181590e6eb2893e8d82b4b9097a7801
Oleg <fasfas7971@gmail.com>
2020-05-16 22:31:08 +0300

    Develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,58 @@
 import React from 'react';
-
+import {
+  Route,
+  NavLink,
+  Switch,
+} from 'react-router-dom';
 import './App.css';
 
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+import { HomePage } from './components/HomePage';
+import { Tabs } from './components/Tabs';
 
-const App = () => (
-  <div className="App">
-    <h1>Tabs with router</h1>
-  </div>
-);
+const tabs: Tab[] = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+const App: React.FC = () => {
+  return (
+    <>
+      <nav>
+        <ul className="list">
+          <li className="item">
+            <NavLink
+              activeClassName="activeLink"
+              className="linka"
+              to="/"
+              exact
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="item">
+            <NavLink
+              activeClassName="activeLink"
+              className="linka"
+              to="/tabs"
+            >
+              Tabs
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route
+          path="/tabs/:id?"
+          render={() => (
+            <Tabs tabs={tabs} />
+          )}
+        />
+      </Switch>
+    </>
+  );
+};
 
 export default App;
---
+++ src/components/HomePage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const HomePage: React.FC = () => {
+  return (
+    <div className="header">
+      <h1 className="title">Router Tabs</h1>
+    </div>
+  );
+};
