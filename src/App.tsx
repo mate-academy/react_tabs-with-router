bc86e4d56b3152e7a1786d4b02e35c013fa57949
Aleksandr-Tyagun <alexander.tiagun@gmail.com>
2020-05-19 16:01:27 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,59 @@
 import React from 'react';
+import {
+  HashRouter,
+  Switch,
+  Route,
+  NavLink,
+} from 'react-router-dom';
 
 import './App.css';
+import HomePage from './components/HomePage';
+import TabsPage from './components/TabsPage';
 
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
 
 const App = () => (
-  <div className="App">
-    <h1>Tabs with router</h1>
-  </div>
+  <HashRouter>
+    <div>
+      <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            <NavLink
+              className="navbar-item"
+              activeClassName="is-active"
+              to="/"
+              exact
+            >
+              Home
+            </NavLink>
+            <NavLink
+              className="navbar-item"
+              activeClassName="is-active"
+              to="/tabs"
+              exact
+            >
+              Tabs
+            </NavLink>
+          </div>
+        </div>
+      </nav>
+    </div>
+    <div className="container">
+      <Switch>
+        <Route path="/tabs">
+          <TabsPage tabs={tabs} />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </div>
+  </HashRouter>
+
 );
 
 export default App;
---
+++ src/components/HomePage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <section className="hero">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">
+            Home page
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
