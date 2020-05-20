2ccfd2710e050091f71b281cfe3f42477eacd610
kochatkov <vlad.kochatkov@gmail.com>
2020-05-20 17:22:03 +0300

    'sol'

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
-
 import './App.css';
+import {
+  Switch,
+  Route,
+  NavLink,
+} from 'react-router-dom';
+import HomePage from './HomePage';
+import TabsPage from './TabsPage';
 
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
 
-const App = () => (
-  <div className="App">
-    <h1>Tabs with router</h1>
-  </div>
-);
+const App = () => {
+  return (
+    <div className="App">
+      <nav className="nav">
+        <ul className="nav__list">
+          <li className="nav__item"><NavLink to="/" exact className="nav__link">Home</NavLink></li>
+          <li className="nav__item"><NavLink to="/tabs" className="nav__link">Tabs</NavLink></li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route
+          path="/tabs/:tabsId?"
+          render={({ match }) => (
+            <TabsPage tabsId={match.params.tabsId} />
+          )}
+        />
+      </Switch>
+
+    </div>
+  );
+};
 
 export default App;
---
+++ src/HomePage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <h2 className="tabs__head">Home page</h2>
+  );
+};
+
+export default HomePage;
