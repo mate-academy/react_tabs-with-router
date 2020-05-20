b049f944d9ec5564925db1bd54989b95025d3ff8
Yuriy Panasiuk <yur.panasiuk@gmail.com>
2020-05-20 16:43:06 +0300

    result 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
 import React from 'react';
-
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 import './App.css';
+import TabsPage from './TabsPage';
 
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+const HomePage = () => <h2>Home</h2>;
 
 const App = () => (
-  <div className="App">
-    <h1>Tabs with router</h1>
-  </div>
+  <BrowserRouter>
+    <nav>
+      <ul className="nav_list">
+        <li>
+          <NavLink to="/" exact className="nav_link">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/tabs/" className="nav_link">
+            Tabs
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+    <Route path="/" exact component={HomePage} />
+    <Route
+      path="/tabs/:tabId?"
+      render={({ match }) => <TabsPage tabs={tabs} tabId={match.params.tabId} />}
+    />
+  </BrowserRouter>
 );
 
 export default App;
---
+++ src/Tab.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+type Props = {
+  tab: Tab;
+};
+
+const Tab: React.FC<Props> = ({ tab }) => {
+  return (
+    <>
+      <NavLink
+        className="tab_link"
+        activeClassName="tab__active--link"
+        to={tab.id}
+      >
+        {tab.title}
+      </NavLink>
+
+    </>
+  );
+};
+
+
+export default Tab;
