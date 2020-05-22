9385a2e5171539d24380c90fe0db106b9dc1701f
Nina <pinkangelssugar@gmail.com>
2020-05-22 15:47:50 +0300

    Nina

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <HashRouter>
+  <HashRouter basename="https://github.com/mate-academy/react_tabs-with-router/">
     <App />
   </HashRouter>,
   document.getElementById('root'),
