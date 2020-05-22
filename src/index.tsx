e3bd97dddef1ca12a3f5fbf92920190c33947e36
olenazubashenko <olenazubashenko@gmail.com>
2020-05-22 12:01:56 +0300

    solution

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
 
-ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'));
+ReactDOM.render(
+<HashRouter>
+  <App />
+</HashRouter>, document.getElementById('root'));
