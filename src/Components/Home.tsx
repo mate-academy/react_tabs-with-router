83d93759f4bde38b601ee376494878f71e43a305
Roma <roman.mykhalchuk@yahoo.com>
2020-05-22 11:34:20 +0300

    fixed

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,3 +1,5 @@
 import React from 'react';
 
-export const Home: React.FC = () => <p className="tabs__home">Home Page</p>;
+export const Home: React.FC = () => (
+  <p className="tabs__home">Home Page</p>
+);
