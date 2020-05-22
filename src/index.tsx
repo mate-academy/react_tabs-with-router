e27a9698c5366a10f0bc2a7af3473fa39a17fda9
Nastia-rosy <nastiiarosy@gmail.com>
2020-05-22 17:14:04 +0300

    Solution

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import App from './App';
 import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <HashRouter>
+  <HashRouter  basename="https://github.com/mate-academy/react_tabs-with-router/">
     <App />
   </HashRouter>,
   document.getElementById('root')
