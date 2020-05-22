4ecccf549ab266dd3fabd554386a5ee0a24543e0
Nina <pinkangelssugar@gmail.com>
2020-05-22 15:35:24 +0300

    Nina

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
 import React from 'react';
-
+import {
+  Route, NavLink, Switch,
+} from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import { Tabs } from './components/Tabs';
 
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
 
-const App = () => (
-  <div className="App">
-    <h1>Tabs with router</h1>
-  </div>
-);
+
+const App: React.FC = () => {
+  return (
+    <>
+      <nav>
+        <ul className="navlist">
+          <li className="navlist__item"><NavLink to="/" exact className="navlist__link">Home</NavLink></li>
+          <li className="navlist__item"><NavLink to="/tabs" exact className="navlist__link">Tabs </NavLink></li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route
+          path="/tabs/:tabId?"
+          render={({ match }) => (
+            <Tabs tabs={tabs} activeTabId={match.params.tabId} />
+          )}
+        />
+      </Switch>
+
+    </>
+  );
+};
+
 
 export default App;
---
+++ src/components/Tabs.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const Tabs: React.FC<Props> = ({ tabs, activeTabId }) => (
+  <div className="container">
+    <h2>Tabs</h2>
+    <div className="tabsTable">
+      {tabs.map(tab => (
+        <Link
+          to={`/tabs/${tab.id}`}
+          key={tab.id}
+          id={tab.id}
+          className={activeTabId === tab.id ? 'link_active' : 'link'}
+        >
+          {tab.title}
+        </Link>
+      ))}
+    </div>
+    <div className="tab">
+      {tabs.find(tab => tab.id === activeTabId)?.content}
+    </div>
+  </div>
+
+);
