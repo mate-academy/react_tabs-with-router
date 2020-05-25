176c15ee263fe03c14f7c682f6ce7ba1a780f732
Igor Goncharenko <qoa11a.merry@gmail.com>
2020-05-25 15:45:04 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
+import { Home } from './components/Home';
+import { Header } from './components/Header';
+import { Tabs, Tab } from './components/Tabs';
 
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+const tabs: Tab[] = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
 
-const App = () => (
-  <div className="App">
-    <h1>Tabs with router</h1>
-  </div>
+const App: React.FC = () => (
+  <>
+    <Header />
+    <main>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route
+          path="/tabs/:tabId?"
+          render={({ match }) => (
+            <Tabs tabs={tabs} currentTabId={match.params.tabId} />
+          )}
+        />
+      </Switch>
+    </main>
+  </>
 );
 
 export default App;
