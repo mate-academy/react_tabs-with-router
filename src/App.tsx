e1911625ec323e80f7baf368c04c9d7438ab00d7
Liliia <chuiko1112@gmail.com>
2020-07-14 14:37:51 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
+import { Route, Switch, NavLink } from 'react-router-dom';
+import { TabsList } from './TabsList';
 
 import './App.css';
 
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
 
-const App = () => (
-  <div className="App">
-    <h1>Tabs with router</h1>
-  </div>
-);
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Tabs with router</h1>
+      <nav className="navbar navbar-light bg-light">
+        <form className="form-inline">
+          <NavLink className="btn btn-outline-success" to="/" exact>Home</NavLink>
+          <NavLink className="btn btn-outline-success" to="/tabs/:id?">Tabs</NavLink>
+        </form>
+      </nav>
+      <Switch>
+        <Route
+          path="/tabs/:id?"
+          render={
+            ({ match }) => <TabsList list={tabs} id={match.params.id} />
+          }
+        />
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/TabsList.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Tab } from './interfaces';
+
+import './App.css';
+
+interface Props {
+  list: Tab[];
+  id: string;
+}
+
+export const TabsList: React.FC<Props> = ({ list, id }) => {
+  const defaultId = id === ':id' ? 'tab-1' : id;
+
+  return (
+    <ul>
+      {
+        list.map(tab => (
+          <NavLink className="navig" key={tab.id} to={`${tab.id}`}>
+            {tab.title}
+          </NavLink>
+        ))
+      }
+      <p className="tab-text">
+        {list.find(tab => tab.id === defaultId)!.content}
+      </p>
+    </ul>
+  );
+};
