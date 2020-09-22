58b6b224aa4ec5cfef2598a76c2eb9d44c1f9c4c
Kostya Sabada <kostyasabada@gmail.com>
2020-09-22 20:10:11 +0300

    hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <Router>
+  <HashRouter>
     <App />
-  </Router>,
+  </HashRouter>,
   document.getElementById('root'),
 );
