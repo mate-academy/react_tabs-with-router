78527f86002f768d83dc870ec32ee4463ebeda3f
Elena-brv <leena.brv@gmail.com>
2020-09-22 09:38:49 +0300

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import './App.css';
-
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+import { HomePage } from './components/HomePage/HomePage';
+import { TabsPage } from './components/TabsPage/TabsPage';
 
 const App = () => (
   <div className="App">
     <h1>Tabs with router</h1>
+    <Route path="/" exact component={HomePage} />
+    <Route
+      path="/tabs"
+      render={() => <TabsPage />}
+    />
   </div>
 );
 
