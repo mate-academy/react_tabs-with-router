b1d4f44d59d6c211e76590217155580ebf0aefd9
Anastasiia Bortnichuk <feilanheid@gmail.com>
2020-09-23 17:33:29 +0300

    basename

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/react_tabs-with-router">
     <App />
   </BrowserRouter>,
   document.getElementById('root'),
