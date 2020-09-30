28cb25c193aef60798c3d62528be8d9f137e6956
Andrii Badekha <badekha@gmail.com>
2020-09-30 20:58:04 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
 import React from 'react';
-
 import './App.scss';
+import {
+  Switch,
+  Route,
+  NavLink,
+} from 'react-router-dom';
+import HomePage from './HomePage';
+import TabsPage from './TabsPage';
 
-/*
-import { RouteComponentProps } from 'react-router-dom';
-
-type TabsPageProps = React.FC<RouteComponentProps<{ tabId: string }>>;
-const TabsPage: TabsPageProps = ({ match }) => {...};
-
-or
-
-import { useParams } from 'react-router-dom';
-
-const TabsPage = () => {
-  const { tabId } = useParams<{ tabId: string }>();
-  ...
+const App = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li><NavLink to="/" exact>Home</NavLink></li>
+          <li><NavLink to="/tabs">Tabs</NavLink></li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route
+          path="/tabs/:tabsId?"
+          render={({ match }) => (
+            <TabsPage tabsId={match.params.tabsId} />
+          )}
+        />
+      </Switch>
+    </div>
+  );
 };
-*/
-
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
-
-const App = () => (
-  <div className="App">
-    <h1>Tabs with router</h1>
-  </div>
-);
 
 export default App;
