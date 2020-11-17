92f4c4dc03a5be4bc77146f3f0bf68a07d0f98f1
AntonChetveryk <chetverykanton92@gmail.com>
2020-11-17 13:28:04 +0200

    delete hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <HashRouter basename={'/react_tabs-with-router'}>
+  <HashRouter>
     <App />
   </HashRouter>,
   document.getElementById('root')
