185e8221920b411e8c5a73f64add3e5f266e8e03
DmitryArhipenko <42931799+DmitryArhipenko@users.noreply.github.com>
2020-11-24 15:58:58 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
-import React from 'react';
+import React, { FC } from 'react';
+import {
+  Route,
+  NavLink,
+  Switch,
+} from 'react-router-dom';
+import { Home } from './components/Home/Home';
+import { Tabs } from './components/Tabs/Tabs';
 
 import './App.scss';
 
-/*
-import { RouteComponentProps } from 'react-router-dom';
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
 
-type TabsPageProps = React.FC<RouteComponentProps<{ tabId: string }>>;
-const TabsPage: TabsPageProps = ({ match }) => {...};
-
-or
-
-import { useParams } from 'react-router-dom';
-
-const TabsPage = () => {
-  const { tabId } = useParams<{ tabId: string }>();
-  ...
-};
-*/
-
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
-
-const App = () => (
+const App: FC = () => (
   <div className="App">
-    <h1>Tabs with router</h1>
+    <nav className="menu">
+      <span><NavLink to="/">Home</NavLink></span>
+      <span><NavLink to="/tabs">Tabs</NavLink></span>
+    </nav>
+
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route
+        path="/tabs/:id?"
+        render={({ match }) => <Tabs tabs={tabs} tabId={match.params.id} />}
+      />
+    </Switch>
+
   </div>
 );
 
