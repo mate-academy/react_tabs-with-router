30c98ed5e1fde2d6d075b6abcd877916a2a99659
Oleksandr Orel <oleksandrorel@ukr.net>
2021-03-21 16:11:09 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,26 @@
 import React from 'react';
-
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { Navigation } from './components/Navigation';
+import { HomePage } from './components/HomePage';
+import { TabsPage } from './components/TabsPage';
 import './App.scss';
-
-/*
-import { RouteComponentProps } from 'react-router-dom';
-
-type TabsPageProps = React.FC<RouteComponentProps<{ tabId: string }>>;
-const TabsPage: TabsPageProps = ({ match }) => {...};
-
-or
-
-import { useParams } from 'react-router-dom';
-
-const TabsPage = () => {
-  const { tabId } = useParams<{ tabId: string }>();
-  ...
-};
-*/
-
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+import 'bulma';
 
 const App = () => (
   <div className="App">
-    <h1>Tabs with router</h1>
+    <div className="content is-normal">
+      <h1 className="header">React tabs with router</h1>
+      <Navigation />
+
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/tabs/:tabId?" component={TabsPage} />
+
+        <Redirect path="/home" to="/" />
+        <Redirect to="/tabs" />
+      </Switch>
+    </div>
+
   </div>
 );
 
