0433815b969cf2e0d08c08e51a160f3e2414bac2
Ruslan Shmadchenko <ruslanwpua@gmail.com>
2021-04-08 18:04:49 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,25 @@
 import React from 'react';
+import 'bulma';
 
 import './App.scss';
-
-/*
-import { RouteComponentProps } from 'react-router-dom';
-
-type TabsPageProps = React.FC<RouteComponentProps<{ tabId: string }>>;
-const TabsPage: TabsPageProps = ({ match }) => {...};
-
-or
-
-import { useParams } from 'react-router-dom';
-
-const TabsPage = () => {
-  const { tabId } = useParams<{ tabId: string }>();
-  ...
-};
-*/
-
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+import { Link, Route, Switch } from 'react-router-dom';
+import { TabsPage } from './TabsPage';
 
 const App = () => (
-  <div className="App">
+  <div className="main">
     <h1>Tabs with router</h1>
+    <Link to="/" className="button">Home</Link>
+    <Link to="/tabs" className="button">Tabs</Link>
+    <Switch>
+      <Route
+        path="/tabs/:tabId?"
+        component={TabsPage}
+      />
+      <Route path="/" exact>
+        <h1>Home page</h1>
+      </Route>
+    </Switch>
+
   </div>
 );
 
