bf638ceb2a2e2bad588cb11b1b437b4fa055075e
Oleksandr Vasilkov <vasilkoff1337@gmail.com>
2021-07-09 23:16:28 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,6 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 import './App.scss';
 import { TabsPage } from './TabsPage';
 import { Navigation } from './Navigation';
-/*
-import { RouteComponentProps } from 'react-router-dom';
-
-type TabsPageProps = React.FC<RouteComponentProps<{ tabId: string }>>;
-const TabsPage: TabsPageProps = ({ match }) => {...};
-
-or
-
-import { useParams } from 'react-router-dom';
-
-const TabsPage = () => {
-  const { tabId } = useParams<{ tabId: string }>();
-  ...
-};
-*/
-
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
 
 const App = () => (
   <HashRouter>
