57610613f108cfb6c369a1c61223548780561696
Tolik <tolikdolinskiy@gmail.com>
2021-07-26 13:16:18 +0300

    some message

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,29 @@
 import React from 'react';
 
 import './App.scss';
+import { Route, Switch } from 'react-router-dom';
+import { Header } from './components/Header/Header';
+import { TabsPage } from './components/TabsPage/TabsPage';
 
-/*
-import { RouteComponentProps } from 'react-router-dom';
-
-type TabsPageProps = React.FC<RouteComponentProps<{ tabId: string }>>;
-const TabsPage: TabsPageProps = ({ match }) => {...};
-
-or
-
-import { useParams } from 'react-router-dom';
-
-const TabsPage = () => {
-  const { tabId } = useParams<{ tabId: string }>();
-  ...
-};
-*/
-
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
 
 const App = () => (
   <div className="App">
-    <h1>Tabs with router</h1>
+    <Header />
+    <Switch>
+      <Route path="/" exact>
+        <h1>Home page</h1>
+      </Route>
+      <Route path="/tabs/:tabId?">
+        <TabsPage tabs={tabs} />
+      </Route>
+
+      <h2>Not found</h2>
+    </Switch>
   </div>
 );
 
