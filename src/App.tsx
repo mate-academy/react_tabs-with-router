35d958dd80d96a7fe9826bfed056fc0c14c40588
MaxStets <weawer458@gmail.com>
2021-12-05 20:46:08 +0200

    Solution -- react_tabs-with-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,20 @@
-import React from 'react';
-
+import { Routes, Route } from 'react-router-dom';
 import './App.scss';
+import { Layout } from './component/Layout';
+import { HomePage } from './component/Pages/HomePage';
+import { TabsPage } from './component/Pages/TabsPage';
 
-/*
-import { RouteComponentProps } from 'react-router-dom';
-
-type TabsPageProps = React.FC<RouteComponentProps<{ tabId: string }>>;
-const TabsPage: TabsPageProps = ({ match }) => {...};
-
-or
-
-import { useParams } from 'react-router-dom';
-
-const TabsPage = () => {
-  const { tabId } = useParams<{ tabId: string }>();
-  ...
-};
-*/
-
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
-
-const App = () => (
+const App: React.FC = () => (
   <div className="App">
-    <h1>Tabs with router</h1>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="tabs" element={<TabsPage />}>
+          <Route path=":tabId" element={<TabsPage />} />
+        </Route>
+        <Route path="*" element={<p>Page not found!</p>} />
+      </Route>
+    </Routes>
   </div>
 );
 
