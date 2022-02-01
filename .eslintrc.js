47c40a4469f2e035d5376c775c62d45ed01f8aff
Vova Alputov <alputov1998@gmail.com>
2022-02-01 17:11:32 +0200

    done

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-dupe-keys */
+/* eslint-disable max-len */
 // Uncomment eslint-config-react if you want use react without TS
 
 module.exports = {
   extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
-  // extends: ['@mate-academy/eslint-config-react', 'plugin:cypress/recommended'],
+  extends: ['@mate-academy/eslint-config-react', 'plugin:cypress/recommended'],
 };
