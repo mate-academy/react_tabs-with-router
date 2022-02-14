01771d00a9e79b387e05663abc6a0c67e3ec4481
Yevhenii Sharpylo <82882852+E-Sharpilo@users.noreply.github.com>
2022-02-14 12:19:47 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,6 @@ import { HomePage } from './components/HomePage';
 import { TabsPage } from './components/TabsPage';
 import './App.scss';
 
-/*
-import { RouteComponentProps } from 'react-router-dom';
-
-type TabsPageProps = React.FC<RouteComponentProps<{ tabId: string }>>;
-const TabsPage: TabsPageProps = ({ match }) => {...};
-
-or
-
-import { useParams } from 'react-router-dom';
-
-const TabsPage = () => {
-  const { tabId } = useParams<{ tabId: string }>();
-  ...
-};
-*/
-
 const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
