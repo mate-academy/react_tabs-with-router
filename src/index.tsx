ea76ba6339958b901630cb0e45ad50eadd2fd12e
Yevhenii Kriuchek <evhenii.kriuchek@gmail.com>
2022-03-11 21:14:30 +0200

    hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 import App from './App';
 
 ReactDOM.render(
-  <Router basename="/react_tabs-with-router/">
+  <Router>
     <App />
   </Router>,
   document.getElementById('root'),
