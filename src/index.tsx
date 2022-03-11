20fbdf5148ea4b2878439cfee56aaebaa45c8881
Yevhenii Kriuchek <evhenii.kriuchek@gmail.com>
2022-03-11 21:09:48 +0200

    basename

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <Router>
+  <Router basename="/react_tabs-with-router/">
     <App />
   </Router>,
   document.getElementById('root'),
