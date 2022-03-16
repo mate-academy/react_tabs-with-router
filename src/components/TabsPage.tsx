054a6dc3eebd1213e7a6ab1c2f6d62e073959e35
Yevhenii Kriuchek <evhenii.kriuchek@gmail.com>
2022-03-16 14:30:50 +0200

    tabspage

---
+++ src/components/TabsPage.tsx
@@ -0,0 +1,35 @@
+import classNames from 'classnames';
+import React from 'react';
+import { NavLink, useParams } from 'react-router-dom';
+
+import { Tab } from '../types/Tab';
+
+type Props = {
+  tabs: Tab[]
+};
+
+const TabsPage: React.FC<Props> = ({ tabs }) => {
+  const { tabId } = useParams<{ tabId: string }>();
+
+  const selectedTab = tabs.find(tab => tab.id === tabId);
+
+  return (
+    <>
+      <ul>
+        {tabs.map(tab => (
+          <li key={tab.id}>
+            <NavLink
+              to={tab.id}
+              className={({ isActive }) => classNames({ selected: isActive })}
+            >
+              {tab.title}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+      <p>{selectedTab ? `${selectedTab.content}` : 'Please select a tab'}</p>
+    </>
+  );
+};
+
+export default TabsPage;
