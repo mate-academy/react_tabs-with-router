785f5c70080147e1f18537efad42f7231bb8c18d
Max Shlikhta <maxok.vibe@gmail.com>
2022-04-19 00:10:03 +0300

    Tabs with router

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,19 @@
 import './App.scss';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { MainNavigation } from './components/MainNavigation/MainNavigation';
+import { Tabs } from './components/Tabs/Tabs';
 
-/*
-import { RouteComponentProps } from 'react-router-dom';
-
-type TabsPageProps = React.FC<RouteComponentProps<{ tabId: string }>>;
-const TabsPage: TabsPageProps = ({ match }) => {...};
-
-or
-
-import { useParams } from 'react-router-dom';
-
-const TabsPage = () => {
-  const { tabId } = useParams<{ tabId: string }>();
-  ...
-};
-*/
-
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
-
-const App = () => (
-  <div className="App">
-    <h1>Tabs with router</h1>
+export const App = () => (
+  <div className="app">
+    <MainNavigation />
+    <Routes>
+      <Route path="/" element={<h2 className="app__title">Home page</h2>} />
+      <Route path="/home" element={<Navigate to="/" />} />
+      <Route path="/tabs/*" element={<Tabs />} />
+      <Route
+        path="*"
+        element={<h3 className="app__title">Page not found</h3>}
+      />
+    </Routes>
   </div>
 );
-
-export default App;
---
+++ src/data/tabs.ts
@@ -0,0 +1,22 @@
+export const tabs: Tab[] = [
+  {
+    id: 'OnePlus',
+    title: 'OnePlus',
+    // eslint-disable-next-line max-len
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate. Etiam consequat aliquam cursus. In sodales pretium ultrices. Maecenas lectus est, sollicitudin consectetur felis nec, feugiat ultricies mi.',
+  },
+
+  {
+    id: 'Samsung',
+    title: 'Samsung',
+    // eslint-disable-next-line max-len
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores perspiciatis repellat soluta dolorum, quam quos possimus atque rerum porro voluptate beatae dolor incidunt! Corporis, tempore quasi fugit est. Ex, quae!Aliquam nulla explicabo facilis, consequuntur tenetur! Rem architecto veritatis quo corporis sapiente nesciunt culpa at enim similique officiis adipisci in commodi suscipit, natus facilis, repellat laboriosam eaque obcaecati quaerat vero!',
+  },
+
+  {
+    id: 'Apple',
+    title: 'Apple',
+    // eslint-disable-next-line max-len
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem minus similique architecto sequi delectus non, nobis consequuntur officia, laboriosam reiciendis ea! Natus iste quas perspiciatis magnam repellat, voluptate excepturi esse.',
+  },
+];
