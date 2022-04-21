c743a2fbbc023e90f57b45512330a9d80b14a9e7
Olga Bordiug <obordiug@gmail.com>
2022-04-21 23:08:17 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,41 @@
 import './App.scss';
+import { Routes, Route, NavLink } from 'react-router-dom';
+import { TabsPage } from './components/TabsPage';
 
-/*
-import { RouteComponentProps } from 'react-router-dom';
-
-type TabsPageProps = React.FC<RouteComponentProps<{ tabId: string }>>;
-const TabsPage: TabsPageProps = ({ match }) => {...};
-
-or
-
-import { useParams } from 'react-router-dom';
-
-const TabsPage = () => {
-  const { tabId } = useParams<{ tabId: string }>();
-  ...
-};
-*/
-
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
 
 const App = () => (
   <div className="App">
-    <h1>Tabs with router</h1>
+    <nav className="nav">
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive
+          ? 'nav__link nav__link--active'
+          : 'nav__link'
+        )}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/tabs"
+        className={({ isActive }) => (isActive
+          ? 'nav__link nav__link--active'
+          : 'nav__link'
+        )}
+      >
+        Tabs
+      </NavLink>
+    </nav>
+    <Routes>
+      <Route path="/" element={<h1>Home page</h1>} />
+      <Route path="/tabs" element={<TabsPage tabs={tabs} />}>
+        <Route path=":tabId" element={<TabsPage tabs={tabs} />} />
+      </Route>
+    </Routes>
   </div>
 );
 
