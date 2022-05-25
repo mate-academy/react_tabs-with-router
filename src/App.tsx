1d564bfdfcf032b0d0557dbb00889ea059d93a62
Maksym Yaremenko <restartermord123@ukr.net>
2022-05-25 19:32:15 +0300

    navigation fix

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App: FC = () => {
       <Navigation />
 
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
 
         <Route path="/cards">
           <Route
@@ -37,7 +37,7 @@ const App: FC = () => {
           />
 
           <Route
-            path="*"
+            path="info"
             element={(
               <Tabs
                 tabs={tabs}
