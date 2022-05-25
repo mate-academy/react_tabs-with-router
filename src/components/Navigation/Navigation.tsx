20e4915f06b7c4c934dd00cd3b4176b8a29b7cf9
Maksym Yaremenko <restartermord123@ukr.net>
2022-05-25 18:50:42 +0300

    navigation fix

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import classNames from 'classnames';
 
 import './Navigation.scss';
 
 export const Navigation = React.memo(() => {
+  const [selectedLink, setSelectedLink] = useState('');
+
   return (
     <div className="navigation">
       <NavLink
         to="/"
-        className="navigation__link"
+        className={classNames(
+          'navigation__link',
+          { active: selectedLink === 'homepage' },
+        )}
+        onClick={() => setSelectedLink('homepage')}
       >
         Home
       </NavLink>
 
       <NavLink
         to="/cards/info"
-        className="navigation__link"
+        className={classNames(
+          'navigation__link',
+          { active: selectedLink === 'cards' },
+        )}
+        onClick={() => setSelectedLink('cards')}
       >
         Tabs
       </NavLink>
