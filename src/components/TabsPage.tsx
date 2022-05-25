8d54de7a72a749d0fe425b9f6185d79cb4d58e1d
Oksana Tyshchenko <oksanasergeevna30@gmail.com>
2022-05-25 18:14:58 +0300

    Some changes

--- src/components/TabsPage.tsx
+++ src/components/TabsPage.tsx
@@ -10,24 +10,27 @@ export const TabsPage: React.FC<Props> = ({ tabs }) => {
   const { tabId } = useParams<{ tabId: string }>();
   const search = tabs.find(tab => tab.id === tabId);
 
-  if (!tabId || !search) {
-    return <p className="Tab__message title is-2">Please select a tab</p>;
-  }
-
   return (
-    <ul className="Tab__list">
-      {tabs.map((tab) => (
-        <li key={tab.id}>
-          <NavLink
-            to={`/tabs/${tab.id}`}
-            className="button"
-            activeClassName="is-active"
-          >
-            {tab.title}
-          </NavLink>
-          <p className="Tab__content">{tabId === tab.id ? tab.content : ''}</p>
-        </li>
-      ))}
-    </ul>
+    <div className="Tab__section">
+      <h2 className="title is-2">
+        {!tabId || !search ? 'Please select a tab' : ''}
+      </h2>
+      <ul className="Tab__list">
+        {tabs.map((tab) => (
+          <li key={tab.id}>
+            <NavLink
+              to={`/tabs/${tab.id}`}
+              className="button"
+              activeClassName="is-active"
+            >
+              {tab.title}
+            </NavLink>
+            <p className="Tab__content">
+              {tabId === tab.id ? tab.content : ''}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
