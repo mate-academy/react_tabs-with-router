3b8bd86cf11793cd688850135dd93774b132ffeb
Anastasiia Svintsova <svintsova.nastya@gmail.com>
2022-05-27 14:03:36 +0300

    tabs with router were created

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,38 @@
+import { FC } from 'react';
+import { NavLink, Route, Routes } from 'react-router-dom';
 import './App.scss';
+import { HomePage } from './components/HomePage/HomePage';
+import { TabsPage } from './components/TabsPage/TabsPage';
+import 'bulma';
+import { NotFoundPage } from './components/NotFoundPage/NotFoundPage';
 
-/*
-import { RouteComponentProps } from 'react-router-dom';
-
-type TabsPageProps = React.FC<RouteComponentProps<{ tabId: string }>>;
-const TabsPage: TabsPageProps = ({ match }) => {...};
-
-or
-
-import { useParams } from 'react-router-dom';
-
-const TabsPage = () => {
-  const { tabId } = useParams<{ tabId: string }>();
-  ...
-};
-*/
-
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
-
-const App = () => (
+const App: FC = () => (
   <div className="App">
-    <h1>Tabs with router</h1>
+    <div className="navbar has-shadow">
+      <nav className="navbar-brand">
+        <NavLink
+          to="/"
+          className={({ isActive }) => (
+            isActive ? 'navbar-item--active navbar-item' : 'navbar-item')}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/tabs"
+          className={({ isActive }) => (
+            isActive ? 'navbar-item--active navbar-item' : 'navbar-item')}
+        >
+          Tabs
+        </NavLink>
+      </nav>
+    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/tabs" element={<TabsPage />}>
+        <Route path=":tabId" />
+      </Route>
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   </div>
 );
 
