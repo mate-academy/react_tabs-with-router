2de525f1c849b2c081a5d633e8918a869a13bc1c
pogrebnyakp <pogrebnyakpavel@gmail.com>
2022-05-27 11:56:39 +0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,50 @@
+import React from 'react';
+import {
+  NavLink,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+
 import './App.scss';
+import { HomePage } from './components/HomePage';
+import { TabsPage } from './components/TabsPage';
 
-/*
-import { RouteComponentProps } from 'react-router-dom';
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
 
-type TabsPageProps = React.FC<RouteComponentProps<{ tabId: string }>>;
-const TabsPage: TabsPageProps = ({ match }) => {...};
-
-or
-
-import { useParams } from 'react-router-dom';
-
-const TabsPage = () => {
-  const { tabId } = useParams<{ tabId: string }>();
-  ...
-};
-*/
-
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
-
-const App = () => (
+const App: React.FC = () => (
   <div className="App">
-    <h1>Tabs with router</h1>
+    <div className="container">
+      <nav className="navbar is-warning">
+        <NavLink to="/" className="navbar-item">Home page</NavLink>
+        <NavLink to="/tabs" className="navbar-item">Tabs page</NavLink>
+      </nav>
+    </div>
+
+    <section className="section">
+      <div className="container">
+        <div className="columns">
+          <div className="column">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="home" element={<Navigate to="/" />} />
+              <Route path="tabs" element={<TabsPage tabs={tabs} />} />
+              <Route path="tabs/:tabId" element={<TabsPage tabs={tabs} />} />
+              <Route
+                path="*"
+                element={
+                  <p>Page not found</p>
+                }
+              />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 );
 
