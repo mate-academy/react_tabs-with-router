05653a277986132600bc3a92c36670ea507edb12
pogrebnyakp <pogrebnyakpavel@gmail.com>
2022-05-27 09:37:18 +0300

    tabs page

---
+++ src/components/TabsPage.tsx
@@ -0,0 +1,46 @@
+import { useParams } from 'react-router-dom';
+import classnames from 'classnames';
+
+type Props = {
+  tabs: Tab[],
+};
+
+export const TabsPage: React.FC<Props> = ({ tabs }) => {
+  const { tabId } = useParams<{ tabId: string }>();
+
+  return (
+    <>
+      <h1 className="title">
+        Selected tab is&nbsp;
+        {tabs.find(tab => tab.id === tabId)?.title}
+      </h1>
+
+      <div className="container">
+        <div className="tabs">
+          <ul>
+            {tabs.map(tab => (
+              <li
+                className={classnames({ 'is-active': tabId === tab.id })}
+                key={tab.id}
+              >
+                <a
+                  href={`#/tabs/${tab.id}`}
+                  className="link"
+                >
+                  {tab.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="tabcontent">
+          <p>
+            {tabs.find(tab => tab.id === tabId)?.content
+              || 'Please select a tab'}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,8 +1,8 @@
 // eslint-disable-next-line
 /// <reference types="react-scripts" />
 
-type Tab = {
-  id: number,
+interface Tab {
+  id: string,
   title: string,
   content: string,
-};
+}
