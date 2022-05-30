a4e2ac0042345f633889900afe6be9e20ac507cd
yurii837 <gorogotskiy@gmail.com>
2022-05-30 12:33:27 +0300

    create components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
 import './App.scss';
+import {
+  Link, Redirect, Route, Switch,
+} from 'react-router-dom';
+import { useState } from 'react';
+import { HomePage } from './Components/HomePage/HomePage';
+import { NotFoundPage } from './Components/NotFoundPage/NotFoundPage';
+import { TabsPage } from './Components/TabsPage/TabsPage';
 
 /*
 import { RouteComponentProps } from 'react-router-dom';
@@ -16,16 +23,44 @@ const TabsPage = () => {
 };
 */
 
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+const tabs: Tab[] = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
 
-const App = () => (
-  <div className="App">
-    <h1>Tabs with router</h1>
-  </div>
-);
+const App: React.FC = () => {
+  const [selectedTab, setSelectedTab] = useState(tabs[0] || {});
+
+  const onTabSelected = (newTab: Tab) => {
+    if (newTab.id !== selectedTab.id) {
+      setSelectedTab(newTab);
+    }
+  };
+
+  return (
+    <div className="App">
+      <Link to="/" className="nav__link">
+        Home page
+      </Link>
+      <Link to="/tabs" className="nav__link">
+        Tabs
+      </Link>
+
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/tabs">
+          <TabsPage
+            tabs={tabs}
+            selectedTabId={selectedTab.id}
+            tabChangeMethod={onTabSelected}
+          />
+        </Route>
+        <Redirect path="/home" to="/" />
+        <NotFoundPage />
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,5 @@
-// eslint-disable-next-line
-/// <reference types="react-scripts" />
+interface Tab {
+  id: string,
+  title: string,
+  content: string,
+}
