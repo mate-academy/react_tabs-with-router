e6a5f69b136724ddb57ef68dc9a2ff6d49ba3f85
Viacheslav Petrovskyi <v.petrovskyi@icloud.com>
2022-05-30 17:45:41 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const tabs = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App: React.FC = () => (
+const App = () => (
   <div className="App">
     <nav className="nav">
       <Link className="nav__link" to="/">Home</Link>
