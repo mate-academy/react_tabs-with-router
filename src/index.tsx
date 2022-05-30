ae8fb81856dc1a99d559fe5abc903aac1e932204
Ruslana Tomnyuk <rusia.vasia1@gmail.com>
2022-05-30 10:51:31 +0300

    possible solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
-import { BrowserRouter as HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import 'bulma/css/bulma.min.css';
 
 import App from './App';
