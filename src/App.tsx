520cdb82ce82c63b60934c1ff90f51d799a053f5
Anton Vakoliuk <antonvakolyuk2001@gmail.com>
2022-05-30 17:37:33 +0200

    Deleted comments

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,6 @@ import { Route, Switch, NavLink } from 'react-router-dom';
 import { HomePage } from './components/HomePage';
 import { TabsPage } from './components/TabsPage';
 
-/*
-import { RouteComponentProps } from 'react-router-dom';
-
-type TabsPageProps = React.FC<RouteComponentProps<{ tabId: string }>>;
-const TabsPage: TabsPageProps = ({ match }) => {...};
-
-or
-
-import { useParams } from 'react-router-dom';
-
-const TabsPage = () => {
-  const { tabId } = useParams<{ tabId: string }>();
-  ...
-};
-*/
-
 const App: React.FC = () => (
   <div className="App">
     <header className="header">
