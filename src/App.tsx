8ed30be2515d35813ec5f1cd6a70f4ccb96e6816
Ruslan Hubanov <ruslangubanov88@gmail.com>
2022-05-31 11:16:33 +0300

    add HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
+import { HomePage } from './components/HomePage/HomePage';
 import { TabsPage } from './components/TabsPage/TabsPage';
 import { MainNavigation } from './components/MainNavigation/MainNavigation';
 
@@ -10,6 +11,7 @@ const App: React.FC = () => (
   <div className="App">
     <MainNavigation />
     <Switch>
+      <Route path="/" exact component={HomePage} />
       <Route path="/tabs/:tabId?" component={TabsPage} />
       <Redirect to="/" from="/home" />
     </Switch>
