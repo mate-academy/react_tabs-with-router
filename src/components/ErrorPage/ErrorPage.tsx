0a6bad9b81f58670f6d55dad88727c9c0246bc5a
SeKaM0 <polozhentsew02@gmail.com>
2022-05-31 07:12:51 +0300

    Finish task

---
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,7 @@
+export const ErrorPage = () => {
+  return (
+    <h2>
+      Page not found
+    </h2>
+  );
+};
---
+++ src/components/Tabs/Tabs.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import classNames from 'classnames';
+
+import './Tabs.scss';
+
+type Props = {
+  tabs: Tab[]
+};
+
+export const Tabs: React.FC<Props> = ({ tabs }) => {
+  const { tabId } = useParams();
+  const [selectedTab, setCurrentTab] = useState<Tab | null>(null);
+  const [isIdValid, setIsIdValid] = useState(false);
+
+  useEffect(() => {
+    const tab = tabs.find(t => t.id === tabId) || null;
+
+    setCurrentTab(tab);
+
+    return tab ? setIsIdValid(true) : setIsIdValid(false);
+  }, [tabId]);
+
+  return (
+    <div className="tabs">
+      {tabs.map(tab => (
+        <Link
+          key={tab.id}
+          to={`/tabs/${tab.id}`}
+          className={classNames('tab__button',
+            { 'tab__button-active': tab.id === selectedTab?.id })}
+        >
+          {tab.title}
+        </Link>
+      ))}
+
+      {!isIdValid && <p>Please select a tab</p>}
+
+      {selectedTab && (
+        <div className="tab__content">
+          {selectedTab.title}
+        </div>
+      )}
+    </div>
+  );
+};
