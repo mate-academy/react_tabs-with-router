b89cfd87b8549cfd03e501c237518952a43e928b
Roman Pavlenko <proman9973@gmail.com>
2022-06-22 14:27:06 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
 import './App.scss';
+import { Tabs } from './components/Tabs/Tabs';
 
 /*
 import { RouteComponentProps } from 'react-router-dom';
@@ -16,16 +18,57 @@ const TabsPage = () => {
 };
 */
 
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
 
-const App = () => (
-  <div className="App">
-    <h1>Tabs with router</h1>
-  </div>
-);
+const App : React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Tabs with router</h1>
+      <nav>
+        <NavLink
+          className={({ isActive }) => (isActive ? 'active' : undefined)}
+          to="/tabs"
+        >
+          Tabs
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? 'active' : undefined)}
+          to="/"
+        >
+          Home
+        </NavLink>
+      </nav>
+      <Routes>
+        <Route
+          path="/tabs"
+          element={(
+            <>
+              <p>
+                Please select a tab
+              </p>
+              <Tabs tabs={tabs} />
+            </>
+          )}
+        />
+        <Route
+          path="/tabs/:tabId"
+          element={
+            <Tabs tabs={tabs} />
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <p>page not found</p>
+          }
+        />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
