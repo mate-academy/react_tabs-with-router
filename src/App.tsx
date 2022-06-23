70bdb8ae0d9210c3ab876b8f8d40a98d74edfd10
unknown <avicostov2@gmail.com>
2022-06-23 21:54:59 +0200

    solut

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,53 @@
 import './App.scss';
 
-/*
-import { RouteComponentProps } from 'react-router-dom';
+import {
+  Routes, Route, Navigate, NavLink, useParams,
+} from 'react-router-dom';
+import { Header } from './components/Header';
 
-type TabsPageProps = React.FC<RouteComponentProps<{ tabId: string }>>;
-const TabsPage: TabsPageProps = ({ match }) => {...};
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
 
-or
-
-import { useParams } from 'react-router-dom';
-
-const TabsPage = () => {
+const TabsPage: React.FC = () => {
   const { tabId } = useParams<{ tabId: string }>();
-  ...
-};
-*/
 
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+  return (
+    <div className="tabs">
+      <ul>
+        {tabs.map(tab => (
+          <li key={tab.id}>
+            <NavLink
+              to={`/tabs/${tab.id}`}
+              style={
+                ({ isActive }) => (isActive ? { color: 'black' }
+                  : { color: 'gray' })
+              }
+            >
+              {tab.title}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+      {tabId && (
+        tabs.find(tab => tab.id === tabId)?.content
+      )}
+    </div>
+  );
+};
 
 const App = () => (
   <div className="App">
-    <h1>Tabs with router</h1>
+    <Header />
+
+    <Routes>
+      <Route path="/home" element={<h1>Home</h1>} />
+      <Route path="/" element={<Navigate to="/home" replace />} />
+      <Route path="/tabs" element={<TabsPage />} />
+      <Route path="/tabs/:tabId" element={<TabsPage />} />
+    </Routes>
   </div>
 );
 
