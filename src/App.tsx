7a5ae67c858c1727c89ef1e1cb4d5750737118c9
Andrii Yelnikov <whiteyelnikov@gmail.com>
2022-07-06 22:13:27 +0300

    replaced link with navlink

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
+import { NavLink, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import TabsPage from './TabsPage';
 
 const App: React.FC = () => {
-  const [linkIsActive, setLinkIsActive] = useState('home');
-
   return (
     <div className="App">
       <nav>
-        <Link
-          onClick={() => {
-            setLinkIsActive('home');
-          }}
-          className={linkIsActive === 'home' ? 'link link--active' : 'link'}
+        <NavLink
+          className="link"
           to="/"
         >
           Home page
-        </Link>
+        </NavLink>
 
-        <Link
-          onClick={() => {
-            setLinkIsActive('tabs');
-          }}
-          className={linkIsActive === 'tabs' ? 'link link--active' : 'link'}
+        <NavLink
+          className="link"
           to="/tabs"
         >
           Tabs page
-        </Link>
+        </NavLink>
       </nav>
       <div>
         <Routes>
