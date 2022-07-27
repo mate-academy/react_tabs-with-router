6e3565bcdacde5131641eaa0d6d7a7ee37367c67
mira1zu <mira1zu733@gmail.com>
2022-07-27 14:54:32 +0200

    Routed tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,38 @@
+import React from 'react';
+import { Routes, Route } from 'react-router';
+
 import './App.scss';
 
-/*
-import { RouteComponentProps } from 'react-router-dom';
+import Nav from './components/Nav';
+import HomePage from './components/HomePage';
+import TabsPage from './components/TabsPage';
+import NotFoundPage from './components/NotFoundPage';
 
-type TabsPageProps = React.FC<RouteComponentProps<{ tabId: string }>>;
-const TabsPage: TabsPageProps = ({ match }) => {...};
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
 
-or
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Nav />}>
+            <Route index element={<HomePage />} />
 
-import { useParams } from 'react-router-dom';
+            <Route path="/tabs">
+              <Route index element={<TabsPage tabs={tabs} />} />
+              <Route path=":tabId" element={<TabsPage tabs={tabs} />} />
+            </Route>
+          </Route>
 
-const TabsPage = () => {
-  const { tabId } = useParams<{ tabId: string }>();
-  ...
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
-*/
-
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
-
-const App = () => (
-  <div className="App">
-    <h1>Tabs with router</h1>
-  </div>
-);
 
 export default App;
