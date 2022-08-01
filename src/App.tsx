74a119ada830eca9afef867a56e19cf913e056a5
SpaceHook <maxdumiak39@gmail.com>
2022-08-01 15:04:08 +0300

    React tabs /w router - ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,48 @@
+import {
+  Navigate, Route, Routes,
+} from 'react-router-dom';
 import './App.scss';
+import { Header } from './components/Header/Header';
+import { HomePage } from './components/HomePage/HomePage';
+import { PageTabs } from './components/TapsPage/TabsPage';
 
-/*
-import { RouteComponentProps } from 'react-router-dom';
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
 
-type TabsPageProps = React.FC<RouteComponentProps<{ tabId: string }>>;
-const TabsPage: TabsPageProps = ({ match }) => {...};
+const App: React.FC = () => {
+  return (
+    <div className="app container is-desktop">
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<Navigate to="/" />} />
+        <Route
+          path="/tabs"
+          element={
+            (
+              <PageTabs
+                tabs={tabs}
+              />
+            )
+          }
+        />
 
-or
-
-import { useParams } from 'react-router-dom';
-
-const TabsPage = () => {
-  const { tabId } = useParams<{ tabId: string }>();
-  ...
+        <Route
+          path="/tabs/:tabId"
+          element={
+            (
+              <PageTabs
+                tabs={tabs}
+              />
+            )
+          }
+        />
+      </Routes>
+    </div>
+  );
 };
-*/
-
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
-
-const App = () => (
-  <div className="App">
-    <h1>Tabs with router</h1>
-  </div>
-);
 
 export default App;
---
+++ src/components/TapsPage/TabsPage.tsx
@@ -0,0 +1,43 @@
+import classNames from 'classnames';
+import React from 'react';
+import { NavLink, useParams } from 'react-router-dom';
+import { Tab } from '../../types/Tab';
+
+type Props = {
+  tabs: Tab[],
+};
+
+export const PageTabs: React.FC<Props> = ({ tabs }) => {
+  const { tabId } = useParams();
+  const selectedTab = tabs.find(tab => tab.id === tabId);
+
+  return (
+    <>
+      <h1 className="title">
+        {selectedTab ? `Selected tab is ${selectedTab.title}` : 'Choose a tab'}
+      </h1>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={classNames('', {
+                'is-active': tab.id === selectedTab?.id,
+              })}
+              key={tab.id}
+            >
+              <NavLink
+                to={`/tabs/${tab.id}`}
+              >
+                {tab.title}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="tab-content">
+        {selectedTab?.content}
+      </div>
+    </>
+  );
+};
