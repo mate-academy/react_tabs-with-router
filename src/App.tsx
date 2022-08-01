3fee6f242717acba2455cdedfed023c55481d960
mhrynko <glukym@gmail.com>
2022-08-01 20:00:50 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,57 @@
-import './App.scss';
+import classNames from 'classnames';
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 
-/*
-import { RouteComponentProps } from 'react-router-dom';
+import {
+  Navigate,
+  NavLink,
+  Route,
+  Routes,
+} from 'react-router-dom';
 
-type TabsPageProps = React.FC<RouteComponentProps<{ tabId: string }>>;
-const TabsPage: TabsPageProps = ({ match }) => {...};
-
-or
-
-import { useParams } from 'react-router-dom';
-
-const TabsPage = () => {
-  const { tabId } = useParams<{ tabId: string }>();
-  ...
-};
-*/
-
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+import { TabsPage } from './components/TabsPage';
 
 const App = () => (
-  <div className="App">
-    <h1>Tabs with router</h1>
-  </div>
+  <>
+    {/* Also requires <html class="has-navbar-fixed-top"> */}
+    <nav className="navbar is-fixed-top has-background-light" data-cy="nav">
+      <div className="navbar-menu">
+        <div className="navbar-start">
+          <NavLink
+            to="/"
+            className={({ isActive }) => classNames('navbar-item', {
+              'is-active': isActive,
+            })}
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            to="tabs"
+            className={({ isActive }) => classNames('navbar-item', {
+              'is-active': isActive,
+            })}
+          >
+            Tabs
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+
+    <div className="section">
+      <Routes>
+        <Route path="/" element={<h1 className="title">Home page</h1>} />
+        <Route path="/home" element={<Navigate to="/" />} />
+
+        <Route path="tabs">
+          <Route index element={<TabsPage />} />
+          <Route path=":tabId" element={<TabsPage />} />
+        </Route>
+
+        <Route path="*" element={<h1 className="title">Page not found</h1>} />
+      </Routes>
+    </div>
+  </>
 );
 
 export default App;
