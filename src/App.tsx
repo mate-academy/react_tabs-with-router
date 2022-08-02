8dadb8baad1be814ce235c50a9e0fcbd1da16160
Stas Yasinchyk <grodh06@gmail.com>
2022-08-02 19:50:45 +0300

    add callback for navlinks and add index route

--- src/App.tsx
+++ src/App.tsx
@@ -14,49 +14,46 @@ const tabs = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const App = () => (
-  <>
-    {/* Also requires <html class="has-navbar-fixed-top"> */}
-    <nav className="navbar is-fixed-top has-background-light" data-cy="nav">
-      <div className="navbar-menu">
-        <div className="navbar-start">
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive
-              ? 'navbar-item is-active'
-              : 'navbar-item')}
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="tabs"
-            className={({ isActive }) => (isActive
-              ? 'navbar-item is-active'
-              : 'navbar-item')}
-          >
-            Tabs
-          </NavLink>
+const App = () => {
+  const getNavLinkClass = ({ isActive }: { isActive: boolean }) => {
+    return `navbar-item ${isActive ? 'is-active' : ''}`;
+  };
+
+  return (
+    <>
+      {/* Also requires <html class="has-navbar-fixed-top"> */}
+      <nav className="navbar is-fixed-top has-background-light" data-cy="nav">
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            <NavLink
+              to="/"
+              className={getNavLinkClass}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="tabs"
+              className={getNavLinkClass}
+            >
+              Tabs
+            </NavLink>
+          </div>
         </div>
+      </nav>
+
+      <div className="section">
+        <Routes>
+          <Route path="/" element={<h1 className="title">Home page</h1>} />
+          <Route path="/home" element={<Navigate to="/" />} />
+          <Route path="/tabs">
+            <Route index element={(<Tabs tabs={tabs} />)} />
+            <Route path=":tabId" element={<Tabs tabs={tabs} />} />
+          </Route>
+          <Route path="*" element={<h1 className="title">Page not found</h1>} />
+        </Routes>
       </div>
-    </nav>
-
-    <div className="section">
-      <Routes>
-        <Route path="/" element={<h1 className="title">Home page</h1>} />
-        <Route path="/home" element={<Navigate to="/" />} />
-        <Route
-          path="/tabs"
-          element={(
-            <Tabs tabs={tabs} />
-          )}
-        >
-          <Route path=":tabId" element={<Tabs tabs={tabs} />} />
-        </Route>
-
-        <Route path="*" element={<h1 className="title">Page not found</h1>} />
-      </Routes>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default App;
