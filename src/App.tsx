7f351bfeeec56dbf80047d6cc91ee651a1114887
Andrew Khorsun <andrewkhorsun@gmail.com>
2022-08-02 13:25:52 +0300

    TABS Routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,47 @@
+import React from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-import { Link, Route, Routes } from 'react-router-dom';
+import {
+  Navigate, NavLink, Route, Routes,
+} from 'react-router-dom';
+import { TabsPage } from './components/TabsPage';
 
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+const App: React.FC = () => {
+  const activeClass = ({ isActive }: { isActive: boolean }) => {
+    return (
+      isActive
+        ? 'navbar-item isActive'
+        : 'navbar-item'
+    );
+  };
 
-const App = () => (
-  <>
-    {/* Also requires <html class="has-navbar-fixed-top"> */}
-    <nav className="navbar is-fixed-top has-background-light" data-cy="nav">
-      <div className="navbar-menu">
-        <div className="navbar-start">
-          <Link to="/" className="navbar-item isActive">Home</Link>
-          <Link to="/tabs" className="navbar-item isActive">Tabs</Link>
+  return (
+    <>
+      {/* Also requires <html class="has-navbar-fixed-top"> */}
+      <nav className="navbar is-fixed-top has-background-light" data-cy="nav">
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            <NavLink to="/" className={activeClass}>Home</NavLink>
+            <NavLink to="/tabs" className={activeClass}>Tabs</NavLink>
+          </div>
         </div>
+      </nav>
+
+      <div className="section">
+
+        <Routes>
+          <Route path="/" element={<h1 className="title">Home page</h1>} />
+          <Route path="*" element={<h1 className="title">Page not found</h1>} />
+          <Route path="home" element={<Navigate to="/" />} />
+          <Route path="tabs">
+            <Route index element={<TabsPage />} />
+            <Route path=":tabID" element={<TabsPage />} />
+          </Route>
+        </Routes>
+
       </div>
-    </nav>
-
-    <div className="section">
-
-      <Routes>
-        <Route path="/" element={<h1 className="title">Home page</h1>} />
-        <Route path="/tabs" element={<h1 className="title">Tabs page</h1>} />
-        <Route path="*" element={<h1 className="title">Page not found</h1>} />
-      </Routes>
-
-      <div className="tabs is-boxed">
-        <ul>
-          <li data-cy="tab" className="is-active">
-            <a href="#/">Tab 1</a>
-          </li>
-          <li data-cy="tab">
-            <a href="#/">Tab 2</a>
-          </li>
-          <li data-cy="tab">
-            <a href="#/">Tab 3</a>
-          </li>
-        </ul>
-      </div>
-
-      <div className="block" data-cy="tab-content">
-        Please select a tab
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default App;
