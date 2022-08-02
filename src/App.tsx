28ca9ddb1a6db42e5d0e2850e407d59815850ee6
Oleksandr Kapustei <angelion541@gmail.com>
2022-08-02 14:00:46 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,67 @@
+import React from 'react';
+import {
+  NavLink,
+  Navigate,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import classNames from 'classnames';
+
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+import { TabsPage } from './TabsPage';
 
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+export const App: React.FC = () => {
+  const getLinkClass = ({ isActive }: { isActive: boolean }): string => {
+    return isActive ? 'is-active' : '';
+  };
 
-const App = () => (
-  <>
-    {/* Also requires <html class="has-navbar-fixed-top"> */}
-    <nav className="navbar is-fixed-top has-background-light" data-cy="nav">
-      <div className="navbar-menu">
-        <div className="navbar-start">
-          <a href="/" className="navbar-item isActive">Home</a>
-          <a href="/tabs" className="navbar-item isActive">Tabs</a>
-        </div>
-      </div>
-    </nav>
+  return (
+    <div>
+      <section
+        className="
+          navbar
+          navbar-start
+          is-fixed-top
+          has-background-light
+        "
+        data-cy="nav"
+      >
+        <article className="navbar-menu">
+          <NavLink
+            to="/"
+            className={param => classNames(
+              'navbar-item',
+              getLinkClass(param),
+            )}
+          >
+            Home
+          </NavLink>
 
-    <div className="section">
-      <h1 className="title">Home page</h1>
-      <h1 className="title">Tabs page</h1>
-      <h1 className="title">Page not found</h1>
+          <NavLink
+            to="/tabs"
+            className={param => classNames(
+              'navbar-item',
+              getLinkClass(param),
+            )}
+          >
+            Tabs
+          </NavLink>
+        </article>
+      </section>
 
-      <div className="tabs is-boxed">
-        <ul>
-          <li data-cy="tab" className="is-active">
-            <a href="#/">Tab 1</a>
-          </li>
-          <li data-cy="tab">
-            <a href="#/">Tab 2</a>
-          </li>
-          <li data-cy="tab">
-            <a href="#/">Tab 3</a>
-          </li>
-        </ul>
-      </div>
-
-      <div className="block" data-cy="tab-content">
-        Please select a tab
-      </div>
+      <section className="section">
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="tabs">
+            <Route index element={<TabsPage />} />
+            <Route path=":tabId" element={<TabsPage />} />
+          </Route>
+          <Route path="*" element={<h1>Page not found</h1>} />
+          <Route path="/home" element={<Navigate to="/" />} />
+        </Routes>
+      </section>
     </div>
-  </>
-);
-
-export default App;
+  );
+};
