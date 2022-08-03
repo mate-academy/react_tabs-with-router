14831a8dcade9748f1152026e1e182e3f6c92490
Ihor Kuchin <vedunigor@gmail.com>
2022-08-03 20:38:38 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,43 @@
-import './App.scss';
+import { NavLink, Routes, Route } from 'react-router-dom';
+import { HomePage } from './components/HomePage/HomePage';
+import { TabsPage } from './components/TabsPage/TabsPage';
+import 'bulma/css/bulma.css';
 
-/*
-import { RouteComponentProps } from 'react-router-dom';
-
-type TabsPageProps = React.FC<RouteComponentProps<{ tabId: string }>>;
-const TabsPage: TabsPageProps = ({ match }) => {...};
-
-or
-
-import { useParams } from 'react-router-dom';
-
-const TabsPage = () => {
-  const { tabId } = useParams<{ tabId: string }>();
-  ...
-};
-*/
-
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
 
 const App = () => (
-  <div className="App">
-    <h1>Tabs with router</h1>
+  <div className="App container">
+    <nav className="navbar">
+      <NavLink
+        className={({ isActive }) => (
+          `navbar-item is-tab ${isActive ? ' is-active' : ''}`
+        )}
+        to="/"
+      >
+        Home Page
+      </NavLink>
+
+      <NavLink
+        className={({ isActive }) => (
+          `navbar-item is-tab ${isActive ? ' is-active' : ''}`
+        )}
+        to="/tabs"
+      >
+        Tabs
+      </NavLink>
+    </nav>
+
+    <main>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="tabs/:tabId" element={<TabsPage tabs={tabs} />} />
+        <Route path="/tabs" element={<TabsPage tabs={tabs} />} />
+      </Routes>
+    </main>
   </div>
 );
 
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,8 @@
 // eslint-disable-next-line
 /// <reference types="react-scripts" />
+
+export interface Tabs {
+  id: string,
+  title: string,
+  content: string,
+}
