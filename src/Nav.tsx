bd96387848d9bccfaaffbeb03c8b4ee93bd5ca0e
Roman Marunkevich <vinnibea@gmail.com>
2022-08-03 22:34:58 +0300

    solution

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import classnames from 'classnames';
+
+const activeClass = (isActive: boolean) => {
+  return classnames('navbar-item', {
+    isActive,
+  });
+};
 
 export const Nav: React.FC = () => (
   <nav className="navbar is-fixed-top has-background-light" data-cy="nav">
     <div className="navbar-menu">
       <div className="navbar-start">
-        <NavLink to="/home" className="navbar-item isActive">Home</NavLink>
-        <NavLink to="tabs" className="navbar-item isActive">Tabs</NavLink>
+        <NavLink
+          to="/home"
+          className={({ isActive }) => activeClass(isActive)}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/tabs"
+          className={({ isActive }) => activeClass(isActive)}
+        >
+          Tabs
+        </NavLink>
       </div>
     </div>
   </nav>
