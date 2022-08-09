72f792d0d85ef0c015a8d52d651603c33507df54
Dima Pyvovarchuk <darkfix137@gmail.com>
2022-08-09 17:26:24 +0300

    solution

---
+++ src/components/TabsPage/TabsPage.tsx
@@ -0,0 +1,42 @@
+import classNames from 'classnames';
+import React from 'react';
+import { NavLink, useParams } from 'react-router-dom';
+import { Tab } from '../../types/Tab';
+
+type Props = {
+  tabs: Tab[],
+};
+
+export const TabsPage: React.FC<Props> = ({ tabs }) => {
+  const { tabId } = useParams();
+  const selectedTab = tabs.find(tab => tab.id === tabId);
+
+  return (
+    <div>
+      <h1 className="title">Tabs page</h1>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="tab"
+              className={classNames({
+                'is-active': tab.id === tabId,
+              })}
+            >
+              <NavLink
+                to={`/tabs/${tab.id}`}
+              >
+                {tab.title}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="tab-content">
+        {selectedTab?.content || 'Please select a tab'}
+      </div>
+    </div>
+  );
+};
