f4336ca6aa828bdd30acbd6c65dea3821c0c3bac
Misha Dzhemula <medvedmisha23@gmail.com>
2022-08-26 13:52:28 +0300

    removed comments

--- src/components/TabsPage/TabsPage.tsx
+++ src/components/TabsPage/TabsPage.tsx
@@ -10,26 +10,7 @@ export const TabsPage = () => {
   return (
     <>
       <h1 className="title">Tabs page</h1>
-
-      {/* <div className="tabs is-boxed">
-        <ul>
-          <li data-cy="tab" className="is-active">
-            <Link to="#/">Tab 1</Link>
-          </li>
-          <li data-cy="tab">
-            <Link to="#/">Tab 2</Link>
-          </li>
-          <li data-cy="tab">
-            <Link to="#/">Tab 3</Link>
-          </li>
-        </ul>
-      </div> */}
-
       <Tabs tabs={tabs} />
-
-      {/* <div className="block" data-cy="tab-content">
-        Please select a tab
-      </div> */}
     </>
   );
 };
