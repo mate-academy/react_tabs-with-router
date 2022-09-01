1ca07959d90e2437b43997f8b46aec85397bc608
Oleksii Zasiedka <alex.zasedka@gmail.com>
2022-09-01 13:58:14 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { PageNavLink } from './components/PageNavLink/PageNavLink';
+import { TabsPage } from './components/TabsPage/TabsPage';
 
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
 
 const App = () => (
   <>
@@ -13,34 +16,22 @@ const App = () => (
     <nav className="navbar is-fixed-top has-background-light" data-cy="nav">
       <div className="navbar-menu">
         <div className="navbar-start">
-          <a href="/" className="navbar-item isActive">Home</a>
-          <a href="/tabs" className="navbar-item isActive">Tabs</a>
+          <PageNavLink to="/" text="Home" />
+          <PageNavLink to="/tabs" text="Tabs" />
         </div>
       </div>
     </nav>
 
     <div className="section">
-      <h1 className="title">Home page</h1>
-      <h1 className="title">Tabs page</h1>
-      <h1 className="title">Page not found</h1>
-
-      <div className="tabs is-boxed">
-        <ul>
-          <li data-cy="tab" className="is-active">
-            <a href="#/">Tab 1</a>
-          </li>
-          <li data-cy="tab">
-            <a href="#/">Tab 2</a>
-          </li>
-          <li data-cy="tab">
-            <a href="#/">Tab 3</a>
-          </li>
-        </ul>
-      </div>
-
-      <div className="block" data-cy="tab-content">
-        Please select a tab
-      </div>
+      <Routes>
+        <Route path="/" element={<h1 className="title">Home page</h1>} />
+        <Route path="/home" element={<Navigate to="/" replace />} />
+        <Route path="tabs">
+          <Route index element={<TabsPage tabs={tabs} />} />
+          <Route path=":tabId" element={<TabsPage tabs={tabs} />} />
+        </Route>
+        <Route path="*" element={<h1 className="title">Page not found</h1>} />
+      </Routes>
     </div>
   </>
 );
