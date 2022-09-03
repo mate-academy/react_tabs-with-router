c827e844512653043db065eed1454430a3a7df34
Gena Kupriiuk <gamesgolden04@gmail.com>
2022-09-04 01:28:47 +0300

    React tabs created

---
+++ src/components/TabsPage/TabsPage.tsx
@@ -0,0 +1,41 @@
+import classNames from 'classnames';
+import { Link, useParams } from 'react-router-dom';
+
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+export const TabsPage = () => {
+  const { tabId } = useParams();
+  const currentTab = tabs.find(tab => tab.id === tabId);
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="tab"
+              className={classNames({
+                'is-active': currentTab && currentTab.id === tab.id,
+              })}
+            >
+              <Link to={`/tabs/${tab.id}`}>
+                {tab.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="tab-content">
+        {currentTab?.content || 'Please select a tab'}
+      </div>
+    </>
+  );
+};
