eef3a26c39d774c9e83a300e52612601abfe6b28
DolishniiVolodymyr <volodymyr.dolishnii@gmail.com>
2022-09-03 15:35:33 +0300

    Solution

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,32 @@
+import { NavLink } from 'react-router-dom';
+import classNames from 'classnames';
+
+export const Navigation = () => {
+  return (
+    <nav
+      className="navbar is-light is-fixed-top is-mobile has-shadow"
+      data-cy="Nav"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <NavLink
+            to="/"
+            className={({ isActive }) => (
+              classNames('navbar-item', { 'is-active': isActive })
+            )}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/tabs"
+            className={({ isActive }) => (
+              classNames('navbar-item', { 'is-active': isActive })
+            )}
+          >
+            Tabs
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
