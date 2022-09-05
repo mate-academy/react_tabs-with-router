892faa218947d98a54ba8db455214636e1b5873f
Kate <krasnova2912@gmail.com>
2022-09-05 17:05:13 +0300

    useParams changes

--- src/index.tsx
+++ src/index.tsx
@@ -3,22 +3,20 @@ import {
   HashRouter, Navigate, Route, Routes,
 } from 'react-router-dom';
 import { App } from './App';
-import { Home } from './components/Home';
+import { HomePage } from './components/HomePage';
 import { PageNotFound } from './components/PageNotFound';
-import { Tabs } from './components/Tabs';
+import { TabsPage } from './components/TabsPage';
 
 ReactDOM.render(
   <HashRouter>
     <Routes>
       <Route path="/" element={<App />}>
-        <Route path="/" element={<Home />}>
-          <Route index element={<Home />} />
-          <Route path="home" element={<Navigate to="/" replace />} />
-        </Route>
+        <Route path="/" element={<HomePage />} />
+        <Route path="home" element={<Navigate to="/" replace />} />
 
-        <Route path="/tabs" element={<Tabs />}>
-          <Route index element={<Tabs />} />
-          <Route path=":tabId" element={<Tabs />} />
+        <Route path="/tabs" element={<TabsPage />}>
+          <Route index element={<TabsPage />} />
+          <Route path=":tabId" element={<TabsPage />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Route>
