4cbef3bccdad5239f6076ce25bef33dda9ca6c35
sergii-nosachenko <s.nosachenko@gmail.com>
2022-09-12 16:17:18 +0300

    Added Navigation component

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,24 @@
+import { PageNavLink } from '../PageNavLink/PageNavLink';
+
+export const Navigation: React.FC = () => {
+  return (
+    <nav
+      className="navbar is-light is-fixed-top is-mobile has-shadow"
+      data-cy="Nav"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <PageNavLink
+            to="/"
+            title="Home"
+          />
+
+          <PageNavLink
+            to="/tabs"
+            title="Tabs"
+          />
+        </div>
+      </div>
+    </nav>
+  );
+};
