384c591fa9b833d51e7ade78ab08ab72e6e83690
Tania Bukoros <tetiana.bukoros@gmail.com>
2022-09-12 07:33:08 +0300

    split into components

--- src/App.tsx
+++ src/App.tsx
@@ -2,52 +2,20 @@ import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
 import {
-  Link,
-  Navigate, NavLink, Outlet, Route, Routes, useParams,
+  Navigate, NavLink, Outlet, Route, Routes,
 } from 'react-router-dom';
 import classNames from 'classnames';
-import { Tab } from './types/Tab';
+import { TabsPage } from './TabsPage';
 
-const tabs = [
+export const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-const TabsPage = () => {
-  const { tabId } = useParams();
-  const isSelected = (tab: Tab) => tab.id === tabId;
-  const activeTab = tabs.find(tab => tab.id === tabId) || [];
-
+export const App = () => {
   return (
     <>
-      <h1 className="title">Tabs page</h1>
-      <div className="tabs is-boxed">
-        <ul>
-          {tabs.map(tab => (
-            <li
-              key={tab.id}
-              data-cy="tab"
-              className={classNames({
-                'is-active': isSelected(tab),
-              })}
-            >
-              <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="block" data-cy="tab-content">
-        {'content' in activeTab ? activeTab.content : 'Please select a tab'}
-      </div>
-    </>
-  );
-};
-
-const App = () => {
-  return (
-    <>
-      {/* Also requires <html class="has-navbar-fixed-top"> */}
       <nav
         className="navbar is-light is-fixed-top is-mobile has-shadow"
         data-cy="nav"
@@ -74,7 +42,6 @@ const App = () => {
 
       <div className="section">
         <div className="container">
-          {/* <p>{tabId}</p> */}
           <Routes>
             <Route
               path="/"
@@ -96,8 +63,8 @@ const App = () => {
                 </>
               )}
             >
-              <Route index element={<TabsPage />} />
-              <Route path=":tabId" element={<TabsPage />} />
+              <Route index element={<TabsPage tabs={tabs} />} />
+              <Route path=":tabId" element={<TabsPage tabs={tabs} />} />
             </Route>
           </Routes>
 
@@ -106,5 +73,3 @@ const App = () => {
     </>
   );
 };
-
-export default App;
---
+++ src/TabsPage.tsx
@@ -0,0 +1,38 @@
+import { Link, useParams } from 'react-router-dom';
+import classNames from 'classnames';
+import { Tab } from './types/Tab';
+// import { tabs } from './App';
+
+interface Props {
+  tabs: Tab[];
+}
+
+export const TabsPage: React.FC<Props> = ({ tabs }) => {
+  const { tabId } = useParams();
+  const isSelected = (tab: Tab) => tab.id === tabId;
+  const activeTab = tabs.find(tab => tab.id === tabId) || [];
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="tab"
+              className={classNames({
+                'is-active': isSelected(tab),
+              })}
+            >
+              <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="block" data-cy="tab-content">
+        {'content' in activeTab ? activeTab.content : 'Please select a tab'}
+      </div>
+    </>
+  );
+};
