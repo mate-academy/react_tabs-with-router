9da56ffeeb17334ca959ce66484c70f9c9242030
Tania Troshchuk <troschuk95@gmail.com>
2022-09-21 16:46:22 +0300

    Solution task React Tabs with Router

---
+++ src/components/NavBarLink.tsx
@@ -0,0 +1,22 @@
+import classNames from 'classnames';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+type Props = {
+  to: string,
+  title: string,
+};
+
+export const NavBarLink: React.FC<Props> = ({ to, title }) => {
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) => classNames(
+        'navbar-item',
+        { 'is-active': isActive },
+      )}
+    >
+      {title}
+    </NavLink>
+  );
+};
