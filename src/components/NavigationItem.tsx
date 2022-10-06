5a6da0022494e89aba167b7c65424cbe5bb48187
Nikita Havryliuk <chinter.games1@gmail.com>
2022-10-06 17:29:47 +0300

    added Navigation

---
+++ src/components/NavigationItem.tsx
@@ -0,0 +1,24 @@
+import classNames from 'classnames';
+import { NavLink } from 'react-router-dom';
+
+type Props = {
+  to: string;
+  content: string;
+};
+
+export const NavigationItem: React.FC<Props> = ({
+  to,
+  content,
+}) => {
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) => classNames(
+        'navbar-item',
+        { 'has-background-grey-lighter': isActive },
+      )}
+    >
+      {content}
+    </NavLink>
+  );
+};
