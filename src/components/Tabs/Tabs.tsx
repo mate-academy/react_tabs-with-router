ade4cab379e1e11e9deb8ae79ccc5adde763b5e9
Nikita Havryliuk <chinter.games1@gmail.com>
2022-10-06 16:59:38 +0300

    created Tabs component

---
+++ src/components/Tabs/Tabs.tsx
@@ -0,0 +1,66 @@
+import classNames from 'classnames';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Tab } from '../../types/Tab';
+
+type Props = {
+  tabs: Tab[];
+  selectedTabId: string;
+  onTabSelected: (tab: Tab) => void;
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTabId,
+  onTabSelected,
+}) => {
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
+  const { content } = selectedTab;
+
+  const handleClick = (tab: Tab) => {
+    if (selectedTab.id === tab.id) {
+      return;
+    }
+
+    onTabSelected(tab);
+  };
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+      <div data-cy="TabsComponent">
+        <div className="tabs is-boxed">
+          <ul>
+            {tabs.map(tab => {
+              const { id, title } = tab;
+
+              return (
+                <li
+                  key={id}
+                  className={classNames({
+                    'is-active':
+                      selectedTab.id === tab.id,
+                  })}
+                  data-cy="Tab"
+                >
+                  <Link
+                    to={`#${id}`}
+                    data-cy="TabLink"
+                    onClick={() => handleClick(tab)}
+                  >
+                    {title}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        <div className="block" data-cy="TabContent">
+          {content}
+        </div>
+      </div>
+    </>
+
+  );
+};
