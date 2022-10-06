a78bebda333a4d9c2c789524aa97c5848b9de787
Maks Sobko <tiserettt@gmail.com>
2022-10-06 14:10:45 +0300

    implemented navigation between tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-import { Navigation } from './components/Navigation';
 import { Route, Routes } from 'react-router-dom';
+import { Navigation } from './components/Navigation';
 import { Home } from './pages/Home';
 import { NotFound } from './pages/NotFound';
 import { Tabs } from './pages/Tabs';
-
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
 
 export const App = () => (
   <>
@@ -25,7 +19,7 @@ export const App = () => (
           <Route path="home" element={<Home />} />
           <Route path="tabs" element={<Tabs />}>
             <Route index element={<Tabs />} />
-            <Route path=":tabId" element={<Tabs />} />
+            <Route path=":selectedTabId" element={<Tabs />} />
           </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
--- src/pages/Tabs.tsx
+++ src/pages/Tabs.tsx
@@ -1,23 +1,39 @@
-export const Tabs = () => (
-  <>
-    <h1 className="title">Tabs page</h1>
+import { Link, useParams } from 'react-router-dom';
+import { Tab } from '../types/Tab';
 
-    <div className="tabs is-boxed">
-      <ul>
-        <li data-cy="Tab" className="is-active">
-          <a href="#/">Tab 1</a>
-        </li>
-        <li data-cy="Tab">
-          <a href="#/">Tab 2</a>
-        </li>
-        <li data-cy="Tab">
-          <a href="#/">Tab 3</a>
-        </li>
-      </ul>
-    </div>
+const tabs: Tab[] = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
 
-    <div className="block" data-cy="TabContent">
-      Please select a tab
-    </div>
-  </>
-);
+export const Tabs: React.FC = () => {
+  const { selectedTabId } = useParams();
+
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId);
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              data-cy="Tab"
+              className={`${selectedTab?.id === tab.id ? 'is-active' : ''}`}
+              key={tab.id}
+            >
+              <Link to={`/tabs/${tab.id}`}>
+                {tab.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="block" data-cy="TabContent">
+        {selectedTab ? selectedTab.content : 'Please select a tab'}
+      </div>
+    </>
+  );
+};
