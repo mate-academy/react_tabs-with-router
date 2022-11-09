56f89762ebdfcbddca4ebe6c50602e2bcf4d01a0
Danil <dasha8854567@gmail.com>
2022-11-09 15:45:11 +0200

    commit2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import {
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-import { Outlet } from 'react-router-dom';
 import { Nav } from './components/Nav';
+import { Tabs } from './components/Tabs';
+
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
 
 export const App = () => (
   <>
@@ -10,7 +21,23 @@ export const App = () => (
 
     <div className="section">
       <div className="container">
-        <Outlet />
+        <Routes>
+          <Route path="/">
+            <Route index element={<h1 className="title">Home page</h1>} />
+            <Route path="home" element={<Navigate to="/" replace />} />
+
+            <Route path="tabs" element={<Tabs tabs={tabs} />}>
+              <Route index />
+              <Route path=":selectTab" />
+            </Route>
+
+            <Route
+              path="*"
+              element={<h1 className="title">Page not found</h1>}
+            />
+
+          </Route>
+        </Routes>
       </div>
     </div>
   </>
