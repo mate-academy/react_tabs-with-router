d7f967880e3922fb0c01c13801e485afa8fd238e
Yevheniya Deryka <evgeniyaderika@gmail.com>
2022-11-14 19:05:36 +0200

    Removed useless props and created separate components for pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,8 @@ import './App.scss';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import { MainNav } from './components/MainNav';
 import { TabsPage } from './pages/TabsPage';
-
-const tabs = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-];
+import { HomePage } from './pages/HomePage';
+import { NotFoundPage } from './pages/NotFoundPage';
 
 export const App: React.FC = () => {
   return (
@@ -22,7 +18,7 @@ export const App: React.FC = () => {
           <Routes>
             <Route
               path="/"
-              element={(<h1 className="title">Home page</h1>)}
+              element={<HomePage />}
             />
             <Route
               path="/home"
@@ -30,16 +26,16 @@ export const App: React.FC = () => {
             />
             <Route
               path="*"
-              element={(<h1 className="title">Page not found</h1>)}
+              element={<NotFoundPage />}
             />
             <Route path="tabs">
               <Route
                 index
-                element={<TabsPage tabs={tabs} />}
+                element={<TabsPage />}
               />
               <Route
                 path=":tabId"
-                element={<TabsPage tabs={tabs} />}
+                element={<TabsPage />}
               />
             </Route>
           </Routes>
--- src/pages/TabsPage/TabsPage.tsx
+++ src/pages/TabsPage/TabsPage.tsx
@@ -2,19 +2,13 @@ import classNames from 'classnames';
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 
-type Tab = {
-  id: string,
-  title: string,
-  content: string,
-};
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
 
-type Props = {
-  tabs: Tab[],
-};
-
-export const TabsPage: React.FC<Props> = ({
-  tabs,
-}) => {
+export const TabsPage: React.FC = () => {
   const { tabId } = useParams();
   const selectedTab = tabs.find(tab => tab.id === tabId);
 
@@ -26,17 +20,17 @@ export const TabsPage: React.FC<Props> = ({
 
       <div className="tabs is-boxed">
         <ul>
-          {tabs.map(tab => (
+          {tabs.map(({ id, title }) => (
             <li
-              key={tab.id}
-              className={classNames({ 'is-active': tab.id === tabId })}
+              key={id}
+              className={classNames({ 'is-active': id === tabId })}
               data-cy="Tab"
             >
               <Link
-                to={`/tabs/${tab.id}`}
+                to={`/tabs/${id}`}
                 data-cy="TabLink"
               >
-                {tab.title}
+                {title}
               </Link>
             </li>
           ))}
