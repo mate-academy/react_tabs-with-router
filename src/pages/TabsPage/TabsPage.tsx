9afe29387ae1523cac3779bab5c716b7dcb68fa5
Vlad Harkusha <sivari888@gmail.com>
2022-12-15 17:48:45 +0200

    tabs router was implemented

---
+++ src/pages/TabsPage/TabsPage.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import cn from 'classnames';
+
+import { Tab } from '../../types/Tab';
+
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+export const TabsPage: React.FC = () => {
+  const [selectedTab, setSelectedTab] = useState<Tab | null>(null);
+  const { tabId } = useParams();
+
+  useEffect(() => {
+    const newSelectedTab = tabs.find(tab => tab.id === tabId) || null;
+
+    setSelectedTab(newSelectedTab);
+  }, [tabId]);
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              data-cy="Tab"
+              key={tab.id}
+              className={cn({ 'is-active': tab.id === tabId })}
+            >
+              <Link to={`/tabs/${tab.id}`}>
+                {tab.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {!selectedTab ? (
+        <div className="block" data-cy="TabContent">
+          Please select a tab
+        </div>
+      ) : (
+        <div className="block" data-cy="TabContent">
+          {selectedTab.content}
+        </div>
+      )}
+    </>
+  );
+};
