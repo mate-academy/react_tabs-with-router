65bebd0c89b1b69618fdd68297030cf30ed416c7
Anastasiia Tilikina <anastasiia.tilikina@gmail.com>
2022-12-17 17:31:51 +0200

    Implement Tabs selection using React Router

---
+++ src/components/PageNavLink/PageNavLink.tsx
@@ -0,0 +1,22 @@
+import classNames from 'classnames';
+import { FC } from 'react';
+import { NavLink } from 'react-router-dom';
+
+type Props = {
+  to: string;
+  content: string;
+};
+
+export const PageNavLink: FC<Props> = ({ to, content }) => {
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) => classNames(
+        'navbar-item',
+        { 'is-active': isActive },
+      )}
+    >
+      {content}
+    </NavLink>
+  );
+};
