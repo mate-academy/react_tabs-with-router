f116ad8d7c147d3e51a74aa163decbabd2634ca2
Sapna Choudhary <sapnachoudhary97491@gmail.com>
2022-12-20 22:39:57 +0100

    Solution for react tabs with router

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+import { NavLink } from 'react-router-dom';
+
+export const Navbar = () => (
+  <nav
+    className="navbar is-light is-fixed-top is-mobile has-shadow"
+    data-cy="Nav"
+  >
+    <div className="container">
+      <div className="navbar-brand">
+        <NavLink
+          to="/"
+          className="navbar-item"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/tabs"
+          className="navbar-item"
+        >
+          Tabs
+        </NavLink>
+      </div>
+    </div>
+  </nav>
+);
---
+++ src/components/TabsPage/TabsPage.tsx
@@ -0,0 +1,33 @@
+import { useMatch } from 'react-router-dom';
+import { Tabs } from '../Tabs/Tabs';
+
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+export const TabsPage: React.FC = () => {
+  const selectedTabId = useMatch('/tabs/:tabId')?.params.tabId;
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId);
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+      <Tabs
+        tabs={tabs}
+      />
+      {
+        selectedTab
+          ? (
+            <div className="block" data-cy="TabContent">
+              {selectedTab.content}
+            </div>
+          )
+          : (
+            <p>please selected tab</p>
+          )
+      }
+    </>
+  );
+};
