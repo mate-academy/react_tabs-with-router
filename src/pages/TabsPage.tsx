ab6e29fa68acf74537779ba37e972fee7283b312
s3rhiienko <r.i.serhiienko@gmail.com>
2022-12-20 19:11:33 +0200

    Tabs with router

---
+++ src/pages/TabsPage.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { Tabs } from '../components/Tabs';
+
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+export const TabsPage = () => {
+  const { tabId } = useParams();
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <Tabs
+        tabs={tabs}
+        selectedTabId={tabId}
+      />
+    </>
+  );
+};
