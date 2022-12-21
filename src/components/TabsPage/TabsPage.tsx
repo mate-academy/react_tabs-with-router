f8de6a7fe2ca8440a158a3a3c04c1db8873b1ef4
Vadym Mamchur <vmchr344@gmail.com>
2022-12-21 12:27:05 +0200

    tabs with router have been implemented

---
+++ src/components/TabsPage/TabsPage.tsx
@@ -0,0 +1,39 @@
+import { FC } from 'react';
+import cn from 'classnames';
+import { Link, useParams } from 'react-router-dom';
+import { Tab } from '../../types/Tab';
+
+interface Props {
+  tabs: Tab[];
+}
+
+export const TabsPage: FC<Props> = ({ tabs }) => {
+  const { tabId = '' } = useParams();
+
+  const foundTab = tabs.find(tab => tabId === tab.id);
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              data-cy="Tab"
+              className={cn(
+                { 'is-active': tabId === tab.id },
+              )}
+            >
+              <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {foundTab?.content || 'Please select a tab'}
+      </div>
+
+    </>
+  );
+};
