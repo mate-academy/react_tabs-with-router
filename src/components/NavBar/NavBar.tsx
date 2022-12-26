b8552648b074c9f8ea4f7e6ada41cd9215d64fd8
Vitalii Babiienko <v.babiyenko@gmail.com>
2022-12-26 13:25:41 +0200

    solution

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react';
+import { NavBarLink } from '../NavBarLink';
+
+export const NavBar: FC = () => {
+  return (
+    <nav
+      className="navbar is-light is-fixed-top is-mobile has-shadow"
+      data-cy="Nav"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <NavBarLink
+            to="/"
+            text="Home"
+          />
+
+          <NavBarLink
+            to="tabs"
+            text="Tabs"
+          />
+        </div>
+      </div>
+    </nav>
+  );
+};
