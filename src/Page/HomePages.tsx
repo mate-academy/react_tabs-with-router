e85f3c365018c157a8c196c9ca397c1531d3e153
Nikita Cherkashin <mikitkaaist@gmail.com>
2023-01-10 13:05:59 +0200

    tabs with router

---
+++ src/Page/HomePages.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+export const HomePages: FC = () => (
+  <>
+    <h1 className="title">Home page</h1>
+  </>
+);
---
+++ src/Page/NotPages.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+export const NotPages: FC = () => (
+  <>
+    <h1 className="title">Page not found</h1>
+  </>
+);
