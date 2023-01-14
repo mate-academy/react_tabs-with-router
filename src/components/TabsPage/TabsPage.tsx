dd2b902a60ce378c29ccc98e0e2cae007316b032
Anastasiia Shyptia <shyptianastya27@gmail.com>
2023-01-14 21:06:21 +0200

    Task was done

---
+++ src/components/TabsPage/TabsPage.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import cn from 'classnames';
+import { Link, useParams } from 'react-router-dom';
+import { Tab } from '../../types/Tab';
+
+type Props = {
+  tabs: Tab[];
+};
+
+export const TabsPage: React.FC<Props> = ({ tabs }) => {
+  const { tabId } = useParams();
+
+  const selectedTab = tabs.find(tab => tab.id === tabId);
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={cn({ 'is-active': tabId === tab.id })}
+            >
+              <Link
+                to={`/tabs/${tab.id}`}
+                data-cy="TabLink"
+              >
+                {tab.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab
+          ? selectedTab.content
+          : 'Please select a tab'}
+      </div>
+    </>
+  );
+};
