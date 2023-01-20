a7b979ed57d0828ed2a873cb1ed2d4c149bb6d50
Artem Nasadchuk <nasadchuk007@gmail.com>
2023-01-20 19:33:27 +0200

    implemented react tabs with router

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,25 @@
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import {
+  HashRouter, Navigate, Route, Routes,
+} from 'react-router-dom';
 import { App } from './App';
+import { HomePage } from './components/HomePage';
+import { PageNotFound } from './components/PageNotFound';
+import { TabsPage } from './components/TabsPage';
 
 ReactDOM.render(
   <HashRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path="home" element={<Navigate to="/" replace />} />
+        <Route path="tabs">
+          <Route index element={<TabsPage />} />
+          <Route path=":tabId" element={<TabsPage />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Route>
+    </Routes>
   </HashRouter>,
   document.getElementById('root'),
 );
