409bc38efed31567af75aa68d5599ab2606a189e
Taras Kozii <taras_kozii@ukr.net>
2023-01-24 20:46:58 +0200

    solution v02

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-import { Outlet } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { MainNav } from './components/MainNav';
+import { TabsPage } from './pages/TabsPage';
 
 export const App = () => (
   <>
     <MainNav />
     <div className="section">
       <div className="container">
-        <Outlet />
+        <Routes>
+          <Route path="/">
+            <Route index element={<h1 className="title">Home page</h1>} />
+            <Route path="home" element={<Navigate to="/" replace />} />
+
+            <Route path="tabs">
+              <Route index element={<TabsPage />} />
+              <Route path=":tabId" element={<TabsPage />} />
+            </Route>
+
+            <Route
+              path="*"
+              element={<h1 className="title">Page not found</h1>}
+            />
+          </Route>
+        </Routes>
       </div>
     </div>
   </>
--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,10 @@
 import ReactDOM from 'react-dom';
-import {
-  HashRouter,
-  Navigate,
-  Route,
-  Routes,
-} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
-import { TabsPage } from './pages/TabsPage';
 
 ReactDOM.render(
-  <HashRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<h1 className="title">Home page</h1>} />
-        <Route path="home" element={<Navigate to="/" replace />} />
-
-        <Route path="tabs">
-          <Route index element={<TabsPage />} />
-          <Route path=":tabId" element={<TabsPage />} />
-        </Route>
-
-        <Route path="*" element={<h1 className="title">Page not found</h1>} />
-      </Route>
-    </Routes>
-  </HashRouter>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root'),
 );
