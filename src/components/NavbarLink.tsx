de6a6e7a5752c767f9fc4646a21f2948aa68dd69
Artem Hirzhev <a.hirzhev@gmail.com>
2023-01-26 12:51:29 +0200

    Initial commit

---
+++ src/components/NavbarLink.tsx
@@ -0,0 +1,18 @@
+import classNames from 'classnames';
+import { NavLink } from 'react-router-dom';
+
+type Props = {
+  to: string;
+  text: string;
+};
+
+export const NavbarLink: React.FC<Props> = ({ to, text }) => (
+  <NavLink
+    to={to}
+    className={({ isActive }) => classNames('navbar-item', {
+      'is-active': isActive,
+    })}
+  >
+    {text}
+  </NavLink>
+);
