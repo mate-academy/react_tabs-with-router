ad59831f03ab64eb5a28d94ddfbbb3ab6ffa4b5c
Vladimir Arkanov <vladimirarkanov@icloud.com>
2023-02-01 15:25:48 +0200

    first commit

---
+++ src/components/Tabs.tsx
@@ -0,0 +1,45 @@
+import React, { memo, useMemo } from 'react';
+import { NavLink } from 'react-router-dom';
+import classNames from 'classnames';
+import { Tab } from '../types/Tab';
+
+export type Props = {
+  tabId: string | undefined,
+  tabs: Tab[]
+};
+
+export const Tabs: React.FC<Props> = memo(({ tabs, tabId = '' }) => {
+  const selectedTab = useMemo(() => (oneTab: Tab) => {
+    return oneTab.id === tabId;
+  }, [tabId]);
+
+  const selectedTabContent = useMemo(() => (tab: Tab[]) => {
+    const properTab = tab.find(el => el.id === tabId);
+
+    return properTab?.content;
+  }, [tabId]);
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              data-cy="Tab"
+              className={classNames(
+                { 'is-active': selectedTab(tab) },
+              )}
+            >
+              <NavLink to={`/tabs/${tab.id}`}>{tab.title}</NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTabContent(tabs) || 'Please select a tab'}
+      </div>
+    </>
+  );
+});
---
+++ src/pages/TabsPage.tsx
@@ -0,0 +1,17 @@
+import React, { memo } from 'react';
+import { useParams } from 'react-router-dom';
+import { Tabs } from '../components/Tabs';
+
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+export const TabsPage: React.FC = memo(() => {
+  const { tabId } = useParams();
+
+  return (
+    <Tabs tabs={tabs} tabId={tabId} />
+  );
+});
