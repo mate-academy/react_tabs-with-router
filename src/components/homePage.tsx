6383828fc598a1a52f2148ca6edafcae203e4a70
Serhii Redko <sergey@redko.com.ua>
2023-02-02 21:44:55 +0200

    solution

--- src/components/homePage.tsx
+++ src/components/homePage.tsx
@@ -1,5 +1,5 @@
 import React, { memo } from 'react';
 
-export const homePage: React.FC = memo(() => (
+export const HomePage: React.FC = memo(() => (
   <h1 className="title">Home page</h1>
 ));
--- src/components/pageNotFind.tsx
+++ src/components/pageNotFind.tsx
@@ -1,5 +1,5 @@
 import React, { memo } from 'react';
 
-export const homePage: React.FC = memo(() => (
+export const PageNotFound: React.FC = memo(() => (
   <h1 className="title">Page not found</h1>
 ));
