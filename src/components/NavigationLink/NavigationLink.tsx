2775e830634978fb81398c09682a70c215ef1115
Yana Gonchar <johanna.gonchar@icloud.com>
2023-02-05 23:23:22 +0100

    React - Tabs With Router

---
+++ src/components/NavigationLink/NavigationLink.tsx
@@ -0,0 +1,22 @@
+import React, { memo } from 'react';
+import { NavLink } from 'react-router-dom';
+import cn from 'classnames';
+
+type Props = {
+  to: string;
+  title: string;
+};
+
+export const NavigationLink: React.FC<Props> = memo(({ to, title }) => (
+  <NavLink
+    to={to}
+    className={
+      ({ isActive }) => cn(
+        'navbar-item',
+        { 'has-background-grey-lighter is-active': isActive },
+      )
+    }
+  >
+    {title}
+  </NavLink>
+));
