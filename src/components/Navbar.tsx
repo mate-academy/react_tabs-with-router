36e54b58954086963dd571cd7a6f6bededb4c12b
Viktoriia Kravchenko <kravchenkovictoriya9@gmail.com>
2023-02-07 09:34:29 +0200

    Fixed indentation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,8 +4,9 @@ import { TemplateNavLink } from './TemplateNavLink';
 export const Navbar: FC = memo(() => {
   return (
     <nav
-      className="navbar is-light is-fixed-top is-mobile
-      has-shadow has-navbar-fixed-top"
+      className="
+      navbar is-light is-fixed-top is-mobile has-shadow has-navbar-fixed-top
+      "
       data-cy="Nav"
     >
       <div className="container">
