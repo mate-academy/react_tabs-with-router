6e2bf3f0f5832d9aa5eae732a49cefb238a43034
Saleh BUD <saleh.7734@gmail.com>
2023-02-20 19:08:29 +0200

    Complete

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import classNames from 'classnames';
+import { NavLink } from 'react-router-dom';
+
+export const Navbar = () => (
+  <nav
+    className="navbar is-light is-fixed-top is-mobile has-shadow"
+    data-cy="Nav"
+  >
+    <div className="container">
+      <div className="navbar-brand">
+        <NavLink
+          to="/"
+          className={({ isActive }) => classNames('navbar-item', {
+            'is-active': isActive,
+          })}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/tabs"
+          className={({ isActive }) => classNames('navbar-item', {
+            'is-active': isActive,
+          })}
+        >
+          Tabs
+        </NavLink>
+      </div>
+    </div>
+  </nav>
+);
