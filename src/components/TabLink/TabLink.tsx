de2ee84d4686610d5b6985669c3f5a7dc7b52749
Oleh Chermak <ochermak2002@gmail.com>
2023-02-25 18:09:21 +0200

    Solution

---
+++ src/components/TabLink/TabLink.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+import { Tab } from '../../types/Tab';
+
+type Props = {
+  tab: Tab;
+};
+
+export const TabLink: React.FC<Props> = ({ tab }) => {
+  const { id, title } = tab;
+
+  return (
+    <Link
+      to={`../${id}`}
+      data-cy="TabLink"
+    >
+      {title}
+    </Link>
+  );
+};
