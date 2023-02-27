66341f5db88357b10f471ded799c38524c9bfe6e
Andrew Borys <borysandrey1988@gmail.com>
2023-02-28 00:28:18 +0200

    solution-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+import { NavBar } from './components/NavBar';
+import { HomePage } from './pages/HomePage';
+import { TabsPage } from './pages/TabsPage';
+import { PageNotFound } from './pages/PageNotFound';
 
 export const App = () => (
   <>
     {/* Also requires <html class="has-navbar-fixed-top"> */}
-    <nav
+    {/* <nav
       className="navbar is-light is-fixed-top is-mobile has-shadow"
       data-cy="Nav"
     >
@@ -21,32 +20,21 @@ export const App = () => (
           <a href="/tabs" className="navbar-item">Tabs</a>
         </div>
       </div>
-    </nav>
+    </nav> */}
+    <NavBar />
 
     <div className="section">
-      <div className="container">
-        <h1 className="title">Home page</h1>
-        <h1 className="title">Tabs page</h1>
-        <h1 className="title">Page not found</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<Navigate to="/" replace />} />
 
-        <div className="tabs is-boxed">
-          <ul>
-            <li data-cy="Tab" className="is-active">
-              <a href="#/">Tab 1</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 2</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 3</a>
-            </li>
-          </ul>
-        </div>
+        <Route path="/tabs">
+          <Route index element={<TabsPage />} />
+          <Route path=":tabId" element={<TabsPage />} />
+        </Route>
 
-        <div className="block" data-cy="TabContent">
-          Please select a tab
-        </div>
-      </div>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </div>
   </>
 );
---
+++ src/pages/TabsPage/TabsPage.tsx
@@ -0,0 +1,44 @@
+import classnames from 'classnames';
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { TabLink } from '../../components/TabLink';
+
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+export const TabsPage: React.FC = () => {
+  const { tabId } = useParams();
+  const selectedTab = tabs.find(({ id }) => id === tabId);
+
+  return (
+    <div className="container">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            const { id } = tab;
+            const isSelectedTab = id === selectedTab?.id;
+
+            return (
+              <li
+                key={id}
+                data-cy="Tab"
+                className={classnames({
+                  'is-active': isSelectedTab,
+                })}
+              >
+                <TabLink tab={tab} />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab?.content}
+      </div>
+    </div>
+  );
+};
