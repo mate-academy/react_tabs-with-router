63a7ff7fff2a667946a9a8a460fe62de65ec35db
Anastasiia Kravhenko <steys13712@gmail.com>
2023-02-27 18:29:42 +0200

    solution

--- src/components/TodoTable/TodoTable.tsx
+++ src/components/TodoTable/TodoTable.tsx
@@ -12,25 +12,27 @@ export const TodoTable: FC<Props> = ({ tabs, selectedTabId }) => {
     = tabs.find(tab => tab.id === selectedTabId)?.content || null;
 
   return (
-    <div className="container">
-      <div className="tabs is-boxed">
-        <ul>
-          {tabs.map(tab => {
-            const isActive = tab.id === selectedTabId;
+    <>
+      <div className="container">
+        <div className="tabs is-boxed">
+          <ul>
+            {tabs.map(tab => {
+              const isActive = tab.id === selectedTabId;
 
-            return (
-              <TabLink
-                key={tab.id}
-                tab={tab}
-                isActive={isActive}
-              />
-            );
-          })}
-        </ul>
+              return (
+                <TabLink
+                  key={tab.id}
+                  tab={tab}
+                  isActive={isActive}
+                />
+              );
+            })}
+          </ul>
+        </div>
+        <div className="block" data-cy="TabContent">
+          {tabContent || 'Please select a tab'}
+        </div>
       </div>
-      <div className="block" data-cy="TabContent">
-        {tabContent || 'Please select a tab'}
-      </div>
-    </div>
+    </>
   );
 };
