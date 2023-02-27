4bdcc7e6b01fe334d0ff70cf587ad052264472f5
Yurii Demkiv <demkiv123@gmail.com>
2023-02-27 14:19:18 +0200

    Minor fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { NavBar } from './components/NavBar';
-import { TabsPage } from './components/TabsPage/TabsPage';
+import { TabsPage } from './components/TabsPage';
 
 const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
