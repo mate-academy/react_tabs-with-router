7fa7a3e2409a3ff414f4620c89c5101358904201
Ihor Soloviov <igor.musson.55@gmail.com>
2023-02-28 21:40:01 +0200

    small fixes #2

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-export const Home: React.FC = () => {
-  return (
-    <h1 className="title">Home page</h1>
-  );
-};
+export const Home: React.FC = () => (
+  <h1 className="title">Home page</h1>
+);
