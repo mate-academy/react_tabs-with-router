6f733b9c164020c156eaacc013c63ae060eb3528
Anastasiia Kuznetsova <an.stsii.k@gmail.com>
2023-03-22 19:03:38 +0100

    created nav component

--- src/App.tsx
+++ src/App.tsx
@@ -3,45 +3,16 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
 import {
   Navigate,
-  NavLink,
   Route,
   Routes,
 } from 'react-router-dom';
-import classNames from 'classnames';
 
 import { TabsPage } from './components/TabsPage';
+import { Navbar } from './components/Navbar';
 
 export const App = () => (
   <>
-    {/* Also requires <html class="has-navbar-fixed-top"> */}
-    <nav
-      className="navbar is-light is-fixed-top is-mobile has-shadow"
-      data-cy="Nav"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <NavLink
-            to="/"
-            className={({ isActive }) => classNames(
-              'navbar-item',
-              { 'is-active': isActive },
-            )}
-          >
-            Home
-          </NavLink>
-
-          <NavLink
-            to="/tabs"
-            className={({ isActive }) => classNames(
-              'navbar-item',
-              { 'is-active': isActive },
-            )}
-          >
-            Tabs
-          </NavLink>
-        </div>
-      </div>
-    </nav>
+    <Navbar />
 
     <div className="section">
       <div className="container">
