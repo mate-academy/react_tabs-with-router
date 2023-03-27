0a58af2418ff2350cb7b58c13c55524916e92270
Roshan Jha <kartohajha@gmail.com>
2023-03-28 02:55:11 +0300

    done

---
+++ src/components/PageNavLink.tsx
@@ -0,0 +1,22 @@
+import classNames from 'classnames';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+type PageNavLinkProps = {
+  to: string,
+  text: string,
+};
+
+export const PageNavLink: React.FC<PageNavLinkProps> = ({
+  to,
+  text,
+}) => (
+  <NavLink
+    to={to}
+    className={({ isActive }) => classNames('navbar-item', {
+      'is-active': isActive,
+    })}
+  >
+    {text}
+  </NavLink>
+);
