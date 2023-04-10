c108fa382404abf383720409a61b045e552d7b85
ctk12 <chetankatwa12@gmail.com>
2023-04-10 13:27:25 +0530

    Added React Tabs with Router

---
+++ src/Components/Navigation/Navigation.tsx
@@ -0,0 +1,36 @@
+import classNames from 'classnames';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export const Navigation: React.FC = () => {
+  return (
+    <nav
+      className="navbar is-light is-fixed-top is-mobile has-shadow"
+      data-cy="Nav"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <NavLink
+            to="/"
+            className={({ isActive }) => classNames(
+              'navbar-item',
+              { 'is-active': isActive },
+            )}
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            to="/tabs"
+            className={({ isActive }) => classNames(
+              'navbar-item',
+              { 'is-active': isActive },
+            )}
+          >
+            Tabs
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
---
+++ src/Components/Navigation/Tabs/Tabs.tsx
@@ -0,0 +1,48 @@
+import classNames from 'classnames';
+import React from 'react';
+import { NavLink, useMatch } from 'react-router-dom';
+import { Tab } from '../../../types/Tab';
+
+type Props = {
+  tabs: Tab[];
+};
+
+export const Tabs: React.FC<Props> = ({ tabs }) => {
+  const match = useMatch('/tabs/:tabId');
+  const selectedTabId = match?.params.tabId;
+
+  const selectedTab = tabs.filter(tab => tab.id === selectedTabId);
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            const { id, title } = tab;
+
+            return (
+              <li
+                data-cy="Tab"
+                className={classNames(
+                  { 'is-active': id === selectedTabId },
+                )}
+              >
+                <NavLink to={`/tabs/${id}`}>
+                  {title}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab.length
+          ? selectedTab[0].content
+          : 'Please select a tab'}
+      </div>
+    </>
+  );
+};
