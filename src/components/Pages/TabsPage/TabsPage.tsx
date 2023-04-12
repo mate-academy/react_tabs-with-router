09e6d84b0e37d5d2570189f5b3b92886e6a10af7
Oleksandr Usov <usov.sasha.93@gmail.com>
2023-04-12 17:18:55 +0300

    Added the task solution

---
+++ src/components/Pages/TabsPage/TabsPage.tsx
@@ -0,0 +1,38 @@
+import { useParams } from 'react-router-dom';
+
+import { TabItem } from '../../TabItem';
+
+export const TabsPage: React.FC = () => {
+  const { tabId = '' } = useParams();
+  const tabs = [
+    { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+    { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+    { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  ];
+  const activeTab = tabs.find(tab => tab.id === tabId);
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <TabItem
+              tab={tab}
+              selectedTabId={tabId}
+            />
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab ? (
+          activeTab.content
+        ) : (
+          'Please select a tab'
+        )}
+      </div>
+    </>
+  );
+};
---
+++ src/components/TabItem/TabItem.tsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+
+import { Tab } from '../../types/Tab';
+
+type Props = {
+  tab: Tab;
+  selectedTabId: string;
+};
+
+export const TabItem: React.FC<Props> = ({
+  tab,
+  selectedTabId,
+}) => {
+  return (
+    <li
+      data-cy="Tab"
+      className={classNames(
+        { 'is-active': tab.id === selectedTabId },
+      )}
+      key={tab.id}
+    >
+      <Link to={`../${tab.id}`}>
+        {tab.title}
+      </Link>
+    </li>
+  );
+};
