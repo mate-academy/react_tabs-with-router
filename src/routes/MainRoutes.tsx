40714b71d35408cfc7e9df6877942aa3fece8e03
Bogdan <bogdan_mykhailov@icloud.com>
2023-04-14 11:58:35 +0300

    init commit

---
+++ src/routes/MainRoutes.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react';
+import {
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import { HomePage } from '../pages/HomePage';
+import { TabsPage } from '../pages/TabsPage';
+import { NotFoundPage } from '../pages/NotFoundPage';
+import { PATH } from '../types';
+
+export const MainRoutes: FC = () => {
+  return (
+    <Routes>
+      <Route path={PATH.Main} element={<HomePage />} />
+      <Route path={PATH.Home} element={<Navigate to={PATH.Main} replace />} />
+
+      <Route path={PATH.Tabs}>
+        <Route index element={<TabsPage />} />
+        <Route path={PATH.TAB_ID} element={<TabsPage />} />
+      </Route>
+
+      <Route path={PATH.Error} element={<NotFoundPage />} />
+    </Routes>
+  );
+};
---
+++ src/types/Path.ts
@@ -0,0 +1,7 @@
+export enum PATH {
+  Main = '/',
+  Home = '/home',
+  Tabs = '/tabs',
+  Error = '/*',
+  TAB_ID = ':slug',
+}
