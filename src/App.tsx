f701f72d336940e94da6074e12f5efb9f8533fee
vadym4che <vadym4che@gmail.com>
2023-04-17 06:09:14 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,78 @@
+import { FC, useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+import {
+  Link,
+  NavLink,
+  Navigate,
+  Route,
+  Routes,
+  useParams,
+} from 'react-router-dom';
 
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+interface Tab {
+  id: string;
+  title: string;
+  content: string;
+}
+
+interface Props {
+  tabsArray: Tab[];
+  tabId: string;
+}
+
+const getTabById = (tabsArr: Tab[], idFindBy: string) => {
+  return tabsArr.find(tab => tab.id === idFindBy) || null;
+};
+
+const Tabs: FC<Props> = ({ tabsArray, tabId }) => {
+  const [selectedTab, setSelectedTab] = useState<Tab | null>(null);
+
+  useEffect(() => {
+    setSelectedTab(getTabById(tabsArray, tabId));
+  }, [tabId]);
+
+  return (
+    <>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabsArray.map(tab => {
+            return (
+              <li
+                data-cy="Tab"
+                className={`${tab.id === tabId ? 'is-active' : ''}`}
+              >
+                <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab === null ? 'Please select a tab' : selectedTab.content}
+      </div>
+    </>
+  );
+};
+
+const TabsPage = () => {
+  const { tabId } = useParams();
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+      <Tabs tabsArray={tabs} tabId={tabId || ''} />
+    </>
+  );
+};
 
 export const App = () => (
   <>
@@ -17,35 +83,59 @@ export const App = () => (
     >
       <div className="container">
         <div className="navbar-brand">
-          <a href="/" className="navbar-item is-active">Home</a>
-          <a href="/tabs" className="navbar-item">Tabs</a>
+          <NavLink
+            className={({ isActive }) => `navbar-item ${isActive ? 'is-active' : ''}`}
+            to="/"
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            className={({ isActive }) => `navbar-item ${isActive ? 'is-active' : ''}`}
+            to="/tabs"
+          >
+            Tabs
+          </NavLink>
         </div>
       </div>
     </nav>
 
     <div className="section">
       <div className="container">
-        <h1 className="title">Home page</h1>
-        <h1 className="title">Tabs page</h1>
-        <h1 className="title">Page not found</h1>
+        <Routes>
+          <Route
+            path="/"
+            element={<h1 className="title">Home page</h1>}
+          />
 
-        <div className="tabs is-boxed">
-          <ul>
-            <li data-cy="Tab" className="is-active">
-              <a href="#/">Tab 1</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 2</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 3</a>
-            </li>
-          </ul>
-        </div>
+          <Route
+            path="/home"
+            element={
+              <Navigate to="/" replace />
+            }
+          />
 
-        <div className="block" data-cy="TabContent">
-          Please select a tab
-        </div>
+          <Route
+            path="/tabs/"
+          >
+            <Route
+              index
+              element={<TabsPage />}
+            />
+
+            <Route
+              path=":tabId"
+              element={<TabsPage />}
+            />
+
+          </Route>
+
+          <Route
+            path="*"
+            element={<h1 className="title">Page not found</h1>}
+          />
+
+        </Routes>
       </div>
     </div>
   </>
