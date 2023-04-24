a781df05c567634c22e85cbf03555374bd65d949
Maksym Huriev <makspsih.73@gmail.com>
2023-04-24 14:26:48 +0300

    react_tabs-with-router

---
+++ src/pages/TabsPage.tsx
@@ -0,0 +1,36 @@
+import { Link, useParams } from 'react-router-dom';
+import classNames from 'classnames';
+import { Tab } from '../types/Tab';
+
+type Props = {
+  tabs: Tab[],
+};
+
+export const TabsPage:React.FC<Props> = ({ tabs }) => {
+  const { tabId = 'tab-0' } = useParams();
+  const activeTab = tabs.find(tab => tab.id === tabId) || null;
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={classNames({ 'is-active': tab.id === tabId })}
+            >
+              <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab ? activeTab.content : 'Please select a tab'}
+      </div>
+    </>
+  );
+};
