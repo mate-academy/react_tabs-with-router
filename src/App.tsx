c42d8cb1c9cd0e898fa03e4e135a9ffba7975752
Sergii Iatsenko <sergii.iatsenko@gmail.com>
2023-05-10 17:35:12 +0300

    Solutiom 1.2

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { TabsPage } from './pages/TabsPage/TabsPage';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+import { HomePage } from './pages/HomePage/HomePage';
+import { PageNotFound } from './pages/PageNotFound/PageNotFound';
 
 const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -45,13 +47,13 @@ export const App: React.FC = () => (
     <div className="section">
       <div className="container">
         <Routes>
-          <Route path="/" element={<h1 className="title">Home page</h1>} />
+          <Route path="/" element={<HomePage />} />
           <Route path="home" element={<Navigate to="/" replace />} />
           <Route path="tabs">
             <Route index element={<TabsPage tabs={tabs} />} />
             <Route path=":tabId" element={<TabsPage tabs={tabs} />} />
           </Route>
-          <Route path="*" element={<h1 className="title">Page not found</h1>} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </div>
     </div>
