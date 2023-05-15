b1bf188f963c990cbf4c0271130c5fc3a96fee87
Illia Kyselov <illya.kis99@gmail.com>
2023-05-15 19:09:41 +0300

    solution

---
+++ src/components/NavItem/NavItem.tsx
@@ -0,0 +1,41 @@
+import { Link, useParams } from 'react-router-dom';
+import classNames from 'classnames';
+import { Tab } from '../../types/Tab';
+
+type Props = {
+  tabs: Tab[],
+};
+
+export const NavItem: React.FC<Props> = ({
+  tabs,
+}) => {
+  const { tabId = 'tab-0' } = useParams();
+  const activeTab = tabs.find(tab => tab.id === tabId);
+
+  return (
+    <>
+      <h1 className="title"> Tabs page</h1>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              data-cy="Tab"
+              className={classNames({
+                'is-active': tab.id === tabId,
+              })}
+            >
+              <Link to={`/tabs/${tab.id}`}>
+                {tab.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="block" data-cy="TabContent">
+        {activeTab
+          ? activeTab.content
+          : 'Please select a tab'}
+      </div>
+    </>
+  );
+};
