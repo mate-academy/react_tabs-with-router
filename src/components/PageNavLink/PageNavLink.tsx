b4328ea292c72a6032911114a964f3dc15cdf3bc
Mykyta Holtsov <nikitaholtsov@gmail.com>
2023-05-18 20:14:11 +0300

    react fabs with router solved

---
+++ src/components/PageNavLink/PageNavLink.tsx
@@ -0,0 +1,19 @@
+import classNames from 'classnames';
+import { FC } from 'react';
+import { NavLink } from 'react-router-dom';
+
+interface Props {
+  to: string;
+  text: string;
+}
+
+export const PageNavLink: FC<Props> = ({ to, text }) => (
+  <NavLink
+    to={to}
+    className={({ isActive }) => classNames('navbar-item', {
+      'is-active': isActive,
+    })}
+  >
+    {text}
+  </NavLink>
+);
