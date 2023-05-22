fc706c3da576dc68536b7786a46b7ddfe621e744
Dima <dmitriy.movchan.02@gmail.com>
2023-05-22 23:13:10 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+import {
+  NavLink,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import classNames from 'classnames';
+import React from 'react';
+import { TabsPage } from './TabsPage';
 
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+interface Props {
+  to: string;
+  text: string;
+}
+
+export const PageNavLink: React.FC<Props> = ({ to, text }) => (
+  <NavLink
+    to={to}
+    className={({ isActive }) => classNames('navbar-item', {
+      'is-active': isActive,
+    })}
+  >
+    {text}
+  </NavLink>
+);
 
 export const App = () => (
   <>
@@ -17,35 +36,23 @@ export const App = () => (
     >
       <div className="container">
         <div className="navbar-brand">
-          <a href="/" className="navbar-item is-active">Home</a>
-          <a href="/tabs" className="navbar-item">Tabs</a>
+          <PageNavLink to="/" text="Home" />
+          <PageNavLink to="/tabs" text="Tabs" />
         </div>
       </div>
     </nav>
 
     <div className="section">
       <div className="container">
-        <h1 className="title">Home page</h1>
-        <h1 className="title">Tabs page</h1>
-        <h1 className="title">Page not found</h1>
-
-        <div className="tabs is-boxed">
-          <ul>
-            <li data-cy="Tab" className="is-active">
-              <a href="#/">Tab 1</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 2</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 3</a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="block" data-cy="TabContent">
-          Please select a tab
-        </div>
+        <Routes>
+          <Route path="/" element={<h1 className="title">Home page</h1>} />
+          <Route path="home" element={<Navigate to="/" replace />} />
+          <Route path="*" element={<h1 className="title">Page not found</h1>} />
+          <Route path="tabs">
+            <Route index element={<TabsPage />} />
+            <Route path=":tabId" element={<TabsPage />} />
+          </Route>
+        </Routes>
       </div>
     </div>
   </>
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
+import React from 'react';
 import { App } from './App';
 
 ReactDOM.render(
