9e1ecf7e89390489f4d687ddf59bd8c917ce2cf8
Dima <dmitriy.movchan.02@gmail.com>
2023-05-23 23:55:15 +0300

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,12 @@ import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
 import {
-  NavLink,
   Navigate,
   Route,
   Routes,
 } from 'react-router-dom';
-import classNames from 'classnames';
-import React from 'react';
 import { TabsPage } from './TabsPage';
-
-interface Props {
-  to: string;
-  text: string;
-}
-
-export const PageNavLink: React.FC<Props> = ({ to, text }) => (
-  <NavLink
-    to={to}
-    className={({ isActive }) => classNames('navbar-item', {
-      'is-active': isActive,
-    })}
-  >
-    {text}
-  </NavLink>
-);
+import { PageNavLink } from './NavLink';
 
 export const App = () => (
   <>
--- src/TabsPage.tsx
+++ src/TabsPage.tsx
@@ -33,7 +33,7 @@ export const TabsPage: React.FC = () => {
       </div>
 
       <div className="block" data-cy="TabContent">
-        {selectedTab ? selectedTab.content : 'Please select a tab'}
+        {selectedTab?.content || 'Please select a tab'}
       </div>
     </>
   );
