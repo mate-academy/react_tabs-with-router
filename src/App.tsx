a7d511671e9c7df9200b768bdded0548d99c5186
Nazarii Chepil <nazzikk1998@gmail.com>
2023-05-23 20:35:36 +0300

    created separate component for page not found

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Navigation } from './components/Navigation';
 import { HomePage } from './components/HomePage';
 import { TabsPage } from './components/TabsPage';
 import { Tab } from './types/Tab';
+import { PageNotFound } from './components/PageNotFound';
 
 const tabs: Tab[] = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -26,12 +27,7 @@ export const App = () => (
             <Route index element={<TabsPage tabs={tabs} />} />
             <Route path=":tabId" element={<TabsPage tabs={tabs} />} />
           </Route>
-          <Route
-            path="/*"
-            element={(
-              <h1 className="title">Page not found</h1>
-            )}
-          />
+          <Route path="/*" element={PageNotFound} />
         </Routes>
       </div>
     </div>
