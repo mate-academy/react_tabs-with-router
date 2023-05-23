4d63c7a8b0d2ce196b5e01827f5cd3c33b14f7bd
Valery Sazonov <vksazonov@gmail.com>
2023-05-23 16:10:05 +0300

    tabs exported as props

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,12 @@ import { HomePage } from './pages/HomePage/HomePage';
 import { TabsPage } from './pages/TabsPage/TabsPage';
 import { PageNotFound } from './pages/PageNotFound/PageNotFound';
 
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
 export const App = () => (
   <>
     <nav
@@ -44,8 +50,8 @@ export const App = () => (
           <Route path="/" element={<HomePage />} />
           <Route path="/home" element={<Navigate to="/" replace />} />
           <Route path="/tabs">
-            <Route index element={<TabsPage />} />
-            <Route path=":tabId" element={<TabsPage />} />
+            <Route index element={<TabsPage tabs={tabs} />} />
+            <Route path=":tabId" element={<TabsPage tabs={tabs} />} />
           </Route>
           <Route path="*" element={<PageNotFound />} />
         </Routes>
--- src/pages/TabsPage/TabsPage.tsx
+++ src/pages/TabsPage/TabsPage.tsx
@@ -1,14 +1,13 @@
 import classNames from 'classnames';
 import { FC } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { Tab } from '../../types/Tab';
 
-const tabs = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-];
+interface Props {
+  tabs: Tab[];
+}
 
-export const TabsPage:FC = () => {
+export const TabsPage: FC<Props> = ({ tabs }) => {
   const { tabId } = useParams();
   const selectedTab = tabs.find((tab) => tab.id === tabId);
 
