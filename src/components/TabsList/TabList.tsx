11911d231157823b6bc56279df7c946cc1de961f
Anastasia Radchenko <radchenckooo@gmail.com>
2023-05-25 14:04:44 +0300

    commit

---
+++ src/components/TabsList/TabList.tsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+import { Tab } from '../../types/Tab';
+
+export type Props = {
+  tabs: Tab[],
+  selectedTab: string,
+};
+
+export const TabList: React.FC<Props> = ({ tabs, selectedTab }) => (
+  <div className="tabs is-boxed">
+    <ul>
+      {tabs.map(tab => (
+        <li
+          data-cy="Tab"
+          className={classNames({
+            'is-active': tab.id === selectedTab,
+          })}
+          key={tab.id}
+        >
+          <Link to={`../${tab.id}`}>{tab.title}</Link>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
