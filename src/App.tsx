0ad0bb35e136c67277a5d7f3e1a87a29365f4572
Maksym Didyk <didyk.maksym@gmail.com>
2023-05-26 15:55:38 +0300

    Solution v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Header } from './component/Header/Header';
+import { PageNotFound } from './component/PageNotFound';
+import { HomePage } from './component/HomePage';
+import { TabsPage } from './component/TabsPage';
 
 // const tabs = [
 //   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -10,42 +15,19 @@ import './App.scss';
 
 export const App = () => (
   <>
-    {/* Also requires <html class="has-navbar-fixed-top"> */}
-    <nav
-      className="navbar is-light is-fixed-top is-mobile has-shadow"
-      data-cy="Nav"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <a href="/" className="navbar-item is-active">Home</a>
-          <a href="/tabs" className="navbar-item">Tabs</a>
-        </div>
-      </div>
-    </nav>
+    <Header />
 
     <div className="section">
       <div className="container">
-        <h1 className="title">Home page</h1>
-        <h1 className="title">Tabs page</h1>
-        <h1 className="title">Page not found</h1>
-
-        <div className="tabs is-boxed">
-          <ul>
-            <li data-cy="Tab" className="is-active">
-              <a href="#/">Tab 1</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 2</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 3</a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="block" data-cy="TabContent">
-          Please select a tab
-        </div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="tabs">
+            <Route index element={<TabsPage />} />
+            <Route path=":tabId" element={<TabsPage />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="/home" element={<Navigate to="/" replace />} />
+        </Routes>
       </div>
     </div>
   </>
---
+++ src/component/TabsList/TabsList.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+import { useParams } from 'react-router-dom';
+import { Tab } from '../../types/Tab';
+import { TabItem } from '../TabItem';
+import { TabContent } from '../TabContent';
+
+interface Props {
+  tabs: Tab[];
+}
+
+export const TabsList: FC<Props> = ({ tabs }) => {
+  const { tabId } = useParams();
+
+  const tabContent = tabs.find(tab => tab.id === tabId)?.content || '';
+
+  return (
+    <>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <TabItem tab={tab} />
+          ))}
+        </ul>
+      </div>
+
+      <TabContent tabContent={tabContent} />
+    </>
+  );
+};
