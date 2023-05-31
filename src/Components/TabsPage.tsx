ae7b9ade1ddedcb587be8bc7680925e21f121ca9
Natali <swetswet1234@gmail.com>
2023-06-01 02:17:02 +0300

    develop

---
+++ src/Components/TabsPage.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import classnames from 'classnames';
+
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+export const TabsPage: React.FC = () => {
+  const { tabId } = useParams();
+  const activeTab = tabs.find(tab => tab.id === tabId);
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              data-cy="Tab"
+              key={tab.id}
+              className={classnames({ 'is-active': tabId === tab.id })}
+            >
+              <Link to={`../${tab.id}`}>{tab.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab ? activeTab?.content : 'Please select a tab'}
+      </div>
+    </>
+  );
+};
