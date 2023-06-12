ee7a5abd79f97cdd1636c8a538709b6df1474db9
Daniel <henio09@wp.pl>
2023-06-12 17:50:09 +0200

    separate component for home and key in TabsList

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.scss';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { TabsPage } from './components/TabsPage';
+import { HomePage } from './pages/HomePage';
 
 export const App = () => (
   <>
@@ -11,7 +12,7 @@ export const App = () => (
     <div className="section">
       <div className="container">
         <Routes>
-          <Route path="/" element={<h1 className="title">Home Page</h1>} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/home" element={<Navigate to="/" replace />} />
           <Route path="/tabs" element={<TabsPage />}>
             <Route path=":selectedTab" element={<TabsPage />} />
--- src/components/TabsList.tsx
+++ src/components/TabsList.tsx
@@ -9,7 +9,11 @@ export default function TabsList({ selectedTab }: Props) {
     <div className="tabs is-boxed">
       <ul>
         {tabsData.map(tab => (
-          <TabHeader tab={tab} selectedTab={selectedTab} />
+          <TabHeader
+            tab={tab}
+            selectedTab={selectedTab}
+            key={tab.id}
+          />
         ))}
       </ul>
     </div>
