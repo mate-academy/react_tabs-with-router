f189c5a9eb95aa6f2b2f71ae0098a1fa3953f237
Roman Didusenko <albertushoffman@proton.me>
2023-06-22 15:20:08 +0300

    done

---
+++ src/TabsPage.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Link, useParams } from 'react-router-dom';
+import { Tab } from './types/Tab';
+
+interface Props {
+  tabs: Tab[];
+}
+
+export const TabsPage: React.FC<Props> = ({ tabs }) => {
+  const { tabId } = useParams();
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              data-cy="Tab"
+              className={classNames({ 'is-active': tabId === tab.id })}
+            >
+              <Link
+                to={`/tabs/${tab.id}`}
+              >
+                {tab.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="block" data-cy="TabContent">
+        {tabId
+          ? (tabs.find(tab => tab.id === tabId)?.content)
+          : ('Please select a tab')}
+      </div>
+    </>
+  );
+};
