5faeaa66efe65f5f78b21b5382d6998307d18824
Ruslan Pashkov <nothingsfading@gmail.com>
2023-06-24 00:58:38 +0300

    Add React Tabs with Router

---
+++ src/pages/TabsPage.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import { Tabs } from '../components/Tabs';
+
+export const TabsPage: FC = () => (
+  <>
+    <h1 className="title">Tabs page</h1>
+
+    <Tabs />
+  </>
+);
