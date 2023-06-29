f21f47c321ad48474c93fe5df390aa7f897dc16d
Viacheslav Saprykin <saprykinvp@gmail.com>
2023-06-29 22:24:53 +0300

    react-tabs-with-router ver.1

---
+++ src/routes/TabsPage.tsx
@@ -0,0 +1,42 @@
+import { useMemo } from 'react';
+import { Link, useOutletContext, useParams } from 'react-router-dom';
+import classNames from 'classnames';
+
+import { Tab } from '../types/Tab';
+
+export const TabsPage = () => {
+  const tabs: Tab[] = useOutletContext();
+  const { tabId = '' } = useParams();
+
+  const selectedTab = useMemo(() => {
+    return tabs.find(({ id }) => id === tabId);
+  }, [tabId]);
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={classNames(
+                { 'is-active': tabId === tab.id },
+              )}
+            >
+              <Link to={`/tabs/${tab.id}`}>
+                {tab.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab?.content || 'Please select a tab'}
+      </div>
+    </>
+  );
+};
