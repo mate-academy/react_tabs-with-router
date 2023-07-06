45eb71b4b35c5b4cfdfa3ea7d81dae74b33869d6
Vladyslav Lapshyn <lapshin.vlad96@gmail.com>
2023-07-06 23:48:58 +0300

    made react task tabs-with-router

---
+++ src/pages/TabsPage.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import { Tabs } from '../Components/Tabs';
+
+export const TabsPage: FC = () => (
+  <>
+    <h1 className="title">Tabs page</h1>
+
+    <Tabs />
+  </>
+);
