dea955981270fa5f6c4f73d96958156070ba7b6a
Vasiliy22 <hitretsukrnet@gmail.com>
2023-07-06 11:58:35 +0300

    Solved

--- src/App.tsx
+++ src/App.tsx
@@ -28,13 +28,13 @@ export const App = () => (
     <div className="section">
       <div className="container">
         <Routes>
-          <Route path="*" element={<h1 className="title">Page not found</h1>} />
           <Route path="home" element={<Navigate to="/" />} />
           <Route path="/" element={<h1 className="title">Home page</h1>} />
           <Route path="/tabs">
             <Route index element={(<TabsPage tab={tabs} />)} />
             <Route path="/tabs/:tabId" element={(<TabsPage tab={tabs} />)} />
           </Route>
+          <Route path="*" element={<h1 className="title">Page not found</h1>} />
         </Routes>
       </div>
     </div>
