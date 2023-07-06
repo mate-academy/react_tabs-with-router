8ebbba9a24598db157071e35029556d998e121bd
Ivan Marchuk <imarchuk90@gmail.com>
2023-07-06 14:37:52 +0300

    solution

---
+++ src/components/Tabs.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+import { useParams } from 'react-router-dom';
+import { Tab } from '../types/Tab';
+import { TabItem } from './TabItem';
+
+type Props = {
+  tabs: Tab[];
+};
+
+export const Tabs: FC<Props> = ({ tabs }) => {
+  const { tabId } = useParams();
+
+  const selectedTab = tabs.find(tab => tab.id === tabId);
+
+  return (
+    <>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => <TabItem tab={tab} />)}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+
+        {selectedTab?.content || <p>Please select a tab </p>}
+      </div>
+    </>
+  );
+};
