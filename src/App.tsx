a5330bce6c0378b65fc7d87b0f8dbd57bc6589e8
Oleksandr Kosohor <kosogor2007@gmail.com>
2023-07-06 14:35:19 +0300

    develop

--- src/App.tsx
+++ src/App.tsx
@@ -73,7 +73,7 @@ export const App = () => {
 
               <Route
                 path=":tabId"
-                element={<TodoContent tabs={tabs} tabId={tabId} />}
+                element={<TodoContent tabs={tabs} />}
               />
             </Route>
 
--- src/components/TodoContent.tsx
+++ src/components/TodoContent.tsx
@@ -1,17 +1,17 @@
+import { useParams } from 'react-router-dom';
 import { Tab } from '../types/Tab';
 
 interface Props {
   tabs: Tab[],
-  tabId: string;
 }
 
-export const TodoContent:React.FC<Props> = ({ tabs, tabId }) => {
-  // const { tabId = 0 } = useParams();
+export const TodoContent:React.FC<Props> = ({ tabs }) => {
+  const { tabId = 0 } = useParams();
   const selectedTab = tabs.find(tab => tab.id === tabId);
 
   return (
     <div className="block" data-cy="TabContent">
-      {selectedTab ? selectedTab.title : 'Please select a tab'}
+      {selectedTab?.title || 'Please select a tab'}
     </div>
   );
 };
