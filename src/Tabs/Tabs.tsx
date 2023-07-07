06c06fda198edcb161454d4611b986a6b9140084
Kocherha Roman <f.your.d.2017@gmail.com>
2023-07-07 19:02:54 +0300

    first commit

---
+++ src/Tabs/Tabs.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+import { Tab } from '../types/Tab';
+
+type Props = {
+  tabs: Tab[]
+  selectedTab: Tab | null
+  handleSelected: (tab: Tab) => void
+};
+
+export const Tabs: React.FC<Props> = ({
+  tabs,
+  selectedTab,
+  handleSelected,
+}) => {
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={classNames({
+                'is-active': selectedTab?.id === tab.id,
+              })}
+            >
+              <Link
+                to="#/"
+                onClick={() => handleSelected(tab)}
+              >
+                {tab.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="block" data-cy="TabContent">
+        { selectedTab
+          ? selectedTab.content
+          : 'Please select a tab'}
+      </div>
+    </>
+  );
+};
