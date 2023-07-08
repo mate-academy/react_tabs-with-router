ffa406962d6a4aee34b430e456f3b88f1edec543
Serhii Melnychenko <8melya8@gmail.com>
2023-07-08 21:53:42 +0300

    Solution

---
+++ src/components/TabsPages.tsx
@@ -0,0 +1,20 @@
+import { useParams } from 'react-router-dom';
+import { TabsBoxes, tabs } from './TabsBoxes';
+
+export const TabsPages = () => {
+  const { tabId = '' } = useParams();
+  const selectedTab = tabs.find(tab => tab.id === tabId);
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <TabsBoxes selectedTabId={tabId} />
+
+      <div className="block" data-cy="TabContent">
+        {tabId === '' ? (<>Please select a tab</>)
+          : (<>{selectedTab?.content}</>)}
+      </div>
+    </>
+  );
+};
