1f1b115691c71163a163d690d2900d8591a571ad
AlexKherson <q0992358472@gmail.com>
2023-07-10 01:52:38 +0200

    added tab router

---
+++ src/components/PageNavLink.tsx
@@ -0,0 +1,27 @@
+import classNames from 'classnames';
+import { FC } from 'react';
+import { NavLink } from 'react-router-dom';
+
+interface PageNavLinkProps {
+  to: string,
+  title: string,
+}
+
+export const PageNavLink: FC<PageNavLinkProps> = (props) => {
+  const { to, title } = props;
+
+  return (
+    <NavLink
+      to={to}
+      className={
+        ({ isActive }) => classNames(
+          'navbar-item', {
+            'is-active': isActive,
+          },
+        )
+      }
+    >
+      {title}
+    </NavLink>
+  );
+};
