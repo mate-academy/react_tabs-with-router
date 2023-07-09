239ceb0f876b543c3fde98ad70517627752ec2fa
sylcym <prusak.syl@gmail.com>
2023-07-09 19:54:21 +0200

    Update after code review

--- src/components/TabsPage/TabsPage.tsx
+++ src/components/TabsPage/TabsPage.tsx
@@ -17,15 +17,19 @@ export const TabsPage = () => {
 
       <div className="tabs is-boxed">
         <ul>
-          {tabs.map(tab => (
-            <li
-              data-cy="Tab"
-              key={tab.id}
-              className={classNames({ 'is-active': tabId === tab.id })}
-            >
-              <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
-            </li>
-          ))}
+          {tabs.map(tab => {
+            const { id, title } = tab;
+
+            return (
+              <li
+                data-cy="Tab"
+                key={id}
+                className={classNames({ 'is-active': tabId === id })}
+              >
+                <Link to={`/tabs/${id}`}>{title}</Link>
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div className="block" data-cy="TabContent">
