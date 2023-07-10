9ecbf000bb8eaaa6f31720df08468fba469587ea
Igor Shevtsov <contact.shevtsov@gmail.com>
2023-07-10 23:21:16 +0300

    Solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,45 @@
+import {
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
 import { App } from './App';
+import { HomePage } from './pages/HomePage';
+import { Tabs } from './components/Tabs/Tabs';
+import { PageNotFound } from './pages/PageNotFound';
+import {
+  HomePageRedirect,
+} from './components/HomePageRedirect/HomePageRedirect';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <PageNotFound />,
+    children: [{
+      path: '/',
+      element: <HomePage />,
+    },
+    {
+      path: '/home',
+      element: <HomePageRedirect />,
+    },
+    {
+      path: '/tabs',
+      element: <Tabs />,
+      children: [{
+        path: ':tabId',
+        element: <Tabs />,
+      }],
+    },
+    {
+      path: '/not-found',
+      element: <PageNotFound />,
+    }],
+  },
+]);
 
 ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
+  <RouterProvider router={router} />,
   document.getElementById('root'),
 );
