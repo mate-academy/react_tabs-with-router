e7160d12d89b11069229b82093fbdce8c32a3662
Viktor <vasha2020@gmail.com>
2023-07-20 18:02:48 +0100

    first commit

---
+++ src/Components/Tabs.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import classNames from 'classnames';
+import { Tab } from '../types/Tab';
+
+type TabsProps = {
+  tabs: Tab[],
+};
+
+const Tabs: React.FC<TabsProps> = ({ tabs }) => {
+  const { tabId } = useParams();
+  const selectedTab = tabs.find(tab => (
+    tab.id === tabId
+  )) || null;
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              data-cy="Tab"
+              className={classNames({ 'is-active': tab.id === tabId })}
+            >
+              <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab ? selectedTab.content : 'Please select a tab'}
+
+      </div>
+    </>
+
+  );
+}
+
+export default Tabs;
