a763042c2c42c515cbd4b6e2dbe59e3a4c0b5d37
Ivan Bilotserkivskiy <ivanbilotserkivskiy56@gmail.com>
2023-08-01 14:04:50 +0300

    tabs routes

---
+++ src/Root.tsx
@@ -0,0 +1,31 @@
+import {
+  Route,
+  Routes,
+  HashRouter,
+  Navigate,
+} from 'react-router-dom';
+import { App } from './App';
+import { TabsPage } from './pages/TabsPage';
+
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+export const Root = () => (
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<h1 className="title">Home page</h1>} />
+        <Route path="/home" element={<Navigate to="/" />} />
+        <Route path="tabs">
+          <Route index element={<TabsPage tabs={tabs} />} />
+          <Route path=":tabId" element={<TabsPage tabs={tabs} />} />
+        </Route>
+        <Route path="*" element={<h1 className="title">Page not found</h1>} />
+      </Route>
+    </Routes>
+    <App />
+  </HashRouter>
+);
