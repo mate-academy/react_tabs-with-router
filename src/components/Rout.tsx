5f5d655a21c038ce5a8e746af0db68a9a6171d44
Vitalii Drabynko <vpdrabynko@gmail.com>
2023-08-03 12:30:58 +0300

    solution

---
+++ src/components/Rout.tsx
@@ -0,0 +1,29 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { TabsPage } from './TabsPage';
+
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+export const Rout = () => {
+  return (
+    <div className="section">
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<h1 className="title">Home page</h1>} />
+          <Route path="home" element={<Navigate to="/" />} />
+          <Route path="tabs">
+            <Route index element={<TabsPage tabs={tabs} />} />
+            <Route path=":tabId" element={<TabsPage tabs={tabs} />} />
+          </Route>
+          <Route
+            path="*"
+            element={<h1 className="title">Page not found</h1>}
+          />
+        </Routes>
+      </div>
+    </div>
+  );
+};
