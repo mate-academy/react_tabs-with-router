6138b7c903828675d6d1ed928fde3d51ab86c7b8
Alyona Sarapina <alyona.sarapina@gmail.com>
2023-08-03 12:57:38 +0100

    deleted comments

--- src/components/TabInfo.tsx
+++ src/components/TabInfo.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
 import classNames from 'classnames';
 import { Link, useParams } from 'react-router-dom';
