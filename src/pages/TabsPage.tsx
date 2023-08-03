997a8c79b9abd4aa71a264dbad66cf1cdbd6220d
Iryna Shtynda <ira.shtynda@gmail.com>
2023-08-03 20:13:00 +0300

    add solution

---
+++ src/pages/TabsPage.tsx
@@ -0,0 +1,31 @@
+import { useParams } from 'react-router-dom';
+import { Tabs } from '../components/Tabs';
+
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+export const TabsPage: React.FC = () => {
+  const { tabId } = useParams();
+
+  const selectedTab = tabs.find(({ id }) => id === tabId) || null;
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <Tabs
+        tabs={tabs}
+        selectedTab={tabId}
+      />
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab
+          ? selectedTab.content
+          : 'Please select a tab'}
+      </div>
+    </>
+  );
+};
