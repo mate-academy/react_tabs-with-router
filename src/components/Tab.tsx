107ca279aab2088d5df412fdbaf0adda8447f26b
Zoriana Melnychuk <zoryashka3002@gmail.com>
2023-08-04 02:55:21 +0200

    add task solution

---
+++ src/components/Tab.tsx
@@ -0,0 +1,26 @@
+import classNames from 'classnames';
+import { Link } from 'react-router-dom';
+import { Tab } from '../types/Tab';
+
+interface Props {
+  tab: Tab,
+  tabId: string,
+}
+
+export const TabItem: React.FC<Props> = ({ tab, tabId }) => {
+  return (
+    <li
+      data-cy="Tab"
+      key={tab.id}
+      className={classNames({
+        'is-active': tabId === tab.id,
+      })}
+    >
+      <Link
+        to={`../${tab.id}`}
+      >
+        {tab.title}
+      </Link>
+    </li>
+  );
+};
---
+++ src/components/TabsList.tsx
@@ -0,0 +1,38 @@
+import { useParams } from 'react-router-dom';
+import { TabItem } from './Tab';
+import { Tab } from '../types/Tab';
+
+export interface Props {
+  tabs: Tab[];
+}
+
+export const TabsList: React.FC<Props> = ({ tabs }) => {
+  const { tabId } = useParams();
+  const selectedTab = tabId ? tabs.find(tab => tabId === tab.id) : null;
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            return (
+              <TabItem
+                tab={tab}
+                tabId={tabId || ''}
+              />
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {
+          selectedTab
+            ? selectedTab.content
+            : 'Please select a tab'
+        }
+      </div>
+    </>
+  );
+};
