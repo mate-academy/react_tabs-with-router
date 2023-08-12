984af53b533182266f6aa4476c6d32216b837afb
Vitalii Hovenko <vet.hov.main@gmail.com>
2023-08-12 15:54:49 +0300

    tabs

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,24 @@
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { App } from './App';
+import { Tabs } from './components/Tabs';
+import { Home } from './components/Home';
+import { Error } from './components/Error';
 
 ReactDOM.render(
   <HashRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+
+        <Route path="tabs">
+          <Route index element={<Tabs />} />
+          <Route path=":tabId" element={<Tabs />} />
+        </Route>
+
+        <Route path="*" element={<Error />} />
+      </Route>
+    </Routes>
   </HashRouter>,
   document.getElementById('root'),
 );
