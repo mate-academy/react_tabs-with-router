206d5a2b85433194eb3f1fd94b5df95eac17cdb9
AndruhaMan <andreysut12@gmail.com>
2023-08-13 00:07:24 +0300

    The task is completed

---
+++ src/tabs.ts
@@ -0,0 +1,7 @@
+import { Tab } from './types/Tab';
+
+export const tabs: Tab[] = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
