4e83774fcf80040d25c9fd3320d1e491eefcb01a
Diana Tuz <dianatuz12@gmail.com>
2023-08-25 12:40:25 +0300

    fix

--- src/pages/Tabs.tsx
+++ src/pages/Tabs.tsx
@@ -1,13 +1,8 @@
 import classNames from 'classnames';
 import { Link, useParams } from 'react-router-dom';
+import { tabs } from './TabsArr';
 
 export const Tabs = () => {
-  const tabs = [
-    { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-    { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-    { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-  ];
-
   const { tabId } = useParams();
   const selectedTab = tabs.find(tab => tab.id === tabId);
 
