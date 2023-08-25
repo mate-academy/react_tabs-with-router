35757e103b7daa82354f381c109748f79346a830
Lukasz Warda <lukaszwarda1989@gmail.com>
2023-08-25 08:36:40 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,6 @@ export const App = () => {
               className={({ isActive }) => {
                 return isActive ? 'navbar-item is-active' : 'navbar-item';
               }}
-
             >
               Home
             </NavLink>
