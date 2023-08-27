e39c4fa27ccb8fafe91ce627f0ca1c6070b4ed26
Alla Gutsul <allagutsul1990@gmail.com>
2023-08-27 14:41:10 +0300

    add some files

---
+++ src/components/Tabs/Tabs.tsx
@@ -0,0 +1,32 @@
+import { Link, useParams } from 'react-router-dom';
+import cn from 'classnames';
+import { tabs } from '../../data/tabs';
+
+export const Tabs = () => {
+  const { tabId } = useParams();
+  const selectedTab = tabs.find(tab => tab.id === tabId);
+
+  return (
+    <>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={cn({
+                'is-active': tab.id === tabId,
+              })}
+            >
+              <Link to={`../${tab.id}`}>{tab.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab?.content || 'Please select a tab'}
+      </div>
+    </>
+  );
+};
