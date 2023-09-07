c583d651a72a9cfd4f46747d7a6de1792a671628
Victor Buldenko <gookidoo@gmail.com>
2023-09-07 19:03:38 +0300

    RRD

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+export const Home: React.FC = () => {
+  return (
+    <div className="section">
+      <div className="container">
+        <h1 className="title">Home page</h1>
+      </div>
+    </div>
+  );
+};
---
+++ src/pages/TabsPage.tsx
@@ -0,0 +1,12 @@
+import { Tabs } from '../components/Tabs';
+
+export const TabsPage: React.FC = () => {
+  return (
+    <div className="section">
+      <div className="container">
+        <h1 className="title">Tabs page</h1>
+        <Tabs />
+      </div>
+    </div>
+  );
+};
