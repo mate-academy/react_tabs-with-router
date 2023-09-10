22d63ab4ff7936ee634db7ee3d08849871f7c713
Denis Kononenko <deniskononenko88@gmail.com>
2023-09-10 22:21:54 +0300

    add Navigate

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,4 +1,9 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import { App } from './App';
 import { Tabs } from './components/Tabs';
 
@@ -10,6 +15,7 @@ export const Root = () => (
         <Route path="tabs" element={<Tabs />}>
           <Route path=":tabId" element={<Tabs />} />
         </Route>
+        <Route path="home" element={<Navigate to="/" />} />
         <Route path="*" element={<h1 className="title">Page not found</h1>} />
       </Route>
     </Routes>
