f8804789b055f11c0b7b97eca477435b868b32ce
Olha Momot <helgammt@gmail.com>
2023-09-28 01:34:56 +0300

    implemented navigation

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,31 @@
-import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import {
+  HashRouter,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import { HomePage } from './components/HomePage';
+import { TabsPage } from './components/TabsPage';
 import { App } from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root') as HTMLElement;
+
+const Root = () => (
   <HashRouter>
-    <App />
-  </HashRouter>,
-  document.getElementById('root'),
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path="tabs">
+          <Route index element={<TabsPage />} />
+          <Route path=":tabId" element={<TabsPage />} />
+        </Route>
+        <Route path="home" element={<Navigate to=".." />} />
+        <Route path="*" element={<h1 className="title">Page not found</h1>} />
+      </Route>
+    </Routes>
+  </HashRouter>
 );
+
+createRoot(container)
+  .render(<Root />);
