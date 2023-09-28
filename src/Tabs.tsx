c09c83e4644b7f86b61a9814d978e0d78fd7e073
irinagos <irynagos47@gmail.com>
2023-09-28 15:37:15 +0300

    added logic for tabs with router

---
+++ src/Tabs.tsx
@@ -0,0 +1,39 @@
+import classNames from 'classnames';
+import { Link, useParams } from 'react-router-dom';
+
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+export const Tabs = () => {
+  const { tabId } = useParams();
+  const foundTab = tabs.find((tab) => tab.id === tabId) || 0;
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map((tab) => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={classNames({
+                'is-active': tabId === tab.id,
+              })}
+            >
+              <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {foundTab ? foundTab.content : 'Please select a tab'}
+      </div>
+    </>
+  );
+};
