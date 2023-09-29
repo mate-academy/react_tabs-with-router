5b655fc8c1769bba3869093a001cfa481f0db2a6
Viktoriia Tkachyk <viko66666vs@gmail.com>
2023-09-29 21:02:26 +0300

    Solution

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+export const HomePage = () => {
+  return (
+    <>
+      <h1 className="title">Home page</h1>
+    </>
+  );
+};
