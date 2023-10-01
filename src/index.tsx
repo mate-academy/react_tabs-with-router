2ba894580ef77613cca9c8f084ecc76c11a06e59
Andrzej Pietkiewicz <pietan@me.com>
2023-10-01 22:50:15 +0200

    ReactDOM.render changed to createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
-import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
 import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import { App } from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
