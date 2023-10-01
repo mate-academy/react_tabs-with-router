e6ff1362da97dcb054e333c376a8ad620ef5b278
Andrzej Pietkiewicz <pietan@me.com>
2023-10-01 22:26:12 +0200

    TabsWithRouter Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,76 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+import {
+  NavLink, Route, Routes, useNavigate,
+} from 'react-router-dom';
+import { useEffect } from 'react';
+import { Tabs } from './components/Tabs';
+import { Home } from './components/Home';
+import { Error } from './components/Error';
 
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
 
-export const App = () => (
-  <>
-    {/* Also requires <html class="has-navbar-fixed-top"> */}
-    <nav
-      className="navbar is-light is-fixed-top is-mobile has-shadow"
-      data-cy="Nav"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <a href="/" className="navbar-item is-active">Home</a>
-          <a href="/tabs" className="navbar-item">Tabs</a>
+export const App = () => {
+  function RedirectHome() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      navigate('/');
+    }, [navigate]);
+
+    return null;
+  }
+
+  return (
+    <>
+      {/* Also requires <html class="has-navbar-fixed-top"> */}
+      <nav
+        className="navbar is-light is-fixed-top is-mobile has-shadow"
+        data-cy="Nav"
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <NavLink
+              to="/"
+              className={
+                ({ isActive }) => (
+                  isActive
+                    ? 'navbar-item is-active'
+                    : 'navbar-item')
+              }
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/tabs"
+              className={
+                ({ isActive }) => (
+                  isActive
+                    ? 'navbar-item is-active'
+                    : 'navbar-item')
+              }
+            >
+              Tabs
+            </NavLink>
+          </div>
+        </div>
+      </nav>
+
+      <div className="section">
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<RedirectHome />} />
+            <Route path="/tabs/*" element={<Tabs tabs={tabs} />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
         </div>
       </div>
-    </nav>
-
-    <div className="section">
-      <div className="container">
-        <h1 className="title">Home page</h1>
-        <h1 className="title">Tabs page</h1>
-        <h1 className="title">Page not found</h1>
-
-        <div className="tabs is-boxed">
-          <ul>
-            <li data-cy="Tab" className="is-active">
-              <a href="#/">Tab 1</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 2</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 3</a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="block" data-cy="TabContent">
-          Please select a tab
-        </div>
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
+import React from 'react';
 import { App } from './App';
 
 ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
   document.getElementById('root'),
 );
