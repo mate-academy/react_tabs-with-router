fb6ab6eefec5f122ff1010b3e2c8d8c5afc128e0
Hubert Klamann <hubert.klamann1@gmail.com>
2023-10-02 16:43:39 +0200

    task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,92 @@
+import {
+  Link, Route, Routes, Navigate, useNavigate,
+} from 'react-router-dom';
+import cn from 'classnames';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+import { useEffect, useState } from 'react';
+import { TabPage } from './pages/TabPage';
 
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+export const App = () => {
+  const navigate = useNavigate();
+  const [active, setActive] = useState('home');
 
-export const App = () => (
-  <>
-    {/* Also requires <html class="has-navbar-fixed-top"> */}
-    <nav
-      className="navbar is-light is-fixed-top is-mobile has-shadow"
-      data-cy="Nav"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <a href="/" className="navbar-item is-active">Home</a>
-          <a href="/tabs" className="navbar-item">Tabs</a>
+  useEffect(() => {
+    const currentPath = window.location.hash;
+
+    if (currentPath === '#/') {
+      setActive('home');
+    } else if (currentPath.startsWith('#/tabs')) {
+      setActive('tabs');
+    }
+  }, [navigate]);
+
+  return (
+    <>
+      {/* Also requires <html class="has-navbar-fixed-top"> */}
+      <nav
+        className="navbar is-light is-fixed-top is-mobile has-shadow"
+        data-cy="Nav"
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <Link
+              to="/home"
+              className={cn('navbar-item', {
+                'is-active': active === 'home',
+              })}
+            >
+              Home
+            </Link>
+            <Link
+              to="/tabs"
+              className={cn('navbar-item', {
+                'is-active': active === 'tabs',
+              })}
+            >
+              Tabs
+            </Link>
+          </div>
+        </div>
+      </nav>
+
+      <div className="section">
+        <div className="container">
+          <Routes>
+            <Route path="">
+              <Route index element={<h1 className="title">Home page</h1>} />
+              <Route
+                path="/home"
+                element={(
+                  <>
+                    <h1 className="title">
+                      Home page
+                    </h1>
+                    <Navigate to="/" />
+                  </>
+                )}
+              />
+              <Route
+                path="tabs/*"
+                element={(
+                  <>
+                    <TabPage />
+                  </>
+                )}
+              />
+              <Route
+                path="tabs/:tabId"
+                element={<TabPage />}
+              />
+              <Route
+                path="*"
+                element={<h1 className="title">Page not found</h1>}
+              />
+            </Route>
+          </Routes>
         </div>
       </div>
-    </nav>
-
-    <div className="section">
-      <div className="container">
-        <h1 className="title">Home page</h1>
-        <h1 className="title">Tabs page</h1>
-        <h1 className="title">Page not found</h1>
-
-        <div className="tabs is-boxed">
-          <ul>
-            <li data-cy="Tab" className="is-active">
-              <a href="#/">Tab 1</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 2</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 3</a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="block" data-cy="TabContent">
-          Please select a tab
-        </div>
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
