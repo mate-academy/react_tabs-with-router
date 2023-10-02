49d3be6198088de462df6f61b4e1f5807426ff6c
Hubert Klamann <hubert.klamann1@gmail.com>
2023-10-02 17:02:04 +0200

    task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,60 @@
 import {
-  Link, Route, Routes, Navigate, useNavigate,
+  Route, Routes, Navigate, NavLink,
 } from 'react-router-dom';
 import cn from 'classnames';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-import { useEffect, useState } from 'react';
 import { TabPage } from './pages/TabPage';
 
-export const App = () => {
-  const navigate = useNavigate();
-  const [active, setActive] = useState('home');
-
-  useEffect(() => {
-    const currentPath = window.location.hash;
-
-    if (currentPath === '#/') {
-      setActive('home');
-    } else if (currentPath.startsWith('#/tabs')) {
-      setActive('tabs');
-    }
-  }, [navigate]);
-
-  return (
-    <>
-      {/* Also requires <html class="has-navbar-fixed-top"> */}
-      <nav
-        className="navbar is-light is-fixed-top is-mobile has-shadow"
-        data-cy="Nav"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <Link
-              to="/home"
-              className={cn('navbar-item', {
-                'is-active': active === 'home',
-              })}
-            >
-              Home
-            </Link>
-            <Link
-              to="/tabs"
-              className={cn('navbar-item', {
-                'is-active': active === 'tabs',
-              })}
-            >
-              Tabs
-            </Link>
-          </div>
-        </div>
-      </nav>
-
-      <div className="section">
-        <div className="container">
-          <Routes>
-            <Route path="">
-              <Route index element={<h1 className="title">Home page</h1>} />
-              <Route
-                path="/home"
-                element={(
-                  <>
-                    <h1 className="title">
-                      Home page
-                    </h1>
-                    <Navigate to="/" />
-                  </>
-                )}
-              />
-              <Route
-                path="tabs/*"
-                element={(
-                  <>
-                    <TabPage />
-                  </>
-                )}
-              />
-              <Route
-                path="tabs/:tabId"
-                element={<TabPage />}
-              />
-              <Route
-                path="*"
-                element={<h1 className="title">Page not found</h1>}
-              />
-            </Route>
-          </Routes>
+export const App = () => (
+  <html lang="en" className="has-navbar-fixed-top">
+    <nav
+      className="navbar is-light is-fixed-top is-mobile has-shadow"
+      data-cy="Nav"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <NavLink
+            to="/"
+            className={({ isActive }) => cn('navbar-item',
+              { 'is-active': isActive })}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/tabs"
+            className={({ isActive }) => cn('navbar-item',
+              { 'is-active': isActive })}
+          >
+            Tabs
+          </NavLink>
         </div>
       </div>
-    </>
-  );
-};
+    </nav>
+
+    <div className="section">
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<h1 className="title">Home page</h1>} />
+          <Route
+            path="/home"
+            element={<Navigate to="/" />}
+          />
+          <Route
+            path="tabs/*"
+            element={<TabPage />}
+          />
+          <Route
+            path="tabs/:tabId"
+            element={<TabPage />}
+          />
+          <Route
+            path="*"
+            element={<h1 className="title">Page not found</h1>}
+          />
+        </Routes>
+      </div>
+    </div>
+  </html>
+);
