8a350bd559239ad69096a81e9d0e5d9120b233b7
Oksana Vasyliuk <oksana.vasyliuk13@gmail.com>
2023-10-03 15:12:53 +0200

    Solution

---
+++ src/components/TabsPage.tsx
@@ -0,0 +1,53 @@
+import classNames from 'classnames';
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Tab } from '../types/Tab';
+
+const tabs: Tab[] = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+export const TabsPage: React.FC = () => {
+  const { tabId } = useParams();
+  const selectedTab = tabs.find(tab => tab.id === tabId);
+
+  return (
+    <>
+      <h1 className="title">
+        Tabs page
+      </h1>
+
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            const { id, title } = tab;
+
+            return (
+              <>
+                <li
+                  key={id}
+                  data-cy="Tab"
+                  className={classNames('', {
+                    'is-active': selectedTab?.id === id,
+                  })}
+                >
+                  <Link to={`../${id}`}>{title}</Link>
+                </li>
+              </>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {!selectedTab ? (
+          'Please select a tab'
+        ) : (
+          selectedTab.content
+        )}
+      </div>
+    </>
+  );
+};
