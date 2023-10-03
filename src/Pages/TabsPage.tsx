7ed0d598d91b3785e7e391f350ccaba5c22b6285
danulyk05 <ivan.danulyk2016@gmail.com>
2023-10-03 23:22:19 +0200

    add routers

---
+++ src/Pages/TabsPage.tsx
@@ -0,0 +1,44 @@
+import { useParams, Link } from 'react-router-dom';
+import cn from 'classnames';
+
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+export const TabsPage = () => {
+  const { tabId } = useParams();
+  const selectedTab = tabs.find(tab => tab.id === tabId);
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(({ id, title }) => (
+            <li
+              data-cy="Tab"
+              className={cn({
+                'is-active': id === selectedTab?.id,
+              })}
+              key={id}
+            >
+              <Link
+                to={`/tabs/${id}`}
+                data-cy="TabLink"
+              >
+                {title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab?.content || 'Please select a tab'}
+      </div>
+    </>
+  );
+};
