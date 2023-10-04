03cfb497e209e3dcb53fa44423cf82faa06ff4b1
haduigon <haduigon@gmail.com>
2023-10-04 14:11:25 +0300

    deleted context

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,12 +1,10 @@
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import { App } from './App';
-import { TabProvider } from './components/Tabs/TabContext';
 import { HomePage } from './components/HomePage';
 import { Tabs } from './components/Tabs';
 
 export const Root = () => (
   <HashRouter>
-    <TabProvider>
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<HomePage />} />
@@ -20,6 +18,5 @@ export const Root = () => (
           />
         </Route>
       </Routes>
-    </TabProvider>
   </HashRouter>
 );
