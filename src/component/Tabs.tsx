d4d45c8cee87c4d6087327a966ce0231fd85b3a5
Anna Grechana <grechanaanna@gmial.com>
2023-10-15 17:56:40 +0300

    1st version

---
+++ src/component/Tabs.tsx
@@ -0,0 +1,37 @@
+import { Link, useParams } from 'react-router-dom';
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import classNames from 'classnames';
+
+export const Tabs = () => {
+  const tabs = [
+    { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+    { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+    { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+  ];
+
+  const { tabId } = useParams();
+  const selectTab = tabs.find(tab => tab.id === tabId);
+
+  return (
+    <>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              data-cy="Tab"
+              className={classNames({ 'is-active': selectTab?.id === tab.id })}
+              key={tab.id}
+            >
+              <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectTab ? selectTab?.content : 'Please select a tab'}
+      </div>
+    </>
+  );
+};
