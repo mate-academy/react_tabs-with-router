988b3714d5b322c60cfc3b388333be69cfeb187f
Anna Grechana <grechanaanna@gmial.com>
2023-10-15 18:13:28 +0300

    2nd version

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,12 @@ import './App.scss';
 import classNames from 'classnames';
 import { Tabs } from './component/Tabs';
 
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
 const getLinkClass = ({ isActive }: { isActive: boolean }) => classNames(
   'navbar-item', {
     'is-active': isActive,
@@ -18,7 +24,6 @@ const getLinkClass = ({ isActive }: { isActive: boolean }) => classNames(
 
 export const App = () => (
   <>
-    {/* Also requires <html class="has-navbar-fixed-top"> */}
     <nav
       className="navbar is-light is-fixed-top is-mobile has-shadow"
       data-cy="Nav"
@@ -36,8 +41,8 @@ export const App = () => (
         <Route path="/" element={<h1 className="title">Home page</h1>} />
         <Route path="/home" element={<Navigate to="/" />} />
         <Route path="tabs">
-          <Route index element={<Tabs />} />
-          <Route path=":tabId" element={<Tabs />} />
+          <Route index element={<Tabs tabs={tabs} />} />
+          <Route path=":tabId" element={<Tabs tabs={tabs} />} />
         </Route>
         <Route path="*" element={<h1 className="title">Page not found</h1>} />
       </Routes>
--- src/component/Tabs.tsx
+++ src/component/Tabs.tsx
@@ -1,27 +1,22 @@
 import { Link, useParams } from 'react-router-dom';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-import classNames from 'classnames';
+import { Tab } from '../types/Tab';
 
-export const Tabs = () => {
-  const tabs = [
-    { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-    { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-    { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-  ];
-
+export const Tabs = ({ tabs }: { tabs: Tab[] }) => {
   const { tabId } = useParams();
-  const selectTab = tabs.find(tab => tab.id === tabId);
+  const selectedTab = tabs.find(tab => tab.id === tabId);
 
   return (
     <>
+      <h1 className="title">Tabs page</h1>
       <div className="tabs is-boxed">
         <ul>
-          {tabs.map(tab => (
+          {tabs.map((tab) => (
             <li
               data-cy="Tab"
-              className={classNames({ 'is-active': selectTab?.id === tab.id })}
               key={tab.id}
+              className={tab.id === tabId ? 'is-active' : ''}
             >
               <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
             </li>
@@ -30,7 +25,7 @@ export const Tabs = () => {
       </div>
 
       <div className="block" data-cy="TabContent">
-        {selectTab ? selectTab?.content : 'Please select a tab'}
+        {selectedTab ? selectedTab.content : 'Please select a tab'}
       </div>
     </>
   );
