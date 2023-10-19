0523d4f765f685ede4ea23c70b912481c245352e
Roman Hrytsiuk <vasylguk7@gmail.com>
2023-10-19 23:24:13 +0300

    First commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,28 @@
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import {
+  HashRouter,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
 import { App } from './App';
+import { Tabs } from './components/Tabs';
+import { HomePage } from './components/HomePage';
+import { NotFound } from './components/NotFound';
 
 ReactDOM.render(
   <HashRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path="home" element={<Navigate to="/" />} />
+        <Route path="tabs">
+          <Route index element={<Tabs />} />
+          <Route path=":tabId" element={<Tabs />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   </HashRouter>,
   document.getElementById('root'),
 );
