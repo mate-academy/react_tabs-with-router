ce246e80f8f81a9026f2b333cd79c6e67f2f8eaf
Iki Lo <ikilo@Ikis-MacBook-Air.local>
2023-10-30 18:24:12 +0200

    solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,26 @@
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import {
+  HashRouter,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import { App } from './App';
+import { Tabs } from './pages/Tabs';
 
 ReactDOM.render(
   <HashRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="home" element={<Navigate to="/" />} />
+        <Route index element={<h1 className="title">Home page</h1>} />
+        <Route path="tabs/:tabId?" element={<Tabs />}>
+          <Route index element={<h1 className="title">Tabs page</h1>} />
+        </Route>
+
+        <Route path="*" element={<h1 className="title">Page not found</h1>} />
+      </Route>
+    </Routes>
   </HashRouter>,
   document.getElementById('root'),
 );
