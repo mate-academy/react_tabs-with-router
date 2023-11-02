e8f2a99bbe198fa230275f9177e841e09912cc02
IhorVerba <ihor.verbivskyi@gmail.com>
2023-11-02 13:52:34 +0200

    solution

---
+++ src/components/TabList.tsx
@@ -0,0 +1,33 @@
+import { useContext } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { TabsContext } from '../store/TabsContext';
+
+export const TabList = () => {
+  const { tabs } = useContext(TabsContext);
+  const { tabId } = useParams();
+  const selectedTab = tabs.find(tab => tab.id === tabId);
+
+  return (
+    <div className="container">
+      <h1 className="title">Tabs page</h1>
+
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={tab.id === tabId ? 'is-active' : ''}
+            >
+              <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab?.content || 'Please select a tab'}
+      </div>
+    </div>
+  );
+};
