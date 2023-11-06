794f0d257162e1b55b74ddf89d4d0462fd43fb5e
dima.rogkov <dima.rogkov@cheitgroup.com>
2023-11-06 16:46:35 +0200

    add solution

---
+++ src/Root.tsx
@@ -0,0 +1,10 @@
+import { HashRouter as Router } from 'react-router-dom';
+import { App } from './App';
+
+export const Root = () => {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+};
