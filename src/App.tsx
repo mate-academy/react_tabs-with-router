082839d394d6473ce73778494374f11e0822bbce
BetterSol <94358102+BetterSol@users.noreply.github.com>
2023-11-14 22:31:23 +0200

    eslintrc and readme files updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-import { Routes, Route, Link } from 'react-router-dom';
+import {
+  Routes, Route, Link, NavLink,
+} from 'react-router-dom';
 
 // const tabs = [
 //   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -9,48 +11,62 @@ import { Routes, Route, Link } from 'react-router-dom';
 //   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 // ];
 
-export const App = () => (
-  <>
-    {/* Also requires <html class="has-navbar-fixed-top"> */}
-    <nav
-      className="navbar is-light is-fixed-top is-mobile has-shadow"
-      data-cy="Nav"
-    >
-      <div className="container">
-        <Routes>
-          <Route />
-        </Routes>
-        <div className="navbar-brand">
-          <Link to="/" className="navbar-item is-active">Home</Link>
-          <Link to="/tabs" className="navbar-item">Tabs</Link>
+export const App = () => {
+  // const navigate = useNavigate();
+
+  // const handler = () => {
+  //   navigate('/tabs')
+  // }
+
+  return (
+    <>
+      {/* Also requires <html class="has-navbar-fixed-top"> */}
+      <nav
+        className="navbar is-light is-fixed-top is-mobile has-shadow"
+        data-cy="Nav"
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <NavLink to="/" className="navbar-item is-active">Home</NavLink>
+            <NavLink to="/tabs" className="navbar-item">Tabs</NavLink>
+            {/* <button></button> */}
+          </div>
+        </div>
+      </nav>
+
+      <div className="section">
+        <div className="container">
+          <Routes>
+            <Route
+              path="*"
+              element={<h1 className="title">Page not found</h1>}
+            />
+            <Route path="/" element={<h1 className="title">Home page</h1>} />
+            <Route path="tabs">
+              <Route index element={<h1 className="title">Tabs page</h1>} />
+              {/* <Route path=":tabId" element={} /> */}
+            </Route>
+          </Routes>
+
+          <div className="tabs is-boxed">
+            <ul>
+              <li data-cy="Tab" className="is-active">
+                <Link to="/tabs/1">Tab 1</Link>
+              </li>
+              <li data-cy="Tab">
+                <Link to="/tabs/2">Tab 2</Link>
+              </li>
+              <li data-cy="Tab">
+                <Link to="/tabs/3">Tab 3</Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className="block" data-cy="TabContent">
+            Please select a tab
+          </div>
         </div>
       </div>
-    </nav>
-
-    <div className="section">
-      <div className="container">
-        <h1 className="title">Home page</h1>
-        <h1 className="title">Tabs page</h1>
-        <h1 className="title">Page not found</h1>
-
-        <div className="tabs is-boxed">
-          <ul>
-            <li data-cy="Tab" className="is-active">
-              <Link to="#/">Tab 1</Link>
-            </li>
-            <li data-cy="Tab">
-              <Link to="#/">Tab 2</Link>
-            </li>
-            <li data-cy="Tab">
-              <Link to="#/">Tab 3</Link>
-            </li>
-          </ul>
-        </div>
-
-        <div className="block" data-cy="TabContent">
-          Please select a tab
-        </div>
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
