0f80d786182ee23d786e199fdaf6267a3764fbb4
andrew gidrovich <mr.androvich55555@gmail.com>
2023-11-20 20:02:15 +0200

    add task solution

---
+++ src/pages/TabsPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Tabs from '../components/Tabs';
+
+const TabsPage: React.FC = () => {
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+      <Tabs />
+    </>
+  );
+};
+
+export default TabsPage;
