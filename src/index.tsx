de6819e858e10a7018992ddb43b10bcef0bd68f6
Nazar Kolomiets <lexnazar91@gmail.com>
2023-11-28 17:45:33 +0300

    add task solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,27 @@
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import {
+  HashRouter, Navigate, Route, Routes,
+} from 'react-router-dom';
 import { App } from './App';
+import { TabsPage } from './TabsPage';
+
+export const Root = () => (
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<h1 className="title">Home page</h1>} />
+        <Route path="/home" element={<Navigate to="/" replace />} />
+        <Route path="tabs">
+          <Route index element={<TabsPage />} />
+          <Route path=":tabId?" element={<TabsPage />} />
+        </Route>
+        <Route path="*" element={<h1 className="title">Page not found</h1>} />
+      </Route>
+    </Routes>
+  </HashRouter>
+);
 
 ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
+  <Root />,
   document.getElementById('root'),
 );
