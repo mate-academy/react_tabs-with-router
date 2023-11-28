86671e8001700ec886d392e9c5b61c72a37a96e8
Danylo Artomov <daniilart01@gmail.com>
2023-11-28 23:15:59 +0200

    impl

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,24 @@
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import {
+  HashRouter,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import { App } from './App';
+import { TabsPage } from './components/TabsPage';
 
 ReactDOM.render(
   <HashRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="tabs">
+          <Route path=":tabId?" index element={<TabsPage />} />
+        </Route>
+        <Route path="*" />
+      </Route>
+      <Route path="/home" element={<Navigate to="/" />} />
+    </Routes>
   </HashRouter>,
   document.getElementById('root'),
 );
