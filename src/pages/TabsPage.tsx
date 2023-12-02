b6c71751e77932c273fd5db6ffea9bcfa8c9f21e
«Maria» <nedymai@gmail.com>
2023-12-02 09:03:21 +0200

    solution

---
+++ src/pages/TabsPage.tsx
@@ -0,0 +1,15 @@
+import { Tabs } from '../components/Tabs';
+
+export const TabsPage = () => {
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <div className="section">
+        <div className="container">
+          <Tabs />
+        </div>
+      </div>
+    </>
+  );
+};
