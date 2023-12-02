d5f29d734deccf5373c1fff42966e807a226b975
Natalia Maryshko <n.maryshko@gmail.com>
2023-12-02 21:22:03 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -36,31 +36,5 @@ export const App = () => (
         <Route path="*" element={<h1 className="title">Page not found</h1>} />
       </Routes>
     </div>
-
-    {/* <div className="section">
-      <div className="container">
-        <h1 className="title">Home page</h1>
-        <h1 className="title">Tabs page</h1>
-        <h1 className="title">Page not found</h1>
-
-        <div className="tabs is-boxed">
-          <ul>
-            <li data-cy="Tab" className="is-active">
-              <a href="#/">Tab 1</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 2</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 3</a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="block" data-cy="TabContent">
-          Please select a tab
-        </div>
-      </div>
-    </div> */}
   </>
 );
