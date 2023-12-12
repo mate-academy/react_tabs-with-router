cfc1257490cfa7f865bafc5eeeda264e90d65b79
Dmytro Krasnoholovenko <errisi.v@gmail.com>
2023-12-12 19:48:13 +0200

    result

---
+++ src/pages/TabsPage.tsx
@@ -0,0 +1,16 @@
+import { TabList } from '../components/Tabs/TabList';
+import { Tab } from '../types/Tab';
+
+const tabs: Tab[] = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+export const TabsPage: React.FC = () => (
+  <>
+    <h1 className="title">Tabs page</h1>
+
+    <TabList tabs={tabs} />
+  </>
+);
