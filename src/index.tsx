c804471e1071a21055cea735ae88170b250ad44e
Valeria Oleynik <oleynik.valeria27@gmail.com>
2023-12-15 15:30:37 +0200

    add task solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import { App } from './App';
+
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './App.scss';
+import { Root } from './Root';
 
 createRoot(document.getElementById('root') as HTMLElement)
-  .render(
-    <HashRouter>
-      <App />
-    </HashRouter>,
-  );
+  .render(<Root />);
