69f04bc8d9e2d35ded40b3b711b663647b53ddfe
Maksym Svoboda <maksim4ik71@gmail.com>
2023-12-19 11:06:09 +0200

    made solution for tabs with router

---
+++ src/components/Tabs/Tabs.tsx
@@ -0,0 +1,52 @@
+import cn from 'classnames';
+import { useState } from 'react';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import { Tab } from '../../types/Tab';
+import { TabContent } from './Tab/Tab';
+
+interface Props {
+  tabs: Tab[];
+}
+
+export const Tabs = ({ tabs }: Props) => {
+  const [activeTab, setActiveTab] = useState('');
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              data-cy="Tab"
+              className={cn({
+                'is-active': tab.id === activeTab,
+              })}
+              key={tab.id}
+            >
+              <NavLink
+                to={`/tabs/${tab.id}`}
+                className={({ isActive }) => {
+                  if (isActive) {
+                    setActiveTab(tab.id);
+                  }
+
+                  return '';
+                }}
+              >
+                {tab.title}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        <Routes>
+          <Route path=":tabId" element={<TabContent tabs={tabs} />} />
+        </Routes>
+      </div>
+    </>
+  );
+};
