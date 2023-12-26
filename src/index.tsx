de516deb8002f9818872c77ddfa3caf6b8ba76cd
Volodymyr Sherstov <sherstov93@gmail.com>
2023-12-26 16:58:07 +0200

    solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,39 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import {
+  RouterProvider,
+  createHashRouter,
+} from 'react-router-dom';
 import { App } from './App';
+import { HomePage } from './pages/HomePage';
+import { TabsPage } from './pages/TabsPage';
+import { ErrorPage } from './pages/ErrorPage';
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '*',
+        element: <ErrorPage />,
+      },
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'tabs',
+        children: [
+          {
+            path: ':tabId?',
+            element: <TabsPage />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root') as HTMLElement)
-  .render(
-    <HashRouter>
-      <App />
-    </HashRouter>,
-  );
+  .render(<RouterProvider router={router} />);
