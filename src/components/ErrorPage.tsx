539d47c75a459825f50f327e0739d577d8c38320
Malyholovka Ostap <maligolovkaostap@gmail.com>
2023-12-26 15:32:35 +0200

    solution

---
+++ src/components/ErrorPage.tsx
@@ -0,0 +1,5 @@
+export const ErrorPage = () => (
+  <div className="section">
+    <h1 className="title">Page not found</h1>
+  </div>
+);
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,25 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import {
+  HashRouter, Navigate, Route, Routes,
+} from 'react-router-dom';
 import { App } from './App';
+import { HomePage } from './components/HomePage';
+import { Tabs } from './components/Tabs/Tabs';
+import { ErrorPage } from './components/ErrorPage';
 
 createRoot(document.getElementById('root') as HTMLElement)
   .render(
     <HashRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="home" element={<Navigate to="/" />} />
+
+          <Route path="tabs" element={<Tabs />}>
+            <Route index path=":tabsId" />
+          </Route>
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
     </HashRouter>,
   );
