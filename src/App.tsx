5d41d8e2a001d112bbff12f708d203587f68a34a
Vladyslav Korobka <vladseipa@gmail.com>
2023-12-28 18:04:38 +0200

    after testing, all the test have passed

--- src/App.tsx
+++ src/App.tsx
@@ -2,50 +2,69 @@ import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
 import classNames from 'classnames';
-import { Route, Routes, NavLink } from 'react-router-dom';
+import { useEffect } from 'react';
+import {
+  Route,
+  Routes,
+  NavLink,
+  useNavigate,
+} from 'react-router-dom';
 import { Home } from './components/Home';
 import { Tabs } from './components/Tabs';
 
-const isLinkActive = ({ isActive } : { isActive: boolean }) => (
+const isLinkActive = ({ isActive }: { isActive: boolean }) => (
   classNames('navbar-item', {
     'is-active': isActive,
   })
 );
 
-export const App = () => (
-  <>
-    <nav
-      className="navbar is-light is-fixed-top is-mobile has-shadow"
-      data-cy="Nav"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <NavLink
-            to="/"
-            className={isLinkActive}
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/tabs"
-            className={isLinkActive}
-          >
-            Tabs
-          </NavLink>
+export const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (window.location.hash === '#/home') {
+      navigate('/');
+    }
+  }, [navigate]);
+
+  return (
+    <>
+      <nav
+        className="navbar is-light is-fixed-top is-mobile has-shadow"
+        data-cy="Nav"
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <NavLink
+              to="/"
+              className={isLinkActive}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/tabs"
+              className={isLinkActive}
+            >
+              Tabs
+            </NavLink>
+          </div>
+        </div>
+      </nav>
+
+      <div className="section">
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/tabs">
+              <Route path=":tabId?" element={<Tabs />} />
+            </Route>
+            <Route
+              path="*"
+              element={<h1 className="title">Page not found</h1>}
+            />
+          </Routes>
         </div>
       </div>
-    </nav>
-
-    <div className="section">
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/tabs">
-            <Route path=":tabId?" element={<Tabs />} />
-          </Route>
-          <Route path="*" element={<h1 className="title">Page not found</h1>} />
-        </Routes>
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import { Navigate } from 'react-router-dom';
 
 export const Home: React.FC = () => {
   return <h1 className="title">Home page</h1>;
