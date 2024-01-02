9297a8393aef51dc8696951a3cff4bec936cb270
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-01-02 14:17:52 +0100

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+import {
+  Route, Routes, NavLink, Navigate,
+} from 'react-router-dom';
+import cn from 'classnames';
+import { Tabs } from './components/tabs';
 
 export const App = () => (
   <>
@@ -17,35 +16,41 @@ export const App = () => (
     >
       <div className="container">
         <div className="navbar-brand">
-          <a href="/" className="navbar-item is-active">Home</a>
-          <a href="/tabs" className="navbar-item">Tabs</a>
+          <NavLink
+            to="/"
+            className={
+              ({ isActive }) => cn('navbar-item', { 'is-active': isActive })
+            }
+            end
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/tabs"
+            className={
+              ({ isActive }) => cn('navbar-item', { 'is-active': isActive })
+            }
+          >
+            Tabs
+          </NavLink>
         </div>
       </div>
     </nav>
 
     <div className="section">
       <div className="container">
-        <h1 className="title">Home page</h1>
-        <h1 className="title">Tabs page</h1>
-        <h1 className="title">Page not found</h1>
-
-        <div className="tabs is-boxed">
-          <ul>
-            <li data-cy="Tab" className="is-active">
-              <a href="#/">Tab 1</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 2</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 3</a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="block" data-cy="TabContent">
-          Please select a tab
-        </div>
+        <Routes>
+          <Route path="/" element={<h1 className="title">Home page</h1>} />
+          <Route path="home" element={<Navigate to="/" />} />
+          <Route path="/tabs">
+            <Route index element={<Tabs />} />
+            <Route path=":tabId" element={<Tabs />} />
+          </Route>
+          <Route
+            path="*"
+            element={<h1 className="title">Page not found</h1>}
+          />
+        </Routes>
       </div>
     </div>
   </>
