854f6529f647b502b1355ec352ef1fdd66eb6899
Laura Wronska <laura.wronskaa@gmail.com>
2024-01-04 11:14:54 +0100

    delete unnecessary comments

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import './App.scss';
 import {
   Link, Navigate, Route, Routes,
 } from 'react-router-dom';
-// import classNames from 'classnames';
 import { Tabs } from './components/Tabs';
 
 const tabs = [
@@ -13,62 +12,52 @@ const tabs = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-export const App = () => {
-  // const { pathname } = useLocation();
-
-  return (
-    <>
-      {/* Also requires <html class="has-navbar-fixed-top"> */}
-      <nav
-        className="navbar is-light is-fixed-top is-mobile has-shadow"
-        data-cy="Nav"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <Link
-              to="/"
-              // className={classNames('navbar-item', {
-              //   'is-active': pathname === '/',
-              // })}
-              className="navbar-item is-active"
-            >
-              Home
-            </Link>
-            <Link
-              to="/tabs"
-              // className={classNames('navbar-item', {
-              //   'is-active': pathname === '/',
-              // })}
-              className="navbar-item"
-            >
-              Tabs
-            </Link>
-          </div>
-        </div>
-      </nav>
-
-      <div className="section">
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<h1 className="title">Home page</h1>} />
-            <Route
-              path="/home"
-              element={
-                <Navigate to="/" />
-              }
-            />
-            <Route path="/tabs" />
-            <Route path="tabs">
-              <Route index element={<Tabs tabs={tabs} />} />
-              <Route path=":tabId" element={<Tabs tabs={tabs} />} />
-            </Route>
-            <Route
-              path="*"
-              element={<h1 className="title">Page not found</h1>}
-            />
-          </Routes>
+export const App = () => (
+  <>
+    {/* Also requires <html class="has-navbar-fixed-top"> */}
+    <nav
+      className="navbar is-light is-fixed-top is-mobile has-shadow"
+      data-cy="Nav"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <Link
+            to="/"
+            className="navbar-item is-active"
+          >
+            Home
+          </Link>
+          <Link
+            to="/tabs"
+            className="navbar-item"
+          >
+            Tabs
+          </Link>
         </div>
       </div>
-    </>
-  );
-};
+    </nav>
+
+    <div className="section">
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<h1 className="title">Home page</h1>} />
+          <Route
+            path="/home"
+            element={
+              <Navigate to="/" />
+            }
+          />
+          <Route path="/tabs" />
+          <Route path="tabs">
+            <Route index element={<Tabs tabs={tabs} />} />
+            <Route path=":tabId" element={<Tabs tabs={tabs} />} />
+          </Route>
+          <Route
+            path="*"
+            element={<h1 className="title">Page not found</h1>}
+          />
+        </Routes>
+      </div>
+    </div>
+  </>
+);
