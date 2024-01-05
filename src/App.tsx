d6f762823d492e248def125c481f0e3f87d661d1
aurelia <aurelia.gorzna.dev@gmail.com>
2024-01-05 15:33:21 +0100

    implemented Navigate

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 /* eslint-disable import/no-cycle */
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
 import { TabsPage } from './components/TabsPage/TabsPage';
-import { HomePage } from './components/HomePage';
 
 export const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -14,13 +13,9 @@ export const tabs = [
 ];
 
 export const App: React.FC = () => {
-  if (window.location.pathname === '/home') {
-    window.location.pathname = '/';
-  }
-
   return (
     <div className="app">
-      <nav className="nav">
+      <nav className="nav" data-cy="Nav">
         <ul className="nav__list">
           <li className="is-active">
             <a href="/">Home</a>
@@ -31,8 +26,20 @@ export const App: React.FC = () => {
         </ul>
       </nav>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<Navigate to="/" />} />
+        <Route
+          path="/"
+          element={(
+            <div className="section">
+              <h1 className="title">Home page</h1>
+            </div>
+          )}
+        />
         <Route path="/tabs" element={<TabsPage />} />
+        <Route
+          path="*"
+          element={<h1 className="title">Page not found</h1>}
+        />
       </Routes>
     </div>
   );
