9f61f14f29e8578b89eea9e377fe3bdd88fd913b
Vladyslav Starodub <starodubvlad08@gmail.com>
2024-01-07 14:21:26 +0200

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import { App } from './App';
 import { HomePage } from './components/HomePage';
 import { TabsPage } from './components/TabsPage';
@@ -7,7 +7,7 @@ import { NotFoundPage } from './components/NotFoundPage';
 
 createRoot(document.getElementById('root') as HTMLElement)
   .render(
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<HomePage />} />
@@ -18,5 +18,5 @@ createRoot(document.getElementById('root') as HTMLElement)
 
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
-    </BrowserRouter>,
+    </HashRouter>,
   );
