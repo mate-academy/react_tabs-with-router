b81e3e50431b912fe3c95a6adbf920c463fefc69
SerhiiUnhurian <sergeyunguryan322@gmail.com>
2024-01-23 23:43:46 +0200

    first try

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-import { NavLink } from 'react-router-dom';
-
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+import { NavLink, Outlet } from 'react-router-dom';
+import classNames from 'classnames';
 
 export const App = () => (
   <>
@@ -18,14 +13,28 @@ export const App = () => (
     >
       <div className="container">
         <div className="navbar-brand">
-          <NavLink to="/" className="navbar-item is-active">Home</NavLink>
-          <NavLink to="/tabs" className="navbar-item">Tabs</NavLink>
+          <NavLink
+            to="/"
+            className={({ isActive }) => classNames('navbar-item', {
+              'is-active': isActive,
+            })}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/tabs"
+            className={({ isActive }) => classNames('navbar-item', {
+              'is-active': isActive,
+            })}
+          >
+            Tabs
+          </NavLink>
         </div>
       </div>
     </nav>
 
     <div className="section">
-      <div className="container">
+      {/* <div className="container">
         <h1 className="title">Home page</h1>
         <h1 className="title">Tabs page</h1>
         <h1 className="title">Page not found</h1>
@@ -47,7 +56,8 @@ export const App = () => (
         <div className="block" data-cy="TabContent">
           Please select a tab
         </div>
-      </div>
+      </div> */}
+      <Outlet />
     </div>
   </>
 );
---
+++ src/components/Tabs.tsx
@@ -0,0 +1,37 @@
+import classNames from 'classnames';
+import { NavLink, useParams } from 'react-router-dom';
+
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+export const Tabs = () => {
+  const { tabId } = useParams();
+  const activeTab = tabs.find(tab => tab.id === tabId);
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              data-cy="Tab"
+              className={classNames({
+                'is-active': activeTab?.id === tab.id,
+              })}
+              key={tab.id}
+            >
+              <NavLink to={`/tabs/${tab.id}`}>{tab.title}</NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab?.content || 'Please select a tab'}
+      </div>
+    </div>
+  );
+};
