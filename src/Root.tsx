a4458f471e86425132b665aaca535c08e460c459
Oleksiy Uchaev <corob.dir@gmail.com>
2024-01-24 19:24:39 +0200

    C:/Program Files/Git/home added

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,4 +1,9 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import {
+  HashRouter,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import { TabsPage } from './components/TabsPage';
 import { App } from './App';
 
@@ -7,6 +12,7 @@ export const Root = () => {
     <HashRouter>
       <Routes>
         <Route path="*" element={<h1 className="title">Page not found</h1>} />
+        <Route path="/home" element={<Navigate to="/" />} />
 
         <Route path="/" element={<App />}>
           <Route index element={<h1 className="title">Home page</h1>} />
