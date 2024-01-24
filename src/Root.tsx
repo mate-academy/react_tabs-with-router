96eb27666a5ff6b932d8cec85536e2a2f0278bda
Oleksiy Uchaev <corob.dir@gmail.com>
2024-01-24 19:05:25 +0200

    solution

---
+++ src/Root.tsx
@@ -0,0 +1,19 @@
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { TabsPage } from './components/TabsPage';
+import { App } from './App';
+
+export const Root = () => {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="*" element={<h1 className="title">H</h1>} />
+
+        <Route path="/" element={<App />}>
+          <Route index element={<h1 className="title">Home page</h1>} />
+          <Route path="tabs/:tabId?" element={<TabsPage />} />
+        </Route>
+
+      </Routes>
+    </HashRouter>
+  );
+};
