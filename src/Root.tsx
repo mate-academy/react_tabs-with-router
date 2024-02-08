3f4109a894a8057554868cd64c7c4d14aa9abc32
Yevhen Baiev <baev838@gmail.com>
2024-02-08 22:10:37 +0200

    Initial commit

---
+++ src/Root.tsx
@@ -0,0 +1,24 @@
+import {
+  HashRouter, Navigate, Route, Routes,
+} from 'react-router-dom';
+import { App } from './App';
+import { HomePage } from './components/HomePage';
+import { TabsPage } from './components/TabsPage';
+import { NotFoundPage } from './components/NotFoundPage';
+
+export const Root = () => (
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path="tabs">
+          <Route index element={<TabsPage />} />
+          <Route path=":tabId" element={<TabsPage />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+      <Route path="home" element={<Navigate to="/" />} />
+    </Routes>
+
+  </HashRouter>
+);
