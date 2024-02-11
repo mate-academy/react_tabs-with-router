20eddebecdddc305a3d1f0eae2237992cb95c7e3
Svyatoslav <Svyatoslavkolomoets@gmail.com>
2024-02-11 03:44:01 +0200

    new solution

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Routes, Route, HashRouter } from 'react-router-dom';
+import {
+  Routes,
+  Route,
+  Navigate,
+  HashRouter,
+} from 'react-router-dom';
 import { App } from './App';
 import { HomePage } from './Pages/HomePage';
-
-import { TabItem } from './components/TabItem';
 import { TabsPage } from './Pages/TabsPage';
+import { TabItem } from './components/TabItem';
 
 export const Root: React.FC = () => {
   return (
@@ -12,9 +16,10 @@ export const Root: React.FC = () => {
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<HomePage />} />
-          <Route path="tabs/" element={<TabsPage />}>
-            <Route path=":tabsId?" element={<TabItem />} />
+          <Route path="tabs/*" element={<TabsPage />}>
+            <Route path=":tabsId" element={<TabItem />} />
           </Route>
+          <Route path="home" element={<Navigate to="/" />} />
         </Route>
         <Route path="*" element={<h1 className="title">Page not found</h1>} />
       </Routes>
