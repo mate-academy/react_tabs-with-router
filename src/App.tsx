e96a5b1e8eee0073a741e10577c2d2a527e1868a
Maksym Avramenko <nonste@outlook.com>
2024-02-21 10:39:57 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,44 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+import React, { useEffect, useState } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { Navigation } from './components/Navigation';
+import { TabsPage } from './components/TabsPage';
+import { Tab } from './types/Tab';
+import tabsData from './data/data';
 
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+export const App: React.FC = () => {
+  const [tabs, setTabs] = useState<Tab[]>([]);
 
-export const App = () => (
-  <>
-    {/* Also requires <html class="has-navbar-fixed-top"> */}
-    <nav
-      className="navbar is-light is-fixed-top is-mobile has-shadow"
-      data-cy="Nav"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <a href="/" className="navbar-item is-active">Home</a>
-          <a href="/tabs" className="navbar-item">Tabs</a>
+  useEffect(() => {
+    setTabs(tabsData);
+  }, []);
+
+  return (
+    <>
+      <Navigation />
+      <div className="section">
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<h1 className="title">Home page</h1>} />
+            <Route path="home" element={<Navigate to="/" replace />} />
+            <Route path="tabs" element={<TabsPage tabs={tabs} />}>
+              {tabs.map(tab => (
+                <Route
+                  key={tab.id}
+                  path=":tabId"
+                  element={<h1>{tab.content}</h1>}
+                />
+              ))}
+            </Route>
+            <Route
+              path="*"
+              element={<h1 className="title">Page not found</h1>}
+            />
+          </Routes>
         </div>
       </div>
-    </nav>
-
-    <div className="section">
-      <div className="container">
-        <h1 className="title">Home page</h1>
-        <h1 className="title">Tabs page</h1>
-        <h1 className="title">Page not found</h1>
-
-        <div className="tabs is-boxed">
-          <ul>
-            <li data-cy="Tab" className="is-active">
-              <a href="#/">Tab 1</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 2</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 3</a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="block" data-cy="TabContent">
-          Please select a tab
-        </div>
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { App } from './App';
+
+const AppWrapper: React.FC = () => {
+  document.documentElement.classList.add('has-navbar-fixed-top');
+
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+};
 
 createRoot(document.getElementById('root') as HTMLElement)
   .render(
-    <HashRouter>
-      <App />
-    </HashRouter>,
+    <AppWrapper />,
   );
