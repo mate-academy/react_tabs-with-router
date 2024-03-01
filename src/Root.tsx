0c8e9bad371569ef54f48d5323d73b0ec87e8a14
Nadiia Romanskaya <97957968+NadiiaR@users.noreply.github.com>
2024-03-01 19:00:43 +0200

    add task solution

---
+++ src/Root.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { App } from './App';
+import { HomePage } from './components/HomePage';
+import { TabsPage } from './components/TabsPage';
+
+export const Root = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path="tabs">
+          <Route index element={<TabsPage />} />
+          <Route path=":tabId?" element={<TabsPage />} />
+        </Route>
+      </Route>
+
+      <Route path="*" element={<h1 className="title">Page not found</h1>} />
+    </Routes>
+  </Router>
+);
---
+++ src/components/TabsPage.tsx
@@ -0,0 +1,13 @@
+import { Tabs } from './Tabs';
+
+export const TabsPage = () => {
+  return (
+    <>
+      <div className="container">
+        <h1 className="title">Tabs page</h1>
+
+        <Tabs />
+      </div>
+    </>
+  );
+};
