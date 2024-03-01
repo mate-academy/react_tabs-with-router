463ce09396d003a803529006ee8f8de8e5f99457
Daniil <danil.beshlebnov@gmail.com>
2024-03-01 19:09:03 +0200

    add changes

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { HomePage } from './components/HomePage';
 import { TabsPage } from './components/TabsPage';
 import { NotFoundPage } from './components/NotFoundPage';
@@ -12,7 +12,7 @@ export const Root = () => {
         <Routes>
           <Route path="/" element={<App />}>
             <Route index element={<HomePage />} />
-
+            <Route path="home" element={<Navigate to="/" />} />
             <Route path="tabs">
               <Route index element={<TabsPage />} />
               <Route path=":tabId" element={<TabsPage />} />
