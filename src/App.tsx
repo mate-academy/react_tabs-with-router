4ca9d7ada7a457a87cb374af7500744661a486c7
vova <hohiop09@gmail.com>
2024-03-19 11:39:12 +0200

    save

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,19 @@ import './App.scss';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { TabsPage } from './components/tabs';
 import { Main } from './components/main';
-import { tabsList } from './tabsContent';
-import { Tab } from './components/tab';
 import { Home } from './components/Home';
 
-export const App = () => (
-  <>
-    {/* Also requires <html class="has-navbar-fixed-top"> */}
+export const App = () => {
+  return (
     <Routes>
       <Route path="/" element={<Main />}>
         <Route index element={<Home />} />
         <Route path="home" element={<Navigate to="/" />} />
-        <Route path="tabs/:id?" element={<TabsPage />}>
-          {/* <Route index element={<Tab content="Please select a tab" />} /> */}
-          {tabsList.map(elem => (
-            <Route
-              path={`${elem.id}`}
-              element={<Tab key={elem.id} content={elem.content} />}
-            />
-          ))}
+        <Route path="tabs" element={<TabsPage />}>
+          <Route path=":tabId" element={<TabsPage />} />
         </Route>
         <Route path="*" element={<h1 className="title">Page not found</h1>} />
       </Route>
     </Routes>
-  </>
-);
+  );
+};
