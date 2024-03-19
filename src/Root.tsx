fb9177e244796acf127bdabd9fd8d75f08ffae39
Hovera-Liubomyr <ljusuk@gmail.com>
2024-03-19 21:58:51 +0200

    react_tabs-with-router

---
+++ src/Root.tsx
@@ -0,0 +1,20 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { App } from './App';
+import { HomePage } from './components/HomePage';
+import { TabsPage } from './components/tabs';
+
+export const Root = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path="tabs">
+          <Route index element={<TabsPage />} />
+          <Route path=":tabId" element={<TabsPage />} />
+        </Route>
+        <Route path="home" element={<Navigate to="/" />} />
+        <Route path="*" element={<h1 className="title">Page not found</h1>} />
+      </Route>
+    </Routes>
+  );
+};
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,9 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
-import { App } from './App';
+import { Root } from '../Root';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <HashRouter>
-    <App />
+    <Root />
   </HashRouter>,
 );
