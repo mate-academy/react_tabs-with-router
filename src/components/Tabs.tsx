d9fe43c4e71266ccce9c7729373c9a616c751530
Denis Hurskiy <denis.gurskiy@gmail.com>
2024-03-21 18:56:30 +0100

    Tabs

---
+++ src/components/Tabs.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { Tab } from '../types/Tab';
+
+type Props = {
+  tabs: Tab[];
+};
+
+export const Tabs: React.FC<Props> = ({ tabs }) => {
+  const { tabId } = useParams();
+
+  const activeTab = tabs.find(tab => tab.id === tabId);
+
+  return <>{activeTab ? activeTab.content : 'Please select a tab'}</>;
+};
