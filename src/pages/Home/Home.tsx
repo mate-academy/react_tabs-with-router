86f804f79b1deeed79af1fa4e4260cf8594f2d52
Yaroslav Nazarenko <yinazarenkoo@gmail.com>
2024-03-28 18:52:39 +0100

    solution

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SectionContainer } from '../../components/SectionContainer';
+
+export const Home: React.FC = () => {
+  return (
+    <SectionContainer>
+      <h1 className="title">Home page</h1>
+    </SectionContainer>
+  );
+};
