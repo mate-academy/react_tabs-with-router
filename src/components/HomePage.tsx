864846c5b0c6c5f68eb86a66acb5920edb29aadc
Ihor Prodan <prodanigor0@gmail.com>
2024-04-02 13:44:35 +0200

    task router

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+export const HomePage = () => {
+  return (
+    <h1 className="title" data-cy="Nav">
+      Home page
+    </h1>
+  );
+};
