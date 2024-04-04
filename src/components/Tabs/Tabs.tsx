553c510badff3c4932d1a0f43d8e6ade242219c9
Oleh Duma <hakerprof@gmail.com>
2024-04-04 16:24:25 +0300

    solution

---
+++ src/components/Tabs/Tabs.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Tab } from '../../types/Tab';
+import classNames from 'classnames';
+import { Link, useParams } from 'react-router-dom';
+
+type Props = {
+  tabs: Tab[];
+};
+
+export const Tabs: React.FC<Props> = ({ tabs }) => {
+  const { tabId } = useParams();
+
+  const isValidTabId = tabs.find(tab => tab.id === tabId);
+
+  return (
+    <>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={classNames({ 'is-active': tab.id === tabId })}
+            >
+              <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {!isValidTabId ? (
+        <div className="block" data-cy="TabContent">
+          Please select a tab
+        </div>
+      ) : (
+        <div className="block" data-cy="TabContent">
+          {tabs.find(tab => tab.id === tabId)?.content}
+        </div>
+      )}
+    </>
+  );
+};
