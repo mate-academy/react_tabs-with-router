30c5d169da2a5b53412f004b6384f0bc040d1305
Yevhen Vladarskyi <vladarskyy@gmail.com>
2024-04-09 02:32:23 +0300

    delete comment

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,6 @@ export const App = () => (
 
     <div className="section">
       <div className="container">
-        {/* <h1 className="title">Page not found</h1> */}
         <Routes>
           <Route path="/" element={<h1 className="title">Home page</h1>} />
           <Route path="tabs">
