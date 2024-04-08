d58605df01007730955a6391a0bb8580807a3c58
OleksiiNesteruk <nesteruk.oleksiy@gmail.com>
2024-04-08 11:08:30 +0100

    Added task solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import { App } from './App';
+import { Root } from './Root';
 
-createRoot(document.getElementById('root') as HTMLElement).render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
-);
+const container = document.getElementById('root') as HTMLElement;
+
+createRoot(container).render(<Root />);
