71e348037c16fed4c85658f9234f7eb255cd095e
ArtyWhit <julwer1k@gmail.com>
2024-04-13 17:22:54 +0300

    finished

---
+++ src/pages/TabsPage.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import { Tabs } from '../components/Tabs';
+
+export const TabsPage: FC = () => {
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <Tabs />
+    </>
+  );
+};
