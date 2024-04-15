9461f742820e0f1e57ec3a12c80166fa9d9665cc
Nadiia Vlasenko <vlasenkonadia19@gmail.com>
2024-04-16 00:22:54 +0200

    Changed code

--- src/Root.tsx
+++ src/Root.tsx
@@ -8,11 +8,11 @@ export const Root = () => (
   <Router>
     <Routes>
       <Route path="/" element={<App />}>
-        <Route path="home" element={<Navigate to="/" />} />
+        <Route path="home" element={<Navigate to="/" replace={true} />} />
         <Route index element={<HomePage />} />
-        <Route path="tabs">
+        <Route path="tabs" element={<TabsPage />}>
           <Route index element={<TabsPage />} />
-          <Route path=":tabId?" element={<TabsPage />} />
+          <Route path=":tabId" element={<TabsPage />} />
         </Route>
         <Route path="*" element={<h1 className="title">Page not found</h1>} />
       </Route>
