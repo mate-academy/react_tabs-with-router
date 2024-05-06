5ae4399f1a2a400e22678ff16a49b1fa1f8326a9
mrsvolodya <vovamursky@gmail.com>
2024-05-06 23:16:24 +0300

    start

---
+++ src/Root.tsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import { App } from './App';
+import { HomePage } from './pages/HomePage';
+import { TabsPage } from './pages/TabsPage';
+
+export const Root = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path="tabs">
+          <Route index element={<TabsPage />} />
+          <Route path=":tabId" element={<TabsPage />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
+};
