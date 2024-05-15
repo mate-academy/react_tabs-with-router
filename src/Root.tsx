5b871bb1bba902f08ff4628330d09fea61c8246b
Roman Perepelkin <perepelkinrs@gmail.com>
2024-05-15 10:47:14 +0300

    add task solution

---
+++ src/Root.tsx
@@ -0,0 +1,23 @@
+import { HashRouter, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { App } from './App';
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './App.scss';
+import { TabsPage } from './components/TabsPage';
+
+export const Root = () => (
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="/home" element={<Navigate to="/" />} />
+        <Route path="/" element={<h1 className="title">Home page</h1>} />
+        <Route path="tabs">
+          <Route index element={<TabsPage />} />
+          <Route path=":tabId" element={<TabsPage />} />
+        </Route>
+        <Route path="*" element={<h1 className="title">Page not found</h1>} />
+      </Route>
+    </Routes>
+  </HashRouter>
+);
