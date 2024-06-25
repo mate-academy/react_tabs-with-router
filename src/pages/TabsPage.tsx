f099ce246eb8d2b53c91c2ab8b3284ed947015a9
Rostyslav Sobchyshyn <rosta2604@icloud.com>
2024-06-25 18:34:52 +0100

    Solution

---
+++ src/pages/TabsPage.tsx
@@ -0,0 +1,11 @@
+import { Tabs } from '../components/Tabs';
+
+export const TabsPage: React.FC = () => {
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <Tabs />
+    </>
+  );
+};
