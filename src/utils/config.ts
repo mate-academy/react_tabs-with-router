a0b9b06b80c51a7ad4fd9e9ebf0d8a740c833fb5
Arthur Tolkachov <babanjerelo@gmail.com>
2024-07-11 20:04:46 +0300

    [Feature] added tabs router

---
+++ src/utils/config.ts
@@ -0,0 +1,33 @@
+export const HIGH_LEVEL_NAV_CONFIG = [
+  {
+    key: 'home',
+    to: '/',
+    title: 'Home',
+  },
+  {
+    key: 'tabs',
+    to: '/tabs',
+    title: 'Tabs',
+  },
+];
+
+export const LOW_LEVEL_NAV_CONFIG = [
+  {
+    key: 'tab-1',
+    to: '/tabs/tab-1',
+    title: 'Tab 1',
+    content: 'Some text 1',
+  },
+  {
+    key: 'tab-2',
+    to: '/tabs/tab-2',
+    title: 'Tab 2',
+    content: 'Some text 2',
+  },
+  {
+    key: 'tab-3',
+    to: '/tabs/tab-3',
+    title: 'Tab 3',
+    content: 'Some text 3',
+  },
+];
