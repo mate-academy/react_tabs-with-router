830ba92febb4dad360cac085fe01843a371b30c8
Volodymyr Isaiev <isaiev.v.o@gmail.com>
2024-07-13 22:12:43 +0300

    Add tabs with router

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,56 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
+import { Tabs } from './Tabs/Tabs';
+import classNames from 'classnames';
+import { Tab } from './Tab/Tab';
 
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
 
-export const App = () => (
-  <>
-    {/* Also requires <html class="has-navbar-fixed-top"> */}
-    <nav
-      className="navbar is-light is-fixed-top is-mobile has-shadow"
-      data-cy="Nav"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <a href="/" className="navbar-item is-active">
-            Home
-          </a>
-          <a href="/tabs" className="navbar-item">
-            Tabs
-          </a>
+export const App = () => {
+  const getLinkClasses = ({ isActive }: { isActive: boolean }) => {
+    return classNames('navbar-item', { 'is-active': isActive });
+  };
+
+  return (
+    <>
+      <nav
+        className="navbar is-light is-fixed-top is-mobile has-shadow"
+        data-cy="Nav"
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <NavLink to="/" className={getLinkClasses}>
+              Home
+            </NavLink>
+            <NavLink to="/tabs" className={getLinkClasses}>
+              Tabs
+            </NavLink>
+          </div>
+        </div>
+      </nav>
+
+      <div className="section">
+        <div className="container">
+          <Routes>
+            <Route
+              path={'*'}
+              element={<h1 className="title">Page not found</h1>}
+            />
+            <Route path={'/'} element={<h1 className="title">Home page</h1>} />
+            <Route path={'home'} element={<Navigate to={'/'} replace />} />
+            <Route path={'tabs'} element={<Tabs tabs={tabs} />}>
+              <Route index element={<Tab tabs={tabs} />} />
+              <Route path=":tabId" element={<Tab tabs={tabs} />} />
+            </Route>
+          </Routes>
         </div>
       </div>
-    </nav>
-
-    <div className="section">
-      <div className="container">
-        <h1 className="title">Home page</h1>
-        <h1 className="title">Tabs page</h1>
-        <h1 className="title">Page not found</h1>
-
-        <div className="tabs is-boxed">
-          <ul>
-            <li data-cy="Tab" className="is-active">
-              <a href="#/">Tab 1</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 2</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 3</a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="block" data-cy="TabContent">
-          Please select a tab
-        </div>
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
