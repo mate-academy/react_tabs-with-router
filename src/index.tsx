a19e840927cd530a4584c867575c71bf6b13c605
Stanislav Kushnir <florian.of.lorch@gmail.com>
2024-07-18 00:05:09 +0300

    add task solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,23 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { App } from './App';
+import { HomePage } from './HomePage';
+import { TabsPage } from './TabsPage';
+import { ErrorPage } from './ErrorPage';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <HashRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="home" element={<HomePage />} />
+        <Route path="tabs">
+          <Route index element={<TabsPage />} />
+          <Route path=":tabId" element={<TabsPage />} />
+        </Route>
+
+        <Route path="home" element={<Navigate to="/" />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Route>
+    </Routes>
   </HashRouter>,
 );
