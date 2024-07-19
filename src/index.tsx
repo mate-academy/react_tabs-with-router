e12368ab8abb1908aa213cb989ccfb0e8243c882
DSB-18 <boboshko.dmitriy@gmail.com>
2024-07-19 09:03:19 +0300

    add solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, Navigate } from 'react-router-dom';
 import { App } from './App';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Tabs } from './pages/Tabs';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <HashRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="/home" element={<Navigate to="/" />} />
+        <Route index element={<Home />} />
+        <Route path="tabs">
+          <Route path=":tabID?" element={<Tabs />} />
+        </Route>
+        <Route path="*" element={<h1 className="title">Page not found</h1>} />
+      </Route>
+    </Routes>
   </HashRouter>,
 );
