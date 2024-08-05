812448442dc704d9cb45e0021300dfc9a309a517
Daniil <daniilbarilotti@gmail.com>
2024-08-05 16:54:12 +0200

    add task solution

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,5 @@
 const Home = () => {
-    return <h1 className="title">Home page</h1>;
-  };
-  
-  export default Home;
-  
+  return <h1 className="title">Home page</h1>;
+};
+
+export default Home;
