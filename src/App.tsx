4cc6ce4d6929e7aabcf6b97ee203c99f1a6cc21d
Oleksandr Poviakalo <oleksandr.poviakalo@gmail.com>
2024-08-21 19:28:43 +0300

    Changed path in the NavLink for Home

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ export const App = () => {
       >
         <div className="container">
           <div className="navbar-brand">
-            <NavLink to="/home" className={getNavLinkClass}>
+            <NavLink to="/" className={getNavLinkClass}>
               Home
             </NavLink>
             <NavLink to="/tabs" className={getNavLinkClass}>
