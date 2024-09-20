1cb240407ab34624dfee9d041e32f86976f96b58
Kyryl Maslov <kyrylmaslov2007@gmail.com>
2024-09-20 17:13:42 +0200

    react tabs with router was realised

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const HomePage: React.FC = () => {
+  return (
+    <>
+      <h1 className="title">Home page</h1>
+    </>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/TabsPage/TabsPage.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Tab } from '../../types/Tab';
+import classNames from 'classnames';
+
+const tabs: Tab[] = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+export const TabsPage: React.FC = () => {
+  const { tabId } = useParams();
+  const selectedTabId = tabId ? tabId : '';
+
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId);
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            return (
+              <li
+                data-cy="Tab"
+                className={classNames({
+                  'is-active': selectedTabId === tab.id,
+                })}
+                key={tab.id}
+              >
+                <Link to={`../${tab.id}`}>{tab.title}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab ? selectedTab.content : 'Please select a tab'}
+      </div>
+    </>
+  );
+};
+
+export default TabsPage;
