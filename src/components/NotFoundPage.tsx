a1a80c48e4bbe8013fa9b19f89fc33cd9a2df921
Korolko Anastasiia <korolkonasta@gmail.com>
2024-10-09 18:31:14 +0300

    add solution

---
+++ src/components/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+export const NotFoundPage: React.FC = () => {
+  return (
+    <>
+      <nav
+        className="navbar is-light is-fixed-top is-mobile has-shadow"
+        data-cy="Nav"></nav>
+      <h1 className="title">Page not found</h1>
+    </>
+
+  )
+}
