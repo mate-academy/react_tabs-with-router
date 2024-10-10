72e973cb96bad7ec67c7526cb07e97dc3ab33b42
natalagoncaruk <honcharukna@gmail.com>
2024-10-10 12:11:57 +0300

    correct mistakes

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,9 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
 import { NavLink, Outlet } from 'react-router-dom';
 import { getNavLinkClass } from './utils/getNavLinkClass';
-import { getNewLibraryCopy } from 'cypress/types/bluebird';
-
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
 
 export const App = () => (
   <>
-    {/* Also requires <html class="has-navbar-fixed-top"> */}
     <nav
       className="navbar is-light is-fixed-top is-mobile has-shadow"
       data-cy="Nav"
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import { App } from './App';
+
 import { Root } from './routes/Root';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
