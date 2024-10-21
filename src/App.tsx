52d4a2dc58eddcc1d9e043d84e9a6c97c2c13e1f
Zubkovskiy Bogdan <zubkovvsbogdan@gmail.com>
2024-10-21 18:17:46 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import { Outlet } from 'react-router-dom';
-import { Navigation } from './components/Navigation';
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import './App.scss';
+
+import { Route, Routes, Navigate } from 'react-router-dom';
+
+import { Layout } from './components/Layout';
+
+import { Home, Tabs, NotFoundPage } from './pages';
 
 export const App = () => (
   <>
-    <Navigation />
-    <Outlet />
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="home" element={<Navigate to="/" />} />
+
+        <Route path="tabs">
+          <Route path=":tabsID?" element={<Tabs />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
   </>
 );
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import { createRoot } from 'react-dom/client';
-import { Root } from './Root';
+import { HashRouter } from 'react-router-dom';
+import { App } from './App';
 
-const container = document.getElementById('root') as HTMLElement;
-
-createRoot(container).render(<Root />);
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+);
