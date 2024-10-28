916349c37e085ce9a1d1dd2e3bc01bf83632ae6f
AmateZz <yarike451@gmail.com>
2024-10-28 13:03:55 +0200

    ready

---
+++ src/components/TabsPage.jsx
@@ -0,0 +1,14 @@
+import { TabsList } from './TabsList';
+
+export const TabsPage = () => {
+  return (
+    // eslint-disable-next-line react/jsx-filename-extension
+    <div className="section">
+      <div className="container">
+        <h1 className="title">Tabs page</h1>
+
+        <TabsList />
+      </div>
+    </div>
+  );
+};
