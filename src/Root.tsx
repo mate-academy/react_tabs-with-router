9b0d3b48c94dbb336ebb484302cdf53cc2a157ab
Victor Komara <vitioklss@gmail.com>
2024-11-27 13:19:46 +0200

    add task solution

---
+++ src/Root.tsx
@@ -0,0 +1,25 @@
+import { HashRouter, Navigate } from 'react-router-dom';
+import { App } from './App';
+import { Route, Routes } from 'react-router-dom';
+import { Tabs } from './pages/Tabs';
+import { Home } from './pages/Home';
+import { NotFound } from './pages/NotFound';
+
+export const Root = () => {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Navigate to="/" replace />} />
+
+          <Route path="tabs">
+            <Route path=":tabId?" element={<Tabs />} />
+          </Route>
+
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
+};
