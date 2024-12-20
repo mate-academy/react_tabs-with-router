7115839e289f29d822e6b95ab230a5f91d4b4b2c
Halyna <minimal005@gmail.com>
2024-12-20 17:42:21 +0200

    solution

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { HashRouter } from 'react-router-dom';
 import { App } from './App';
 import { TabsPage } from './pages/TabsPage';
@@ -11,6 +11,7 @@ export const Root = () => (
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<HomePage />} />
+          <Route path="/home" element={<Navigate to="/" replace />} />
           <Route path="tabs">
             <Route index element={<TabsPage />} />
             <Route path=":tabId" element={<TabsPage />} />
